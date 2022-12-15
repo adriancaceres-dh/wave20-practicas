3d92627de050afa7cb3eaf1f401a2343f558380d
Augusto Diaz <augusto.diaz@mercadolibre.com.co>
2022-12-15 16:58:24 -0500

    version funcional carrera de la selva

--- 2. java/Carrera de la Selva/src/Bootcamp/Main.java
+++ 2. java/Carrera de la Selva/src/Bootcamp/Main.java
@@ -1,6 +1,7 @@
 package Bootcamp;
 
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -34,17 +35,26 @@ public class Main {
                         if(tipoCircuito > 3 || tipoCircuito < 1);
                         else{
                             if(tipoCircuito != 3) {
-                                anadirParticipante(entrada, nroCompetidores, tipoCircuito, datosParticipante);
+                                try {
+                                    annadirParticipante(participantes, entrada, nroCompetidores, tipoCircuito, datosParticipante);
+                                } catch(InputMismatchException e) {
+                                    System.out.println("Ingrese un número válido");
+                                }
                                 nroCompetidores++;
+                                bandera = false;
                             }
                             else {
                                 try {
                                     System.out.println("Digite su edad");
                                     int edad = entrada.nextInt();
                                     if(edad < 18) throw new Exception();
-                                    anadirParticipante(entrada, nroCompetidores, tipoCircuito, datosParticipante);
+                                    annadirParticipante(participantes, entrada, nroCompetidores, tipoCircuito, datosParticipante);
+                                    nroCompetidores++;
+                                    bandera = false;
+                                } catch(InputMismatchException e) {
+                                    System.out.println("Ingrese un número valido");
                                 } catch(Exception e) {
-                                    System.out.println("Tiene que ser mayor de edad");
+                                    System.out.println("Tiene que ser mayor de edad para inscribirse en este circuito");
                                 }
                             }
                         }
@@ -52,29 +62,94 @@ public class Main {
                     break;
                 }
                 case 2: {
-                    
+                    int tipoCircuito;
+                    System.out.println("Digite el tipo de circuito al que desea inscribirse" +
+                            "\n1. Circuito chico" +
+                            "\n2. Circuito medio" +
+                            "\n3. Circuito avanzado");
+                    tipoCircuito = entrada.nextInt();
+                    for(Map.Entry<String, Object[]> entry : participantes.entrySet()) {
+                        if((int) entry.getValue()[6] == tipoCircuito) {
+                            System.out.println("DNI: " + String.valueOf(entry.getKey()));
+                            System.out.println("Nombre: " + String.valueOf(entry.getValue()[0]));
+                            System.out.println("Apellido: " + String.valueOf(entry.getValue()[1]));
+                            System.out.println("Edad: " + String.valueOf(entry.getValue()[2]));
+                            System.out.println("Celular: " + String.valueOf(entry.getValue()[3]));
+                            System.out.println("Nro Emergencia: " + String.valueOf(entry.getValue()[4]));
+                            System.out.println("Tipo de sangre: " + String.valueOf(entry.getValue()[5]));
+                            System.out.println("Numero de inscripcion: " + String.valueOf(entry.getValue()[7]));
+                            System.out.println();
+                        }
+                    }
+                    break;
+                }
+                case 3: {
+                    String dni;
+                    System.out.print("Digite el DNI del participante a eliminar: ");
+                    entrada.nextLine();
+                    dni = entrada.nextLine();
+                    participantes.remove(dni);
+                    break;
+                }
+                case 4: {
+                    String dni;
+                    System.out.println("Digite el DNI del participante a consultar");
+                    entrada.nextLine();
+                    dni = entrada.nextLine();
+                    int tipoCircuito = (int) participantes.get(dni)[6];
+                    int edad = (int) participantes.get(dni)[2];
+                    int totalACancelar= 0;
+                    switch(tipoCircuito) {
+                        case 1: {
+                            if(edad >= 18) {
+                                totalACancelar = 1500;
+                            }
+                            else {
+                                totalACancelar = 1300;
+                            }
+                            break;
+                        }
+                        case 2: {
+                            if (edad >= 18) {
+                                totalACancelar = 2300;
+                            }
+                            else {
+                                totalACancelar = 2000;
+                            }
+                            break;
+                        }
+                        case 3: {
+                            totalACancelar = 2800;
+                            break;
+                        }
+                    }
+                    String mensaje = totalACancelar == 0 ? "Ud no se encuentra inscrito" : "Su total a cancelar es: " + totalACancelar;
+                    System.out.println(mensaje);
                 }
             }
         } while(opcion != 0);
     }
 
-    private static void anadirParticipante(Scanner entrada, int nroCompetidores, int tipoCircuito, Object[] datosParticipante) {
+    private static void annadirParticipante(Map<String, Object[]> participantes, Scanner entrada, int nroCompetidores, int tipoCircuito, Object[] datosParticipante) {
         String dni;
-        System.out.println("Digite su DNI");
-        dni = entrada.nextLine();
-        System.out.println("Digite su Nombre");
+        System.out.print("Digite su DNI: ");
+        dni = entrada.next();
+        System.out.print("Digite su Nombre: ");
+        entrada.nextLine();
         datosParticipante[0] = entrada.nextLine();
-        System.out.println("Digite su apellido");
+        System.out.print("Digite su apellido: ");
         datosParticipante[1] = entrada.nextLine();
-        System.out.println("Digite su edad");
+        System.out.print("Digite su edad: ");
         datosParticipante[2] = entrada.nextInt();
-        System.out.println("Digite su celular");
+        entrada.nextLine();
+        System.out.print("Digite su celular: ");
         datosParticipante[3] = entrada.nextLine();
-        System.out.println("Digite un número para casos de emergencia");
+        System.out.print("Digite un número para casos de emergencia: ");
         datosParticipante[4] = entrada.nextLine();
-        System.out.println("Digite su grupo sanguíneo");
+        System.out.print("Digite su grupo sanguíneo: ");
         datosParticipante[5] = entrada.nextLine();
         datosParticipante[6] = tipoCircuito;
         datosParticipante[7] = nroCompetidores;
+        participantes.put(dni, datosParticipante);
     }
 }
