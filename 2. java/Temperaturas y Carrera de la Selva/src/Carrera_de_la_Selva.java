ceb4f6980d89cea516a99932a7dba32bbda1d7c1
Emanoide47 <emanuel.fonseca@mercadolibre.com>
2022-12-15 17:04:30 -0300

    Se agregan comentario al código.

--- 2. java/Temperaturas y Carrera de la Selva/src/Carrera_de_la_Selva.java
+++ 2. java/Temperaturas y Carrera de la Selva/src/Carrera_de_la_Selva.java
@@ -2,11 +2,26 @@ import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 
+/**
+
+ * Esta clase define las listas de categorias que existen en la carrera como algunas funciones básicas de agregación y eliminación
+ * de participantes.
+
+ * @author: Emanuel Fonseca
+ */
 public class Carrera_de_la_Selva {
+
+    // Lista que contiene los participantes de la categoria Chico.
     Map<Integer, Participante> categoriaChico;
+
+    // Lista que contiene los participantes de la categoria Medio.
     Map<Integer, Participante> categoriaMedio;
+
+    // Lista que contiene los participantes de la categoria Avanzada.
     Map<Integer, Participante> categoriaAvanzada;
 
+    // Numero de inscripto del participante. Cabe destacar que es único para cada participante e incremental luego de una
+    // nueva inscripción.
     private int numeroDeInscripción;
 
     public Carrera_de_la_Selva() {
@@ -16,8 +31,15 @@ public class Carrera_de_la_Selva {
         numeroDeInscripción = 1;
     }
 
+    /**
+     * Constructor para la serie de números aleatorios
+     * @param nuevoParticipante El parámetro nuevoParticipamente define el nuevo participante a agregar en la categoria chica.
+
+     */
     public void agregarCompetidorCategoriaChica(Participante nuevoParticipante) {
         categoriaChico.put(numeroDeInscripción, nuevoParticipante);
+
+        // Al momento de agregar al participante ya se calcula el monto que debe pagar según su edad.
         if (nuevoParticipante.getEdad() < 18) {
             nuevoParticipante.setMontoPagar(1300);
         } else {
@@ -30,6 +52,12 @@ public class Carrera_de_la_Selva {
         return numeroDeInscripción;
     }
 
+    /**
+
+     * Muestra solo los participantes de la categoria chica.
+
+     * @param
+     */
     public void mostrarParticipantesCategoriaChico() {
         for (Map.Entry<Integer, Participante> participante : categoriaChico.entrySet()) {
             System.out.println("----" + " Numero de inscripción: " + participante.getKey() + " ---------------");
@@ -38,15 +66,23 @@ public class Carrera_de_la_Selva {
         }
     }
 
+    /**
+
+     * Elimina un usuario, pasado por parámetro, de la categoria chica.
+
+     * @param eliminar es el participante que será eliminado de la competencia categoria chica.
+     */
     public void desincribirParticipante(Participante eliminar) {
         int dniEliminar = eliminar.getDni();
         for (Map.Entry<Integer, Participante> participante : categoriaChico.entrySet()) {
+            // Comparamos que el DNI de la persona a eliminar sea igual a alguno de la lista de competidores en esta categoria.
             if (participante.getValue().getDni() == dniEliminar) {
                 categoriaChico.remove(participante.getKey());
                 System.out.println("Participante eliminado con exito");
                 return;
             }
         }
+        // No se encontró el usuario pasado por parámetro.
         System.out.println("Persona no existe en categoria");
     }
 }
--- 2. java/Temperaturas y Carrera de la Selva/src/Participante.java
+++ 2. java/Temperaturas y Carrera de la Selva/src/Participante.java
@@ -1,12 +1,32 @@
+/**
+ * Esta clase define los objetos que contendrán los datos de cada participante.
+
+ * @author: Emanuel Fonseca
+*/
 public class Participante {
+
+    // Variable que almacena el valor del DNI de la persona.
     private int dni;
+
+    // Nombre de la persona.
     private String nombre;
+
+    // Apellido de la persona.
     private String apellido;
+
+    // Edad de la persona.
     private int edad;
+
+    // Celular de la persona.
     private int celular;
+
+    // Numero de emergencia en caso de llamar para la persona.
     private int numEmergencia;
+
+    // Grupo sanguineo de la persona.
     private String grupoSanguineo;
 
+    // Monto a pagar por la categoria ingresada del participante.
     private int montoPagar;
 
     public Participante(int dni, String nombre, String apellido, int edad, int celular, int numEmergencia, String grupoSanguineo) {
@@ -56,6 +76,7 @@ public class Participante {
         return grupoSanguineo;
     }
 
+    // Obtención de los datos en un String entero del participante.
     public String getDatosParticipante(){
         return getDni() +" - "+ getNombre() +" - "+ getApellido() +" - "+ getEdad() +" - "+ getCelular() +" - "+ getNumEmergencia() +" - "+ getGrupoSanguineo();
     }
