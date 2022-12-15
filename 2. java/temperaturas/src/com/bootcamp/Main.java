6534c1a54e2a5f8be385b551bc82f39b3abf97c8
DiegoAlejandroMalagon <diego.malagon@mercadolibre.com.co>
2022-12-15 12:19:03 -0500

    Ejercicio de temperaturas

---
+++ 2. java/temperaturas/src/com/bootcamp/Main.java
@@ -0,0 +1,30 @@
+package com.bootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String ciudades[] = {"Londres", "Madrid", "Nueva York", "Buenos Aires", "Asunción", "Sao Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
+        int[][] temperaturas = {{-2, 33}, {-3, 32}, {-8, 27}, {4, 37}, {6, 42}, {5, 43}, {0, 39}, {-7, 26}, {-1, 31}, {-10, 35}};
+
+        int tempMinima = temperaturas[0][0];
+        int tempMaxima = temperaturas[0][1];
+        int posTempMin = 0;
+        int posTempMax = 0;
+
+        for (int i = 1; i < 10; i++) {
+            if (temperaturas[i][0] < tempMinima) {
+                tempMinima = temperaturas[i][0];
+                posTempMin = i;
+            }
+
+            if (temperaturas[i][1] > tempMaxima) {
+                tempMaxima = temperaturas[i][1];
+                posTempMax = i;
+            }
+
+        }
+
+        System.out.println("La menor temperatura la tuvo " + ciudades[posTempMin] + " con " + tempMinima);
+        System.out.println("La mayor temperatura la tuvo " + ciudades[posTempMax] + " con " + tempMaxima);
+    }
+}
