d3c79ff55e857d9c13500fe7e5d692b538a39622
Alejandrotm20 <edison.torres@mercadolibre.com.co>
2022-12-15 12:04:08 -0500

    Ejercicio 1 de Java

--- 2. java/EjercicioTemperaturas/src/Bootcamp/Temperaturas.java
+++ 2. java/EjercicioTemperaturas/src/Bootcamp/Temperaturas.java
@@ -1,5 +1,6 @@
 package Bootcamp;
-public class Ejercicio1 {
+
+public class Temperaturas {
     public static void main(String[] args){
         String ciudades[] = {"Londres","Madrid", "Nueva York", "Buenos Aires", "Asuncion", "Sao Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokio"};
         int temperaturas[][] = {{-2,33}, {-3,32}, {-8,27}, {4,37}, {6,42}, {5,43}, {0,39}, {-7,26}, {-1,31}, {-10,35}};
