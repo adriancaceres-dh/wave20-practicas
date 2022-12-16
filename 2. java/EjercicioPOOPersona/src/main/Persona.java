4882fbea45c7e8d62be6671074c4bde89930fda6
itornati <ignacio.tornati@mercadolibre.com>
2022-12-16 17:16:05 -0300

    Ejercicio de POO

---
+++ 2. java/EjercicioPOOPersona/src/main/Persona.java
@@ -0,0 +1,95 @@
+package main;
+
+public class Persona {
+    String nombre;
+    int edad;
+    String dni;
+    int peso;
+    double altura;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public Persona(String nombre, int edad, String dni, int peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int calcularIMC(){
+        double imc  = peso/(altura*altura);
+        int valorRetorno = 1;
+        if(imc <20){
+            valorRetorno = -1;
+        }else if(imc >= 20 && imc <= 25 ){
+            valorRetorno = 0;
+        }
+
+        return valorRetorno;
+
+
+    }
+
+    public boolean esMayorDeEdad(){
+        return edad>=18;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", dni='" + dni + '\'' +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+}
