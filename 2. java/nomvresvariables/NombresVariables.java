df8d1699bacef43137f6b7a5ec92112ed83572ac
brumen18 <agustin.iglesias@mercadolibre.com>
2022-12-16 16:33:51 -0300

    añadido proyecto nombresvariables.

---
+++ 2. java/nomvresvariables/NombresVariables.java
@@ -0,0 +1,21 @@
+
+package nombresvariables;
+
+
+public class NombresVariables {
+
+    public static void main(String[] args) {
+        
+//este es el codigo base del ejercicio Nombres de variables. En el encontrarás errores que deberás corregir
+
+        String nombre;
+        int edad;
+        double sueldo;
+        String apellido;
+        String direccion;
+        boolean licencia_de_conducir;
+        double EstaturaDeLaPersona;
+        int cantidad_de_hijos;
+    }
+    
+}
