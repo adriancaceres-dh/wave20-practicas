2c2dfd278a2c96096d3cf5bade6d27709c3a5325
LuBenavides22 <luciano.benavides@mercadolibre.com>
2022-12-16 10:57:06 -0300

    improved ejercicio2

--- 2. java/src/com/bootcamp/Ejercicio_2.java
+++ 2. java/src/com/bootcamp/Ejercicio_2.java
@@ -1,12 +1,14 @@
 package com.bootcamp;
 
+import java.sql.Array;
 import java.sql.SQLOutput;
 import java.util.*;
 
 public class Ejercicio_2 {
 
-    public static void registrarParticipante(Map<String, Map<String, String>> participantes,
-                                                                         Scanner sc) {
+    public static void registrarParticipante(Map<String, Map<String, String>> participantes, Scanner sc,
+                                             int[] circuito_chico, int[] circuito_medio, int circuito_avanzado,
+                                             int identificador, int option) {
 
         System.out.println("DNI");
         String dni = sc.nextLine();
@@ -26,12 +28,18 @@ public class Ejercicio_2 {
 
         Map<String, String> datos = new TreeMap<>();
 
+        datos.put("identificador", Integer.toString(++identificador));
         datos.put("dni", dni);
         datos.put("apellido", apellido);
         datos.put("edad", edad);
         datos.put("celular", celular);
         datos.put("emergencia", emergencia);
         datos.put("grupo_sanguineo", grupo_sanguineo);
+        switch (option){
+            case(1):
+                datos.put("circuito", "chico");
+
+        }
 
         participantes.put(nombre, datos);
 
@@ -46,15 +54,28 @@ public class Ejercicio_2 {
     public static void main (String[] args) {
         Map<String, Map<String, String>> participantes = new HashMap<>();
         Scanner sc = new Scanner(System.in);
+        int identificador = 0;
+        int[] circuito_chico= {1300, 1500};
+        int[] circuito_medio= {2000, 2300};
+        int circuito_avanzado= 2800;
 
         while(true){
             System.out.println("Elija una opcion:");
             System.out.println("1. Registrar un participante");
             System.out.println("2. Salir");
-            int option = sc.nextInt();
+            int option = Integer.parseInt(sc.nextLine());
             switch(option){
                 case(1):
-        registrarParticipante(participantes, sc);
+                    System.out.println("Elija un circuito");
+                    System.out.println("1. Circuito Chico");
+                    System.out.println("2. Circuito Medio");
+                    System.out.println("3. Circuito Avanzado");
+                    option = Integer.parseInt(sc.nextLine());
+                    switch (option){
+                        case(1):
+                            registrarParticipante(participantes, sc, circuito_chico, circuito_medio, circuito_avanzado,
+                            identificador, option);
+                    }
                 case(2):
                     return;
             }
