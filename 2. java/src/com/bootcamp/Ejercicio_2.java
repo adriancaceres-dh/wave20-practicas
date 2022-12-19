bbafcfd39247407e564a2797310336463f9241a4
LuBenavides22 <luciano.benavides@mercadolibre.com>
2022-12-19 11:01:28 -0300

    mostrat participantes

--- 2. java/src/com/bootcamp/Ejercicio_2.java
+++ 2. java/src/com/bootcamp/Ejercicio_2.java
@@ -8,7 +8,7 @@ public class Ejercicio_2 {
 
     public static void registrarParticipante(Map<String, Map<String, String>> participantes, Scanner sc,
                                              int[] circuito_chico, int[] circuito_medio, int circuito_avanzado,
-                                             int identificador, int option) {
+                                             int identificador, int circuito) {
 
         System.out.println("DNI");
         String dni = sc.nextLine();
@@ -28,18 +28,35 @@ public class Ejercicio_2 {
 
         Map<String, String> datos = new TreeMap<>();
 
-        datos.put("identificador", Integer.toString(++identificador));
+        datos.put("identificador", Integer.toString(identificador));
         datos.put("dni", dni);
         datos.put("apellido", apellido);
         datos.put("edad", edad);
         datos.put("celular", celular);
         datos.put("emergencia", emergencia);
         datos.put("grupo_sanguineo", grupo_sanguineo);
-        switch (option){
+
+        System.out.println(circuito);
+
+        switch (circuito){
             case(1):
                 datos.put("circuito", "chico");
-
+                if(Integer.parseInt(edad) < 18) datos.put("monto", "1300");
+                if(Integer.parseInt(edad) >= 18) datos.put("monto", "1500");
+                break;
+            case(2):
+                datos.put("circuito", "medio");
+                if(Integer.parseInt(edad) < 18) datos.put("monto", "2000");
+                if(Integer.parseInt(edad) >= 18) datos.put("monto", "2300");
+                break;
+            case(3):
+                datos.put("circuito", "avanzado");
+                if(Integer.parseInt(edad) < 18) datos.put("monto", "invalid participant");
+                if(Integer.parseInt(edad) >= 18) datos.put("monto", "2800");
+                break;
         }
+
+
 
         participantes.put(nombre, datos);
 
@@ -49,6 +66,17 @@ public class Ejercicio_2 {
         }
         System.out.println("Se ha registrado correctamente");
 
+    }
+
+    public static void mostrarParticipantes(String circuito_elegido, Map<String, Map<String, String>> participantes) {
+        for(Map.Entry<String, Map<String, String>> participante : participantes.entrySet()){
+            for(Map.Entry<String, String> datos : participante.getValue().entrySet()) {
+                if (datos.getKey() == "circuito"){
+                    System.out.println(datos.getValue() + " " + circuito_elegido);
+                }
+
+            }
+        }
     }
 
     public static void main (String[] args) {
@@ -62,7 +90,8 @@ public class Ejercicio_2 {
         while(true){
             System.out.println("Elija una opcion:");
             System.out.println("1. Registrar un participante");
-            System.out.println("2. Salir");
+            System.out.println("2. Mostrar Participantes");
+            System.out.println("3. Salir");
             int option = Integer.parseInt(sc.nextLine());
             switch(option){
                 case(1):
@@ -70,13 +99,23 @@ public class Ejercicio_2 {
                     System.out.println("1. Circuito Chico");
                     System.out.println("2. Circuito Medio");
                     System.out.println("3. Circuito Avanzado");
-                    option = Integer.parseInt(sc.nextLine());
-                    switch (option){
-                        case(1):
-                            registrarParticipante(participantes, sc, circuito_chico, circuito_medio, circuito_avanzado,
-                            identificador, option);
-                    }
+                    int circuito = Integer.parseInt(sc.nextLine());
+                    registrarParticipante(participantes, sc, circuito_chico, circuito_medio, circuito_avanzado,
+                                            identificador, circuito);
+                    identificador++;
+                    break;
                 case(2):
+                    System.out.println("Elija los participantes de que circuito");
+                    System.out.println("1. Circuito Chico");
+                    System.out.println("2. Circuito Medio");
+                    System.out.println("3. Circuito Avanzado");
+                    String circuito_elegido = sc.nextLine();
+                    if(circuito_elegido == "1") circuito_elegido = "chico";
+                    if(circuito_elegido == "2") circuito_elegido = "medio";
+                    if(circuito_elegido == "3") circuito_elegido = "avanzado";
+                    mostrarParticipantes(circuito_elegido, participantes);
+                    break;
+                case(3):
                     return;
             }
 
