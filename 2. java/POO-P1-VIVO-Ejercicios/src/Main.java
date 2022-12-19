435763957a6ef9ad80edf62d61af6cefd2fe59d9
Santiago Valdez <santiago.svaldez@mercadolibre.com>
2022-12-19 09:36:33 -0300

    Se finaliza ejercicio de POO

--- 2. java/POO-P1-VIVO-Ejercicios/src/Main.java
+++ 2. java/POO-P1-VIVO-Ejercicios/src/Main.java
@@ -1,6 +1,39 @@
 public class Main {
     public static void main(String[] args) {
-        Persona persona = new Persona("Santiago",23,"5010",80,1.80);
-        System.out.println(persona);
+
+        //Se instancia a el objeto persona. Se "Crea".
+        Persona personaCreada = new Persona();
+
+        //La persona está rellenando un formulario y los datos obligatorios son Nombre, edad y dni.
+        Persona personaRegistrada = new Persona("Pepe", 24, "1234");
+
+        //La persona ya ingreso en el sistema y ahora que quiere realizar una accion se necesita más datos sobre ella.
+        Persona personaFull = new Persona("Santiago", 23, "5010", 80, 1.80);
+
+        System.out.println("Datos completos: ");
+        System.out.println(" -> " + personaFull.toString(personaFull));
+
+        if (personaFull.altura > 0 && personaFull.peso > 0) {
+            System.out.println("IMC: ");
+
+            int Imc = personaFull.calcularImc(personaFull);
+
+            if(Imc == -1)
+            {
+                System.out.println(" * Bajo Peso. ");
+            }
+            else if (Imc == 0)
+            {
+                System.out.println(" * Peso Saludable. ");
+            }
+            else if (Imc == 1)
+            {
+                System.out.println(" * Sobrepeso. ");
+            }
+        }
+
+        System.out.println(" Es mayor de edad? ");
+        System.out.println(" -> " + personaFull.esMayorDeEdad(personaFull));
+
     }
 }
--- 2. java/POO-P1-VIVO-Ejercicios/src/Persona.java
+++ 2. java/POO-P1-VIVO-Ejercicios/src/Persona.java
@@ -1,4 +1,5 @@
 import java.util.Map;
+import java.lang.Math. *;
 
 public class Persona {
     String nombre;
@@ -11,20 +12,55 @@ public class Persona {
     public Persona()
     {
     }
-
-    public void Persona(String nombre, Integer edad, String dni)
+    public Persona(String nombre, Integer edad, String dni)
     {
         this.nombre = nombre;
         this.edad = edad;
         this.dni = dni;
     }
-
     public Persona(String nombre, Integer edad, String dni, double peso, double altura) {
         this.nombre = nombre;
         this.edad = edad;
         this.dni = dni;
         this.peso = peso;
         this.altura = altura;
+    }
+
+    /*
+    cacularIMC(), la fórmula para calcularlo es: peso/(altura^2) - (peso expresado en kg y altura en mts),
+    si este cálculo devuelve un valor menor que 20, la función debe retornar -1, si devuelve un número
+    entre 20 y 25 inclusive para los dos valores, el método debe retornar un 0, por último, si devuelve un
+    número mayor que 25 debe retornar un 1
+    */
+
+    public int calcularImc(Persona p)
+    {
+        //pre: altura y peso deben ser mayor a cero.
+        double Imc = p.peso / Math.pow(p.altura, 2);
+
+        if(Imc < 20)
+        {
+             return -1;
+        }
+        else if (Imc >= 20 && Imc <= 25)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+
+    public boolean esMayorDeEdad(Persona p)
+    {
+        //pre: la persona debe tener seteada la edad.
+        return p.edad > 18;
+    }
+
+    public String toString(Persona p)
+    {
+        return "{ Nombre: " + p.nombre + ", Edad: " + p.edad + ", Dni: " + p.dni + ", Peso: " + p.peso + " }";
     }
 
 }
