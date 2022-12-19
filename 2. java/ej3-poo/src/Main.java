ffedae7b24946a6186308ee49e79562d2e04f8b8
FacundoRuiz00 <facundoandru@mercadolibre.com>
2022-12-19 10:04:09 -0300

    Agregado de prints

--- 2. java/ej3-poo/src/Main.java
+++ 2. java/ej3-poo/src/Main.java
@@ -4,7 +4,34 @@ public class Main {
         Persona p2 = new Persona("Pedro",15,"45886987");
         Persona p3 = new Persona("Juan",25,"89766543",87.9,1.8);
 
-        System.out.println(p3.esMayorDeEdad());
-        System.out.println(p3.cacularIMC());
+
+        System.out.println(mostrarIMC(p1));
+        System.out.println(mostrarMayoriaDeEdad(p1));
     }
+
+    public static String mostrarIMC(Persona persona) {
+        String texto = "El IMC DE" + persona.nombre + " ES: ";
+        switch (persona.calcularIMC()) {
+            case -1:
+                texto += "Bajo peso";
+                break;
+            case 0:
+                texto += "Peso saludable";
+                break;
+            case 1:
+                texto += "Sobrepeso";
+                break;
+            default:
+                texto += "INVÁLIDO";
+                break;
+        }
+
+        return texto;
+    }
+
+    public static String mostrarMayoriaDeEdad(Persona persona) {
+        String texto = persona.nombre + " es ";
+        return persona.esMayorDeEdad() ? texto + "mayor de edad" : texto + "menor de edad";
+    }
+
 }
