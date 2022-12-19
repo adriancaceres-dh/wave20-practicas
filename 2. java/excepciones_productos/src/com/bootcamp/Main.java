6699f3001ff26eaefd7a0938ca2fd0f95881bcde
luis-lopezgomez <luis.lopezgomez@mercadolibre.com.mx>
2022-12-19 10:25:33 -0600

    Ejercicio POO P2 refactors

--- 2. java/excepciones_productos/src/com/bootcamp/Main.java
+++ 2. java/excepciones_productos/src/com/bootcamp/Main.java
@@ -3,6 +3,7 @@ package com.bootcamp;
 public class Main {
     public static void main(String[] args) {
         PracticaExcepciones practica = new PracticaExcepciones();
-        practica.calcularCociente();
+        double cociente = practica.calcularCociente();
+        System.out.println(cociente);
     }
 }
