126ccf00a5597e5e4d83a07b7fa9a20c76e0e554
ale-espindola <alejandra.espindola@mercadolibre.com>
2022-12-20 14:09:46 -0300

    Ejercicio práctica en vivo Vehiculos-Garage

---
+++ 2. java/EjercicioVehiculosGarage/src/com/bootcamp/Main.java
@@ -0,0 +1,71 @@
+package com.bootcamp;
+
+import java.sql.SQLOutput;
+import java.util.Comparator;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        Vehiculo v1 = new Vehiculo("Ford", "Fiesta", 1000);
+        Vehiculo v2 = new Vehiculo("Ford", "Focus", 1200);
+        Vehiculo v3 = new Vehiculo("Ford", "Explorer", 2500);
+        Vehiculo v4 = new Vehiculo("Fiat", "Uno", 500);
+        Vehiculo v5 = new Vehiculo("Fiat", "Cronos", 1000);
+        Vehiculo v6 = new Vehiculo("Fiat", "Torino", 1250);
+        Vehiculo v7 = new Vehiculo("Chevrolet", "Aveo", 1250);
+        Vehiculo v8 = new Vehiculo("Chevrolet", "Spin", 2500);
+        Vehiculo v9 = new Vehiculo("Toyota", "Corola", 1200);
+        Vehiculo v10 = new Vehiculo("Toyota", "Fortuna", 3000);
+        Vehiculo v11 = new Vehiculo("Renault", "Logan", 950);
+
+        Garaje G1 = new Garaje(123);
+
+        G1.agregarNuevoVehiculo(v1);
+        G1.agregarNuevoVehiculo(v2);
+        G1.agregarNuevoVehiculo(v3);
+        G1.agregarNuevoVehiculo(v4);
+        G1.agregarNuevoVehiculo(v5);
+        G1.agregarNuevoVehiculo(v6);
+        G1.agregarNuevoVehiculo(v7);
+        G1.agregarNuevoVehiculo(v8);
+        G1.agregarNuevoVehiculo(v9);
+        G1.agregarNuevoVehiculo(v10);
+        G1.agregarNuevoVehiculo(v11);
+
+        System.out.println("Vehiculos ordenados por precio");
+        List<Vehiculo> ordenadoPorPrecio = G1.getVehiculos().stream()
+                .sorted(Comparator.comparingInt(Vehiculo::getCosto)
+                        .reversed())
+                .collect(Collectors.toList());
+        ordenadoPorPrecio.forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("Vehiculos ordenados por precio y marca");
+        List<Vehiculo> ordenadoPorPrecioYMarca = G1.getVehiculos().stream()
+                .sorted(Comparator.comparingInt(Vehiculo::getCosto)
+                        .reversed())
+                .sorted((x,y) -> x.getMarca().compareToIgnoreCase(y.getMarca()))
+                .collect(Collectors.toList());
+        ordenadoPorPrecioYMarca.forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("Ejercicio filtro precios");
+        List<Vehiculo> menoresA1000 = G1.getVehiculos().stream().filter(x->x.getCosto()<1000).collect(Collectors.toList());
+        List<Vehiculo> mayoresA1000 = G1.getVehiculos().stream().filter(x->x.getCosto()>=1000).collect(Collectors.toList());
+
+        System.out.println("MENORES A 1000");
+        menoresA1000.forEach(System.out::println);
+        System.out.println("MAYORES A 1000");
+        mayoresA1000.forEach(System.out::println);
+
+        System.out.println();
+        System.out.println("PROMEDIO TOTAL");
+        int sum = G1.getVehiculos().stream()
+                .collect(Collectors.summingInt(Vehiculo::getCosto));
+        OptionalDouble sum2 = G1.getVehiculos().stream().mapToDouble(Vehiculo::getCosto).average();
+        System.out.println(sum / G1.getVehiculos().stream().count());
+        System.out.println(sum2);
+    }
+}
---
+++ 2. java/EjercicioVehiculosGarage/src/com/bootcamp/Vehiculo.java
@@ -0,0 +1,49 @@
+package com.bootcamp;
+
+public class Vehiculo {
+    private String modelo;
+    private String marca;
+    private int costo;
+
+    public Vehiculo() {
+    }
+
+    public Vehiculo(String marca, String modelo, int costo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.costo = costo;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" +
+                "modelo='" + modelo + '\'' +
+                ", marca='" + marca + '\'' +
+                ", costo=" + costo +
+                '}';
+    }
+}
