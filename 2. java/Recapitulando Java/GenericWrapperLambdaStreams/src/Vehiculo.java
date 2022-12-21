2958f34eb6367d8091b4b5921bcdb06e973a37f1
joaquinpereyrar <joaquin.pereyrarotulo@mercadolibre.com>
2022-12-21 09:59:26 -0300

    Se agrega el ejercicio integrador 1 de Generic Wrapper Lambda Streams

---
+++ 2. java/Recapitulando Java/GenericWrapperLambdaStreams/src/Vehiculo.java
@@ -0,0 +1,44 @@
+public class Vehiculo {
+    private String modelo;
+    private String marca;
+    private Integer precio;
+
+    public Vehiculo(String marca, String modelo, Integer precio) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.precio = precio;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehiculo{" +
+                "modelo='" + modelo + '\'' +
+                ", marca='" + marca + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+}
