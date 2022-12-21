4f17061fc48272252f48c0f3752a59e79e71f3cc
joaquinpereyrar <joaquin.pereyrarotulo@mercadolibre.com>
2022-12-21 15:23:39 -0300

    Se ajusta el método de comer de la Clase AdaptadorComer para imprima dependiendo del tipo de alimentación del animal

--- 2. java/Clases Abstractas e Interfaces/ClasesAbstractasInterfaces/src/Ejercicio3/Clases/AdaptadorComer.java
+++ 2. java/Clases Abstractas e Interfaces/ClasesAbstractasInterfaces/src/Ejercicio3/Clases/AdaptadorComer.java
@@ -4,12 +4,17 @@ import Ejercicio3.Interfaces.IComer;
 
 public class AdaptadorComer implements IComer {
 
+    private String gustoAlimenticio;
     public AdaptadorComer(String gustoAlimenticio) {
-
+        this.gustoAlimenticio = gustoAlimenticio;
     }
 
     @Override
     public void comer() {
-
+        if(gustoAlimenticio.equals("Carnivoro")){
+            System.out.println("Comiendo carne");
+        } else if (gustoAlimenticio.equals("Herviboro")) {
+            System.out.println("Comiendo hierbas");
+        }
     }
 }
