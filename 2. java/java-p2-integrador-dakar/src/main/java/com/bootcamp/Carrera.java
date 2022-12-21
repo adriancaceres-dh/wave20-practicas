500e020d1d908c49df407f0396321dc520931bd7
gonchibarr <gonzalo.barreiro@mercadolibre.com>
2022-12-21 15:04:32 -0300

    Dakar

---
+++ 2. java/java-p2-integrador-dakar/src/main/java/com/bootcamp/Carrera.java
@@ -0,0 +1,56 @@
+package com.bootcamp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Carrera {
+
+    private double distancia;
+    private double premioDolares;
+    private String nombre;
+    private int cantidadVehiculosPermitidos;
+    private List<Vehiculo> vehiculos;
+
+    public Carrera(double distancia, double premioDolares, String nombre, int cantidadVehiculosPermitidos) {
+        this.distancia = distancia;
+        this.premioDolares = premioDolares;
+        this.nombre = nombre;
+        this.cantidadVehiculosPermitidos = cantidadVehiculosPermitidos;
+        vehiculos = new ArrayList<>();
+    }
+
+    public boolean darDeAltaAuto(double velocidad, double aceleracion, double anguloDeGiro, String patente) {
+        if (vehiculos.size() < cantidadVehiculosPermitidos) {
+            Auto auto = new Auto(velocidad, aceleracion, anguloDeGiro, patente);
+            vehiculos.add(auto);
+            return true;
+        }
+        return false;
+    }
+    public boolean darDeAltaMoto(double velocidad, double aceleracion, double anguloDeGiro, String patente) {
+        if (vehiculos.size() < cantidadVehiculosPermitidos) {
+            Moto moto = new Moto(velocidad, aceleracion, anguloDeGiro, patente);
+            vehiculos.add(moto);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean eliminarVehiculo(Vehiculo vehículo){
+        return vehiculos.remove(vehículo);
+    }
+    public boolean eliminarVehiculoConPatente(String unaPatente){
+        return vehiculos.remove(new Auto(unaPatente));
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder(String.format("Carrera: ** %s **", nombre.toUpperCase()));
+        sb.append("\n");
+        vehiculos.stream().forEach(v -> {
+            sb.append(v.toString());
+            sb.append("\n");
+        });
+        return sb.toString();
+    }
+}
