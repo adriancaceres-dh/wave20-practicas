0bacea248afb9579cf4cd7e2de731e93cb336f5c
andrearuiz-ml <andrea.ruiz@mercadolibre.com>
2022-12-21 13:47:20 -0300

    agencia de viajes - parte 1

---
+++ 2. java/AgenciaDeTurismo/Dominio/src/Productos/BoletoViaje.java
@@ -0,0 +1,10 @@
+package Productos;
+
+public class BoletoViaje extends Producto {
+
+    public BoletoViaje(double precio, String descripcion) {
+        super(precio,descripcion);
+    }
+
+
+}
---
+++ 2. java/AgenciaDeTurismo/Dominio/src/Productos/Comida.java
@@ -0,0 +1,7 @@
+package Productos;
+
+public class Comida extends Producto {
+    public Comida(double precio, String descripcion) {
+        super(precio,descripcion);
+    }
+}
