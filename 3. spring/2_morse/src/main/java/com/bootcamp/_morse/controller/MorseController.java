5d9ea1f8d42c16d81be4dad8876d83fbb52fd3a6
Jose Ivan Drozd <jose.drozd@mercadolibre.com>
2022-12-22 18:03:31 -0300

    Ejercicio morse spring en equipo

---
+++ 3. spring/2_morse/src/main/java/com/bootcamp/_morse/controller/MorseController.java
@@ -0,0 +1,22 @@
+package com.bootcamp._morse.controller;
+
+
+import com.bootcamp._morse.model.ConvertidorMorse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
+
+@RestController
+public class MorseController {
+
+
+    @GetMapping("/{codigo}")
+    public String conversorMorse(@PathVariable String codigo){
+
+        return ConvertidorMorse.convertir(codigo);
+
+    }
+
+}
