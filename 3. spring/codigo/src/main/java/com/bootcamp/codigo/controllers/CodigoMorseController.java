46cf6a4bd7e7644d686774da2e5098d355783954
JuankOrtizR <juancarlos.ortiz@mercadolibre.com.co>
2022-12-22 15:24:15 -0500

    codigo Morse lindo

--- 3. spring/codigo/src/main/java/com/bootcamp/codigo/controllers/CodigoMorseController.java
+++ 3. spring/codigo/src/main/java/com/bootcamp/codigo/controllers/CodigoMorseController.java
@@ -1,2 +1,79 @@
-package com.bootcamp.codigo.controllers;public class CodigoMorse {
+package com.bootcamp.codigo.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/codigo")
+public class CodigoMorseController {
+
+    Map<String, Character> morse = new HashMap<>();
+
+    @GetMapping("convertir/{codigo}")
+    public String convertirATexto(@PathVariable("codigo") String codigo){
+        return convertir(codigo);
+    }
+
+    private String convertir(String codigo) {
+        llenarLista();
+        String vectorString[] = codigo.split("   ");
+        String frase = "";
+
+        for(String item: vectorString){
+            String letras[] = item.split(" ");
+            for (String letra: letras){
+                frase += morse.get(letra);
+            }
+            frase += " ";
+        }
+
+        return frase.toUpperCase();
+    }
+
+    private void llenarLista() {
+        morse.put(".-", 'a');
+        morse.put("-...", 'b');
+        morse.put("-.-.", 'c');
+        morse.put("-..", 'd');
+        morse.put(".", 'e');
+        morse.put("..-.", 'f');
+        morse.put("--.", 'g');
+        morse.put("....", 'h');
+        morse.put("..", 'i');
+        morse.put(".---", 'j');
+        morse.put("-.", 'k');
+        morse.put(".-..", 'l');
+        morse.put("--", 'm');
+        morse.put("-.", 'n');
+        morse.put("---", 'o');
+        morse.put(".--.", 'p');
+        morse.put("--.-", 'q');
+        morse.put(".-.", 'r');
+        morse.put("...", 's');
+        morse.put("-", 't');
+        morse.put("..-", 'u');
+        morse.put("...-", 'v');
+        morse.put(".--", 'w');
+        morse.put("-..-", 'x');
+        morse.put("-.--", 'y');
+        morse.put("--..", 'z');
+        morse.put(".----", '1');
+        morse.put("..---", '2');
+        morse.put("...--", '3');
+        morse.put("....-", '4');
+        morse.put(".....", '5');
+        morse.put("-....", '6');
+        morse.put("--...", '7');
+        morse.put("---..", '8');
+        morse.put("----.", '9');
+        morse.put("-----", '0');
+    }
+
 }
