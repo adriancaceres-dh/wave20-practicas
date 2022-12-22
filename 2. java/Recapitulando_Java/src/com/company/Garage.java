e9b553f6f0562bc0fa8f9bcaa8c5ca10d54bca1f
SoriGonzalez <sorayagonzalez343@gmail.com>
2022-12-22 00:20:59 -0500

    Recapitulando_Java

---
+++ 2. java/Recapitulando_Java/src/com/company/Garage.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garage {
+    int id;
+    List<Vehiculo> vehiculos;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setVehiculos(List<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public Garage(int id, List<Vehiculo> vehiculos) {
+        this.id = id;
+        this.vehiculos = vehiculos;
+    }
+}
---
+++ 2. java/Recapitulando_Java/src/com/company/Vehiculo.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Vehiculo {
+    String modelo;
+    String marca;
+    double costo;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    public Vehiculo(String modelo, String marca, double costo) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.costo = costo;
+    }
+}
