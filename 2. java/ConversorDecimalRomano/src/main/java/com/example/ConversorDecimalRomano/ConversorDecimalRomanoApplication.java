b5ba9e714b98f4b8367f4ada87f9d45b3da42c70
DiegoAlejandroMalagon <diego.malagon@mercadolibre.com.co>
2022-12-22 16:38:20 -0500

    ejercicios con Spring (Conversor romano-decimal y morse-texto)

---
+++ 2. java/ConversorDecimalRomano/src/main/java/com/example/ConversorDecimalRomano/ConversorDecimalRomanoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConversorDecimalRomano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorDecimalRomanoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorDecimalRomanoApplication.class, args);
+	}
+
+}
---
+++ 2. java/ConversorMorseTexto/src/test/java/com/example/ConversorMorseTexto/ConversorMorseTextoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConversorMorseTexto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorMorseTextoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
