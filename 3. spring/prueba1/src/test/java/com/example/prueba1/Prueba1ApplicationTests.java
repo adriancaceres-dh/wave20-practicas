990e7fb5c49282e77439515c17c62e55e8e21bd1
andrearuiz-ml <andrea.ruiz@mercadolibre.com>
2022-12-22 13:11:06 -0300

    spring hola mundo

---
+++ 3. spring/prueba1/src/test/java/com/example/prueba1/Prueba1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prueba1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
