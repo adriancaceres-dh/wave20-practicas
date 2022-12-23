bd54b5ac857c87fa6b905443d9c93beb1b39ee6c
NathamgML <nathalia.montero@mercadolibre.com.co>
2022-12-23 11:24:47 -0500

    Spring Roman Numbers exercise

---
+++ 3. spring/1.Ejercicio de Romanos/demospring/src/main/java/com/bootcampspring/demospring/controller/RomanNumberController.java
@@ -0,0 +1,20 @@
+package com.bootcampspring.demospring.controller;
+
+import com.bootcampspring.demospring.service.RomanNumberService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RomanNumberController {
+    @GetMapping(value ="/greeting/{name}")
+    public String greeting( @PathVariable String name){
+        return "Hello "+ name +" <3";
+    }
+
+    @GetMapping(value ="/arabic-to-roman/{number}")
+    public String greeting( @PathVariable Integer number){
+        RomanNumberService romanNumberService= new RomanNumberService();
+        return romanNumberService.romanNumber(number);
+    }
+}
---
+++ 3. spring/1.Ejercicio de Romanos/demospring/src/test/java/com/bootcampspring/demospring/DemospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcampspring.demospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
