41cbcc99ab07e567a261fe15708d4a3aac31dff1
SoriGonzalez <sorayagonzalez343@gmail.com>
2022-12-22 21:48:58 -0500

    Numeros Romanos

---
+++ 3. spring/conv_romanos/src/main/java/com/bootcamp/conv_romanos/ConvRomanosApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.conv_romanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvRomanosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvRomanosApplication.class, args);
+	}
+
+}
---
+++ 3. spring/conv_romanos/src/test/java/com/bootcamp/conv_romanos/ConvRomanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.conv_romanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvRomanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
