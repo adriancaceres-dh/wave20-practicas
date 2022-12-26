9fc8d7780616dc3a42fb8d21fb7ec1c375c80638
Cindy Ortega Palma <cindy.ortega@mercadolibre.com>
2022-12-26 16:00:54 -0300

    ejercicio de distribuidora

---
+++ 2. java/POO/DistribuidoraDeProductos/src/com/bootcamp/Producto.java
@@ -0,0 +1,32 @@
+package com.bootcamp;
+
+public class Producto {
+    private String nombre;
+    private double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    double calcular(int cantidadDeProductos){
+        double total = this.precio * cantidadDeProductos;
+        return total;
+    }
+}
