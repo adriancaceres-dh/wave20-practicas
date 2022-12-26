aaeb0b22b1e08b74538654ff2084d179ef35985e
DianapParraE <diana.parra@mercadolibre.com.co>
2022-12-25 22:32:57 -0500

    Mi primer proyecto

---
+++ 3. spring/pruebaspring/src/main/java/com/miprimerproyecto/pruebaspring/HelloRestController.java
@@ -0,0 +1,14 @@
+package com.miprimerproyecto.pruebaspring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+    @GetMapping("/{name}")
+    public String sayHello(@PathVariable String name){
+        return "Hello World " + name;
+    }
+}
