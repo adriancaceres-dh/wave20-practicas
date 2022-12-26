a9e55e384d1ca9ae04e95ab882ddab6b7b88454a
Cindy Ortega Palma <cindy.ortega@mercadolibre.com>
2022-12-26 13:04:24 -0300

    nuevo ejercicio de Practicaexcepciones

---
+++ 2. java/POO/PracticaExcepciones/src/com/bootcamp/Main.java
@@ -0,0 +1,12 @@
+package com.bootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+
+        PracticaExcep practica = new PracticaExcep();
+
+        practica.Cociente();
+
+        System.out.println("Fin del programa");
+    }
+}
