b905cf3bae1ad4184d3d5c837b5463b9ee0b6ec0
IvanGonWM <ivan.amaro@mercadolibre.com>
2022-12-26 14:58:10 -0300

    DTO - Ejercicio Star Wars

---
+++ 3. spring/DTO - Ejercicio Star Wars/entity/Personaje.java
@@ -0,0 +1,17 @@
+package com.bootcamp.prueba.entity;
+
+import lombok.Data;
+
+@Data
+public class Personaje {
+    private String name;
+    private String height;
+    private String mass;
+    private String hairColor;
+    private String skinColor;
+    private String eyeColor;
+    private String birthYear;
+    private String gender;
+    private String homeworld;
+    private String species;
+}
