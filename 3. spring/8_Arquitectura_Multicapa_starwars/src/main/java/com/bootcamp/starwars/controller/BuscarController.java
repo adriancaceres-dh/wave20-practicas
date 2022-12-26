7f7180ad921518e89547068b0b2cdd9a4dd77ac8
manueldiazmeli <manuel.diaz@mercadolibre.com.co>
2022-12-26 14:38:43 -0500

    Cambios ejercicio StarWars

--- 3. spring/8_Arquitectura_Multicapa_starwars/src/main/java/com/bootcamp/starwars/controller/BuscarController.java
+++ 3. spring/8_Arquitectura_Multicapa_starwars/src/main/java/com/bootcamp/starwars/controller/BuscarController.java
@@ -3,6 +3,7 @@ package com.bootcamp.starwars.controller;
 import com.bootcamp.starwars.dto.response.PersonajeResponseDto;
 import com.bootcamp.starwars.service.BuscarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,7 @@ public class BuscarController {
     private BuscarService buscarService;
 
     @GetMapping("/{query}")
-    public List<PersonajeResponseDto> find(@PathVariable String query) {
-        return buscarService.find(query);
+    public ResponseEntity<List<PersonajeResponseDto>> find(@PathVariable String query) {
+        return ResponseEntity.ok(buscarService.find(query));
     }
 }
