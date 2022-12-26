cc54fa67feb1accd0a4b12762be6787206002b18
davnunezq <david.nunez@mercadolibre.com.co>
2022-12-26 09:05:16 -0500

    ejercicio StarWars-arquitectura multicapa

---
+++ 3. spring/starwars/src/main/java/com/example/starwars/controller/StarwarsController.java
@@ -0,0 +1,28 @@
+package com.example.starwars.controller;
+
+import com.example.starwars.dto.CharacterDTO;
+import com.example.starwars.service.StarWarsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StarwarsController {
+    StarWarsService starWarsService;
+    public StarwarsController() {
+        starWarsService=new StarWarsService();
+    }
+
+    @GetMapping("/personajes")
+    public ResponseEntity<List<CharacterDTO>> searchCharacter(@RequestParam String name){
+
+        return new ResponseEntity<>(starWarsService.consulta(name), HttpStatus.ACCEPTED);
+    }
+
+}
