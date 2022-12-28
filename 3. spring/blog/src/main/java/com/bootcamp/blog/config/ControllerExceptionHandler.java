a69ed9976e5810b9faf327dce810637baafa97e1
luis-lopezgomez <luis.lopezgomez@mercadolibre.com.mx>
2022-12-28 11:41:39 -0600

    Ejercicio Blog

--- 3. spring/blog/src/main/java/com/bootcamp/blog/config/ControllerExceptionHandler.java
+++ 3. spring/blog/src/main/java/com/bootcamp/blog/config/ControllerExceptionHandler.java
@@ -7,8 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 
-@ControllerAdvice
+@ControllerAdvice(annotations = RestController.class)
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(IdNotFoundException.class)
