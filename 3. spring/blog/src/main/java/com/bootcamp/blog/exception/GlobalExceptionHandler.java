5cc88212d06a7ecea76126a8a9511c4e7bbe5ead
MatiasSegoviaML <matiasfederico.segovia@mercadolibre.com>
2022-12-28 12:44:59 -0300

    blog

---
+++ 3. spring/blog/src/main/java/com/bootcamp/blog/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.bootcamp.blog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> notFoundException(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new MessageExceptionDto(e.getMessage()));
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<?> badRequestException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new MessageExceptionDto(e.getMessage()));
+    }
+}
