82fa22ba394802cc8795a812ef182277037d2532
ttrivelli1 <tammara.trivelli@mercadolibre.com>
2022-12-29 17:30:59 -0300

    Se agrega los @ que faltaban en los dto para los getters, setters, constructor y tostring

--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/dto/ProductTwoWeeksResponseDTO.java
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/dto/ProductTwoWeeksResponseDTO.java
@@ -1,8 +1,14 @@
 package com.bootcamp.be_java_hisp_w20_g4.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.util.List;
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ProductTwoWeeksResponseDTO {
     private Integer user_id;
     private List<PostDTO> postDTOList;
