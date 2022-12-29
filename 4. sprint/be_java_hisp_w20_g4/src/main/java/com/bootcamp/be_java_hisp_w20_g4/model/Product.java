f9571a2a842a4e07b68cc3e42753d8bbda0d10c0
ttrivelli1 <tammara.trivelli@mercadolibre.com>
2022-12-29 17:23:25 -0300

    Se agrega la lista de produtos en el respositorio crrespondiente y los @ que faltaban en la case Product

--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/model/Product.java
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/model/Product.java
@@ -1,5 +1,14 @@
 package com.bootcamp.be_java_hisp_w20_g4.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
     private int id;
     private String productName;
--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/repository/ProductRepository.java
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/repository/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.bootcamp.be_java_hisp_w20_g4.repository;
 
+import com.bootcamp.be_java_hisp_w20_g4.model.Product;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class ProductRepository implements IProductRepository{
+
+    private List<Product> products;
+
+
 
 
 }
