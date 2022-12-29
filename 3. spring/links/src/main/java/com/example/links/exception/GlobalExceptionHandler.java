aa17f5216d7fabb6d611046f390149c9fed56e00
Esteban Demarchi <esteban.demarchi@mercadolibre.com>
2022-12-29 10:43:48 -0300

    Add exception to global handler
    to handle wrong path variables

--- 3. spring/links/src/main/java/com/example/links/exception/GlobalExceptionHandler.java
+++ 3. spring/links/src/main/java/com/example/links/exception/GlobalExceptionHandler.java
@@ -2,9 +2,11 @@ package com.example.links.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
@@ -13,6 +15,12 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> NotFoundException(Exception e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new MessageException(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<?> invalidArgumentException(Exception e) {
+        return ResponseEntity.badRequest()
+                .body(new MessageException("Wrong path variable"));
     }
 
     @ExceptionHandler(BadRequestException.class)
