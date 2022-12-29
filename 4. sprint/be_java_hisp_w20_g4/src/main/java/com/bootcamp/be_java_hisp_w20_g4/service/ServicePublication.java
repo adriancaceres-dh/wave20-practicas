d4c2f6cd88743b3315a5552d4a9aeeb302b8de4d
ttrivelli1 <tammara.trivelli@mercadolibre.com>
2022-12-29 17:12:32 -0300

    Se agrega inyeccion de codigo en los services para que pueda inicializar los repositorios

--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/service/ServicePublication.java
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/service/ServicePublication.java
@@ -1,7 +1,18 @@
 package com.bootcamp.be_java_hisp_w20_g4.service;
 
+import com.bootcamp.be_java_hisp_w20_g4.repository.IProductRepository;
+import com.bootcamp.be_java_hisp_w20_g4.repository.IPublicationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServicePublication implements  IServicePublication{
+
+    @Autowired
+    IPublicationRepository publicationRepository;
+
+    @Autowired
+    IProductRepository productRepository;
+
+
 }
--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/service/ServiceUsr.java
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/service/ServiceUsr.java
@@ -1,7 +1,14 @@
 package com.bootcamp.be_java_hisp_w20_g4.service;
 
+import com.bootcamp.be_java_hisp_w20_g4.repository.IPublicationRepository;
+import com.bootcamp.be_java_hisp_w20_g4.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ServiceUsr implements IServiceUsr {
+
+    @Autowired
+    IUserRepository userRepository;
+
 }
