a888daa92ce1722dca05167f83718dd97710cc42
ttrivelli1 <tammara.trivelli@mercadolibre.com>
2022-12-29 17:32:33 -0300

    Cambio de importacion de una clase del dto

--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/dto/PostDTO.java
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/dto/PostDTO.java
@@ -1,11 +1,13 @@
 package com.bootcamp.be_java_hisp_w20_g4.dto;
 
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.ProductDTO;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
