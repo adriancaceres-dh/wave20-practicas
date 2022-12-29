415b48047a3cb345880cb6ccc01a4403e3df98de
itornati <ignacio.tornati@mercadolibre.com>
2022-12-29 12:58:20 -0500

    Recapitulando Spring

---
+++ 3. spring/concesionariaApp/src/main/java/com/concesionaria/concesionariaApp/ConcesionariaAppApplication.java
@@ -0,0 +1,13 @@
+package com.concesionaria.concesionariaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesionariaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesionariaAppApplication.class, args);
+	}
+
+}
---
+++ 3. spring/concesionariaApp/src/test/java/com/concesionaria/concesionariaApp/ConcesionariaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concesionaria.concesionariaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesionariaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
