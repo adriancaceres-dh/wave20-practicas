94333b7409ba07fa9c2dd52c7c1e6127194fe5a6
CabreraRodMeli <rodrigo.cabrera@mercadolibre.com>
2022-12-29 17:32:23 -0300

    mv de un dto a response

--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/dto/response/ProductTwoWeeksResponseDTO.java
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/dto/response/ProductTwoWeeksResponseDTO.java
@@ -1,4 +1,6 @@
-package com.bootcamp.be_java_hisp_w20_g4.dto;
+package com.bootcamp.be_java_hisp_w20_g4.dto.response;
+
+import com.bootcamp.be_java_hisp_w20_g4.dto.PostDTO;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
