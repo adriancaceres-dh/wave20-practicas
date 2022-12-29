10a0c965a6e83026d67cea4c1c5028a417db8197
manueldiazmeli <manuel.diaz@mercadolibre.com.co>
2022-12-29 17:54:52 -0500

    US 0001 Funcion para hacer Follow - Fix busqueda existencia

--- 4. sprint/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/service/FollowService.java
+++ 4. sprint/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/service/FollowService.java
@@ -5,6 +5,8 @@ import com.bootcamp.be_java_hisp_w20_g7.exception.FollowAlreadyExistException;
 import com.bootcamp.be_java_hisp_w20_g7.repository.FollowRepository;
 import com.bootcamp.be_java_hisp_w20_g7.repository.IFollowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,11 +19,13 @@ public class FollowService implements IFollowService{
     public String follow(int userIdFollower, int userIdFollowed) {
         Follow follow = new Follow(userIdFollower,userIdFollowed);
         boolean exists = alreadyExists(follow);
-        if(!exists){
+        if(exists){
+            throw new FollowAlreadyExistException("No se puede seguir al usuario con id: " + userIdFollowed +" porque ya se esta siguiendo");
+        }else if (userIdFollower == userIdFollowed){
+            throw new FollowAlreadyExistException("El usuario no se puede seguir a si mismo");
+        }else{
             followRepository.save(follow);
             return "El usuario con id: " + userIdFollower + " empezo a seguir al usuario con id: " + userIdFollowed;
-        }else{
-            throw new FollowAlreadyExistException("No se puede seguir al usuario con id: " + userIdFollowed +" porque ya se esta siguiendo");
         }
     }
 
@@ -32,7 +36,6 @@ public class FollowService implements IFollowService{
 
     public boolean alreadyExists(Follow follow){
         return followRepository.findAll().stream()
-                .map(f -> f.equals(follow))
-                .findFirst().orElse(false);
+                .anyMatch(f -> f.equals(follow));
     }
 }
