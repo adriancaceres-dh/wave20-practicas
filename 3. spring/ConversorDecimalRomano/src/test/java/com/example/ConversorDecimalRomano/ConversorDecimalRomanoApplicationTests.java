835c9e3b1c36bf07f977a87c602fa9e7a45443d9
DiegoAlejandroMalagon <diego.malagon@mercadolibre.com.co>
2022-12-31 18:21:07 -0500

    Correcion carpeta destino

---
+++ 3. spring/ConversorDecimalRomano/src/test/java/com/example/ConversorDecimalRomano/ConversorDecimalRomanoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConversorDecimalRomano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorDecimalRomanoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ 3. spring/ConversorMorseTexto/src/main/java/com/example/ConversorMorseTexto/ConversorMorseTextoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ConversorMorseTexto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorMorseTextoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorMorseTextoApplication.class, args);
+	}
+
+}
