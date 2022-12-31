75868e38afab72f0fa24be779fca8e07b50b4787
DiegoAlejandroMalagon <diego.malagon@mercadolibre.com.co>
2022-12-31 13:58:53 -0500

    Eliminando archivos inutilizados

--- 2. java/ClasesAbstractasSeriesNumericas/src/Main.java
+++
@@ -1,9 +0,0 @@
-import serieNumerica.Impares;
-
-public class Main {
-    public static void main(String[] args){
-        Impares impar = new Impares();
-        impar.iniciarSerie(1);
-        System.out.println(impar.numSiguiente());
-    }
-}
