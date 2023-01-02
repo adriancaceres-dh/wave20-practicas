0ca41edf75fbf3eb65c59379c490962eaf0270f3
ramosjpruiz <juanpablo.ramos@mercadolibre.com>
2023-01-02 10:05:49 -0300

    Agrega dependencia y Bean de ModelMapper, inicializa repositorio usuarios

--- 4. sprint/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/BeJavaHispW20G1Application.java
+++ 4. sprint/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/BeJavaHispW20G1Application.java
@@ -1,13 +1,20 @@
 package com.bootcamp.be_java_hisp_w20_g1;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BeJavaHispW20G1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeJavaHispW20G1Application.class, args);
-	}
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeJavaHispW20G1Application.class, args);
+    }
 
 }
--- 4. sprint/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/repository/UserRepository.java
+++ 4. sprint/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/repository/UserRepository.java
@@ -1,8 +1,45 @@
 package com.bootcamp.be_java_hisp_w20_g1.repository;
 
+import com.bootcamp.be_java_hisp_w20_g1.model.User;
 import com.bootcamp.be_java_hisp_w20_g1.repository.interfaces.IUserRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 @Repository
 public class UserRepository implements IUserRepository {
+
+    private List<User> userRepository;
+
+    public UserRepository() {
+        this.userRepository = buildUserRepository();
+
+    }
+
+    List<User> buildUserRepository() {
+        File file = null;
+        try {
+            file = ResourceUtils.getFile("classpath:users.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<User>> typeRef = new TypeReference<>() {};
+        List<User> users = null;
+        try {
+            users = objectMapper.readValue(file, typeRef);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(users);
+        return users;
+    }
 }
