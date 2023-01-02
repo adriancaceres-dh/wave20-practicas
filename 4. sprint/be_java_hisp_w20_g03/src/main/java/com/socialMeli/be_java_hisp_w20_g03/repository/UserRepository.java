705d65cf53884881550767ed6ce6e60701ab731b
efruizz <efrain.ruiz@mercadolibre.com>
2023-01-02 10:20:07 -0300

    Add requirement US 008 for US 004

--- 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/repository/UserRepository.java
+++ 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/repository/UserRepository.java
@@ -16,12 +16,11 @@ public class UserRepository implements IUserRepository {
     public UserRepository() {
         this.userList = new ArrayList<>();
 
-        userList.add(new User(234, "vendedor1", new ArrayList<User>(), List.of(
+        userList.add(new User(234, "vendedor1", new ArrayList<User>(), new ArrayList<>()));
+        userList.add(new User(4698, "usuario1", new ArrayList<User>(), List.of(
                 new User(123,"vendedor4", new ArrayList<User>(), new ArrayList<User>()),
                 new User(781,"usuario4", new ArrayList<User>(), new ArrayList<User>()),
-                new User(6631,"vendedor3", new ArrayList<User>(), new ArrayList<User>())
-        )));
-        userList.add(new User(4698, "usuario1", new ArrayList<User>(), new ArrayList<User>()));
+                new User(6631,"vendedor3", new ArrayList<User>(), new ArrayList<User>()))));
         userList.add(new User(1536, "usuario2", new ArrayList<User>(), new ArrayList<User>()));
         userList.add(new User(2236, "usuario3", new ArrayList<User>(), new ArrayList<User>()));
         userList.add(new User(6932, "vendedor2", new ArrayList<User>(), new ArrayList<User>()));
