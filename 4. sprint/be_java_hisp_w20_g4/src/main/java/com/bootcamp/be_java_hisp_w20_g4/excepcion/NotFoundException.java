153e2d4b9a7b1f517421eed65ed6fb8b34805559
Gabriela Stocco <gabriela.stocco@mercadolibre.com>
2023-01-02 09:43:50 -0300

    agregando exceptions

---
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/excepcion/NotFoundException.java
@@ -0,0 +1,9 @@
+//package main.java.com.bootcamp.be_java_hisp_w20_g4.excepcion;
+package com.bootcamp.be_java_hisp_w20_g4.excepcion;
+public class NotFoundException extends RuntimeException{
+    public NotFoundException() {
+    }
+    public NotFoundException(String message){
+       super(message);
+    }
+}
