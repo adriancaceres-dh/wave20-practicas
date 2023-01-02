fd7b485a1f71ce955481e651628ceba0a09372e8
Esteban Demarchi <esteban.demarchi@mercadolibre.com>
2023-01-02 09:34:06 -0300

    Eliminar anotaciones en dto abstracto

--- 4. sprint/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/dto/response/UserResponseBaseDto.java
+++ 4. sprint/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/dto/response/UserResponseBaseDto.java
@@ -1,19 +1,39 @@
 package com.bootcamp.be_java_hisp_w20_g1.dto.response;
 
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public abstract class UserResponseBaseDto {
     private int userId;
     private String userName;
+
+    public UserResponseBaseDto(int userId, String userName) {
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    public UserResponseBaseDto() {
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserResponseBaseDto{" +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                '}';
+    }
 }
--- 4. sprint/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/dto/response/UserResponseDto.java
+++ 4. sprint/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/dto/response/UserResponseDto.java
@@ -4,11 +4,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.*;
 
-@EqualsAndHashCode(callSuper = true)
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserResponseDto extends UserResponseBaseDto {
 
