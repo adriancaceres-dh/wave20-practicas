138bf683f11ca32e7c96a1a8478a071641247cf2
Diego Fernando Alderete <diego.alderete@mercadolibre.com>
2023-01-02 14:55:27 -0300

    Subo la actualización de UserController y UserRespondeDTO

--- 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/controller/UserController.java
+++ 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/controller/UserController.java
@@ -1,33 +1,23 @@
 package com.bootcamp.be_java_hisp_w20_g2.controller;
 
-
-import com.bootcamp.be_java_hisp_w20_g2.dto.response.UserFollowersResponseDTO;
-import com.bootcamp.be_java_hisp_w20_g2.model.User;
-import com.bootcamp.be_java_hisp_w20_g2.service.UserService;
+import com.bootcamp.be_java_hisp_w20_g2.dto.response.UserResponseDTO;
 import com.bootcamp.be_java_hisp_w20_g2.service.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController()
+@RequestMapping("/users")
 public class UserController {
     @Autowired
-    private UserService userService;
-    public UserController(){
-
+    private IUserService userService;
+    @PostMapping("/{userId}/follow/{userIdToFollow}")
+    public ResponseEntity<Boolean> follow(@PathVariable int userId, @PathVariable int userIdToFollow){
+        return new ResponseEntity<>(userService.follow(userId, userIdToFollow), HttpStatus.OK);
     }
-
-    @GetMapping("/users/{userId}/followers/list")
-    public ResponseEntity<?> getFollowersById(@PathVariable int userId) {
-        UserFollowersResponseDTO userFollowersResponseDTO = userService.findAllFollowers(userId);
-        return new ResponseEntity<>(userFollowersResponseDTO, HttpStatus.OK);
-
-        //if(userFollowersResponseDTO!=null) return new ResponseEntity<>(userFollowersResponseDTO, HttpStatus.OK);
-        //return new ResponseEntity<>(null, HttpStatus.NOT_FOUND); //Se maneja con excepciones, ahora para que no me tire error el IDE
-
+    @GetMapping("/{userId}/followers/count")
+    public ResponseEntity<UserResponseDTO> lista (@PathVariable int userId){
+        return new ResponseEntity<>(userService.followerList(userId), HttpStatus.OK);
     }
-
 }
