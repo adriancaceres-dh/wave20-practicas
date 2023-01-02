a9ae89803365c012c120efc60154d3301894797d
Emanoide47 <emanuel.fonseca@mercadolibre.com>
2023-01-02 12:31:43 -0300

    Se modifica la implementación de lista users, dentro de UserRepository, para HashMap<Integer, User>.. Desarollo de CategoryRepository con algunos datos de prueba.Desarrollo de postRespository más específico con último avance de US0005 y US0006

--- 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/repository/PostRepository.java
+++ 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/repository/PostRepository.java
@@ -1,7 +1,28 @@
 package com.bootcamp.be_java_hisp_w20_g2.repository;
 
+import com.bootcamp.be_java_hisp_w20_g2.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
 
 @Repository
 public class PostRepository {
+    @Autowired
+    private ProductRepository productRepository;
+    private final HashMap<Integer, Post> posts; // Se crea por tema de búsqueda, de actualización y eliminación.
+    private static int currentId = 0;
+
+    public PostRepository(){
+        posts = new HashMap<>();
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == null) {
+            post.setId(currentId++);
+        }
+        productRepository.save(post.getProduct());
+        posts.put(post.getId(), post);
+        return post;
+    }
 }
--- 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/repository/ProductRepository.java
+++ 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/repository/ProductRepository.java
@@ -1,7 +1,28 @@
 package com.bootcamp.be_java_hisp_w20_g2.repository;
 
+import com.bootcamp.be_java_hisp_w20_g2.model.Product;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
 
 @Repository
 public class ProductRepository {
+    private final HashMap<Integer, Product> products;
+    private static int currentId = 0;
+
+    public ProductRepository(){
+        products = new HashMap<>();
+    }
+
+    public Product save(Product product) {
+        if (product.getId() == null) {
+            product.setId(currentId++);
+        }
+        products.put(product.getId(), product);
+        return product;
+    }
+
+    public Product findOne(int id) {
+        return products.get(id);
+    }
 }
