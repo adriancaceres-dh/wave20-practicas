578c9f1bcd94ecf702492748ac66f841d64cce47
lpodio <lorenzo.podio@mercadolibre.com>
2023-01-02 15:00:23 -0300

    US 0007: implementación del endpoint para dejar de seguir a un usario en Usercontroller

--- 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/controller/UserController.java
+++ 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/controller/UserController.java
@@ -2,14 +2,13 @@ package com.bootcamp.be_java_hisp_w20_g2.controller;
 
 
 import com.bootcamp.be_java_hisp_w20_g2.dto.response.UserFollowersResponseDTO;
-import com.bootcamp.be_java_hisp_w20_g2.model.User;
 import com.bootcamp.be_java_hisp_w20_g2.service.UserService;
-import com.bootcamp.be_java_hisp_w20_g2.service.interfaces.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -28,6 +27,12 @@ public class UserController {
         //if(userFollowersResponseDTO!=null) return new ResponseEntity<>(userFollowersResponseDTO, HttpStatus.OK);
         //return new ResponseEntity<>(null, HttpStatus.NOT_FOUND); //Se maneja con excepciones, ahora para que no me tire error el IDE
 
+    }
+
+    @PostMapping("/users/{userId}/unfollow/{userIdToUnfollow}")
+    public ResponseEntity<String> postUnfollow(@PathVariable int userId, @PathVariable int userIdToUnfollow) {
+        userService.unfollowUser(userId, userIdToUnfollow);
+        return ResponseEntity.ok().body("Operación exitosa");
     }
 
 }
