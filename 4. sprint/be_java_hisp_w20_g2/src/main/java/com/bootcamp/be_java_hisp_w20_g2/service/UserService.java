2e51754d00e6fad6644e9b1ecd0ac78a53297ce9
lpodio <lorenzo.podio@mercadolibre.com>
2023-01-02 14:57:15 -0300

    US 0007: implementacion y sobreescritura del metodo unfollowUser en el servicio de usuario

--- 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/service/UserService.java
+++ 4. sprint/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/service/UserService.java
@@ -3,6 +3,7 @@ package com.bootcamp.be_java_hisp_w20_g2.service;
 
 import com.bootcamp.be_java_hisp_w20_g2.dto.response.UserFollowersResponseDTO;
 import com.bootcamp.be_java_hisp_w20_g2.dto.response.UserResponseDTO;
+import com.bootcamp.be_java_hisp_w20_g2.exception.BadRequestException;
 import com.bootcamp.be_java_hisp_w20_g2.exception.UserNotFoundException;
 import com.bootcamp.be_java_hisp_w20_g2.model.User;
 import com.bootcamp.be_java_hisp_w20_g2.repository.UserRepository;
@@ -11,8 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -45,6 +44,40 @@ public class UserService implements IUserService {
     @Override
     public void saveUser(User user){
         userRepository.save(user);
+    }
+
+    @Override
+    public void unfollowUser(int userId, int userIdToUnfollow) {
+        User user = userRepository.findOne(userId);
+        User userToUnfollow = userRepository.findOne(userIdToUnfollow);
+
+        //Checkeo que existan los usuarios, si alguno no existe lanzo la excepcion correspondiente
+        if(user == null) {
+            throw new BadRequestException("Usuario no encontrado");
+        }
+        if(userToUnfollow == null){
+            throw new BadRequestException("Usuario a dejar de seguir no encontrado");
+        }
+
+        // Listas Auxiliares
+        List<User> userFollowing = user.getFollowing();
+        List<User> userFollowers = userToUnfollow.getFollowers();
+
+        //Checkeo que las listas no esten vacias, lanzo excepcion en caso que lo estén
+        if (userFollowing.size() == 0){
+            throw new BadRequestException("El usuario no sigue a nadie");
+        }
+        if(userFollowers.size() == 0){
+            throw new BadRequestException("El usuario no tiene seguidores");
+        }
+
+        //Si las listan no están vacias hago la modificación correspondiente
+        user.removeFollowing(userToUnfollow);
+        userToUnfollow.removeFollower(user);
+
+        //Actualizo los usuarios en el repo con los cambios hechos
+        userRepository.save(user);
+        userRepository.save(userToUnfollow);
     }
 
 }
