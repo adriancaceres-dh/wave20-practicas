6f8e2c9da0ec12891b76290ef813d90d4ec4dd48
davnunezq <david.nunez@mercadolibre.com.co>
2023-01-03 10:53:01 -0500

    correccion US0006 filtrar ultimas dos publicaciones

--- 4. sprint/be_java_hisp_w20_g6_Quintero/src/main/java/com/bootcamp/be_java_hisp_w20_g6/service/Implement/PostServiceImpl.java
+++ 4. sprint/be_java_hisp_w20_g6_Quintero/src/main/java/com/bootcamp/be_java_hisp_w20_g6/service/Implement/PostServiceImpl.java
@@ -1,12 +1,12 @@
 package com.bootcamp.be_java_hisp_w20_g6.service.Implement;
 
 import java.time.LocalDate;
-import java.time.Period;
+
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import com.bootcamp.be_java_hisp_w20_g6.dto.response.PostListResponseDTO;
 import com.bootcamp.be_java_hisp_w20_g6.dto.response.PostResponseDTO;
@@ -15,14 +15,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bootcamp.be_java_hisp_w20_g6.dto.request.PostRequestDto;
-import com.bootcamp.be_java_hisp_w20_g6.exception.UserExistsException;
-import com.bootcamp.be_java_hisp_w20_g6.exception.UserNotFoundException;
+
 import com.bootcamp.be_java_hisp_w20_g6.model.PostModel;
 import com.bootcamp.be_java_hisp_w20_g6.repository.PostRepository;
 import com.bootcamp.be_java_hisp_w20_g6.service.Interface.IPostService;
 import com.bootcamp.be_java_hisp_w20_g6.service.Interface.IUserService;
 
 import org.modelmapper.config.Configuration;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 public class PostServiceImpl implements IPostService {
@@ -61,7 +62,7 @@ public class PostServiceImpl implements IPostService {
         List<PostResponseDTO> followedPost=new ArrayList<>();
         for(int id : userService.getUserById(user_id).getFollowed()){
             postRepository.getPostList().stream().filter(p->p.getUser_id()==id)
-                    .filter(p-> Period.between(p.getDate(),dateNow).getDays()<=15)
+                    .filter(p-> DAYS.between(p.getDate(),dateNow)<=15)
                     .forEach(p->followedPost.add(
                             new PostResponseDTO(p.getUser_id(),p.getId(),p.getDate()
                                     ,p.getProduct(),p.getCategory(),p.getPrice())
