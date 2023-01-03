085a5e4adc15239f599b694d92f07e5930b10d3a
Franco Alvarez <franco.alvarez@mercadolibre.cl>
2023-01-03 11:51:12 -0300

    actualizando DTO con constructores

--- 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/UserFollowerCountDTO.java
+++ 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/UserFollowerCountDTO.java
@@ -1,8 +1,12 @@
 package com.socialMeli.be_java_hisp_w20_g03.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserFollowerCountDTO {
     private int user_id;
     private String user_name;
