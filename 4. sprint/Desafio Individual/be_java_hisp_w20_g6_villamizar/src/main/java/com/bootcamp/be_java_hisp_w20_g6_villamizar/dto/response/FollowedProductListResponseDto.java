2779b640325aa5b56eb12b1e9c2b5604600b6e00
Daniela Villamizar Mendoza <daniela.villamizar@mercadolibre.com.co>
2023-01-04 17:06:50 -0500

    Eliminacio nde version anterior - proyecto duplicado

--- 4. sprint/Desafio Individual/be_java_hisp_w20_g6_villamizar/src/main/java/com/bootcamp/be_java_hisp_w20_g6_villamizar/dto/response/FollowedProductListResponseDto.java
+++
@@ -1,4 +0,0 @@
-package com.bootcamp.be_java_hisp_w20_g6_villamizar.dto.response;
-
-public class FollowedProductListResponseDto {
-}
--- 4. sprint/Desafio Individual/be_java_hisp_w20_g6_villamizar/src/main/java/com/bootcamp/be_java_hisp_w20_g6_villamizar/exception/FollowerNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.bootcamp.be_java_hisp_w20_g6_villamizar.exception;
-
-public class FollowerNotFoundException extends RuntimeException{
-
-    public FollowerNotFoundException(String message){
-        super(message);
-    }
-}
