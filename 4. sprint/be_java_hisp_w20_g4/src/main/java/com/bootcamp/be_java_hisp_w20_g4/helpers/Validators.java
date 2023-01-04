a2368afc4756e70c69f20087104963199c38190b
IvanMajewski1 <ivan.majewski@mercadolibre.com>
2023-01-04 11:34:15 -0300

    Agrego comentarios a los helpers

--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/helpers/Validators.java
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/helpers/Validators.java
@@ -6,9 +6,18 @@ import com.bootcamp.be_java_hisp_w20_g4.model.User;
 
 public class Validators {
 
+    /**
+     * Este método controla que el String recibido cumpla las condiciones requeridas
+     * @param order - Es el String que especifica el orden
+     */
     public static void isValidOrder(String order){
         if (! ((order == null) || order.toLowerCase().equals("name_asc") || order.toLowerCase().equals("name_desc"))) throw new BadRequestException("Parametro de orden incorrecto");
     }
+
+    /**
+     * Este método controla que el String recibido cumpla las condiciones requeridas
+     * @param order - Es el String que especifica el orden
+     */
     public static void isValidDateOrder(String order) {
         if (! ((order == null) || order.toLowerCase().equals("date_asc") || order.toLowerCase().equals("date_desc"))) throw new BadRequestException("Parametro de orden incorrecto");
     }
--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/helpers/publication/PublicationValidator.java
+++
@@ -1,8 +0,0 @@
-package com.bootcamp.be_java_hisp_w20_g4.helpers.publication;
-
-import com.bootcamp.be_java_hisp_w20_g4.excepcion.BadRequestException;
-
-public class PublicationValidator {
-
-
-}
