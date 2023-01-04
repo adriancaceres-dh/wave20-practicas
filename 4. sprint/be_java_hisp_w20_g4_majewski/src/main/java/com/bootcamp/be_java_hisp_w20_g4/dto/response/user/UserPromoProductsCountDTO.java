efcdee1de898583fea719ace5baf7d02e4c0f2ba
IvanMajewski1 <ivan.majewski@mercadolibre.com>
2023-01-04 16:41:34 -0300

    Creo la clase UserPromoProductsCountDTO para devolver información de un vendedor y la cantidad de publicaciones en promoción que tiene

---
+++ 4. sprint/be_java_hisp_w20_g4_majewski/src/main/java/com/bootcamp/be_java_hisp_w20_g4/dto/response/user/UserPromoProductsCountDTO.java
@@ -0,0 +1,15 @@
+package com.bootcamp.be_java_hisp_w20_g4.dto.response.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class UserPromoProductsCountDTO {
+
+    private int user_id;
+    private String user_name;
+    private Integer promo_products_count;
+}
--- 4. sprint/be_java_hisp_w20_g4_majewski/src/main/java/com/bootcamp/be_java_hisp_w20_g4/model/Publication.java
+++ 4. sprint/be_java_hisp_w20_g4_majewski/src/main/java/com/bootcamp/be_java_hisp_w20_g4/model/Publication.java
@@ -22,9 +22,8 @@ public class Publication {
     private double discount;
     private Product product;
     private Category category;
-
     private int user_id;
-    private int contador = 3;
+    private static int contador = 0;
 
     public Publication(LocalDate date, double price, Product product, Category category, int user_id) {
         this.user_id = user_id;
