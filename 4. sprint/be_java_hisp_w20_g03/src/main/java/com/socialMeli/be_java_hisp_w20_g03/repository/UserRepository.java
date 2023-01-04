bd72ac6b374b94664a71ea5e06f7bd63063db2ce
efruizz <efrain.ruiz@mercadolibre.com>
2023-01-04 09:35:07 -0300

    Add test data

--- 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/repository/UserRepository.java
+++ 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.socialMeli.be_java_hisp_w20_g03.repository;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -14,17 +15,69 @@ public class UserRepository implements IUserRepository {
 
     public UserRepository() {
         this.userList = new ArrayList<>();
+        User usuario1 = User.builder()
+                .user_id(234)
+                .user_name("usuario1")
+                .followers(new ArrayList<>())
+                .followed(new ArrayList<>())
+                .build();
+        User usuario2 = User.builder()
+                .user_id(6631)
+                .user_name("usuario2")
+                .followers(new ArrayList<>())
+                .followed(new ArrayList<>())
+                .build();
+        User usuario3 = User.builder()
+                .user_id(4698)
+                .user_name("usuario3")
+                .followers(new ArrayList<>())
+                .followed(new ArrayList<>())
+                .build();
+        User usuario4 = User.builder()
+                .user_id(1536)
+                .user_name("usuario4")
+                .followers(new ArrayList<>())
+                .followed(new ArrayList<>())
+                .build();
+        User usuario5 = User.builder()
+                .user_id(2236)
+                .user_name("usuario5")
+                .followers(new ArrayList<>())
+                .followed(new ArrayList<>())
+                .build();
+        User usuario6 = User.builder()
+                .user_id(6932)
+                .user_name("usuario6")
+                .followers(new ArrayList<>())
+                .followed(new ArrayList<>())
+                .build();
+        User usuario7 = User.builder()
+                .user_id(123)
+                .user_name("usuario7")
+                .followers(new ArrayList<>())
+                .followed(new ArrayList<>())
+                .build();
 
-        userList.add(new User(234, "vendedor1", new ArrayList<User>(), new ArrayList<>()));
-        userList.add(new User(4698, "usuario1", new ArrayList<User>(), List.of(
-                new User(123,"vendedor4", new ArrayList<User>(), new ArrayList<User>()),
-                new User(781,"usuario4", new ArrayList<User>(), new ArrayList<User>()),
-                new User(6631,"vendedor3", new ArrayList<User>(), new ArrayList<User>()))));
-        userList.add(new User(1536, "usuario2", new ArrayList<User>(), new ArrayList<User>()));
-        userList.add(new User(2236, "usuario3", new ArrayList<User>(), new ArrayList<User>()));
-        userList.add(new User(6932, "vendedor2", new ArrayList<User>(), new ArrayList<User>()));
-        userList.add(new User(6631, "vendedor3", new ArrayList<User>(), new ArrayList<User>()));
-        userList.add(new User(123, "vendedor4", new ArrayList<User>(), new ArrayList<User>()));
+        usuario1.getFollowers().add(usuario2);
+        usuario1.getFollowers().add(usuario3);
+        usuario1.getFollowers().add(usuario4);
+        usuario2.getFollowed().add(usuario1);
+        usuario3.getFollowed().add(usuario1);
+        usuario4.getFollowed().add(usuario1);
+
+        usuario2.getFollowers().add(usuario3);
+        usuario2.getFollowers().add(usuario4);
+        usuario3.getFollowed().add(usuario2);
+        usuario4.getFollowed().add(usuario2);
+
+        userList.add(usuario1);
+        userList.add(usuario2);
+        userList.add(usuario3);
+        userList.add(usuario4);
+        userList.add(usuario5);
+        userList.add(usuario6);
+        userList.add(usuario7);
+
     }
 
     public List<User> getUsers() {
