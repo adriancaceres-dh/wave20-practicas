f2029421ec459f6919224e6efd683c66dbb0d3e8
IvanMajewski1 <ivan.majewski@mercadolibre.com>
2023-01-04 16:25:49 -0300

    Agrego el método addPublicationPromotion a la interfaz IServicePublication

--- 4. sprint/be_java_hisp_w20_g4_majewski/src/main/java/com/bootcamp/be_java_hisp_w20_g4/service/publication/IServicePublication.java
+++ 4. sprint/be_java_hisp_w20_g4_majewski/src/main/java/com/bootcamp/be_java_hisp_w20_g4/service/publication/IServicePublication.java
@@ -1,13 +1,14 @@
 package com.bootcamp.be_java_hisp_w20_g4.service.publication;
 
 import com.bootcamp.be_java_hisp_w20_g4.dto.request.PostDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.request.PostPromotionDTO;
 import com.bootcamp.be_java_hisp_w20_g4.dto.response.product.ProductTwoWeeksResponseDTO;
 import com.bootcamp.be_java_hisp_w20_g4.dto.response.publication.PublicationDTO;
 
 public interface IServicePublication {
 
-
-    PublicationDTO addPublication(PostDTO publication);
     ProductTwoWeeksResponseDTO getLastTwoWeeksPublications(int userId, String order);
+    PublicationDTO addPublication(PostDTO publication);
+    PublicationDTO addPublicationPromotion(PostPromotionDTO publicationPromotionDto);
 
 }
