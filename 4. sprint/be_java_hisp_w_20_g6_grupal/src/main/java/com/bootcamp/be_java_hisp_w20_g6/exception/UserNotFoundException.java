b03e6aa20775df7f7cbfb3585e035e4347ce169d
Daniel Angel <daniel.rojas@mercadolibre.com.co>
2023-01-04 16:37:55 -0500

    correción repos

--- 4. sprint/be_java_hisp_w_20_g6_grupal/src/main/java/com/bootcamp/be_java_hisp_w20_g6/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.be_java_hisp_w20_g6.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
