9d7510f3ca11df413b4deaa9da36302ddc48ae8b
IvanMajewski1 <ivan.majewski@mercadolibre.com>
2023-01-04 11:24:54 -0300

    Agrego carpeta publication dentro de carpeta dto

--- 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/helpers/Validators.java
+++ 4. sprint/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/helpers/Validators.java
@@ -5,7 +5,7 @@ import com.bootcamp.be_java_hisp_w20_g4.model.Seller;
 import com.bootcamp.be_java_hisp_w20_g4.model.User;
 
 public class Validators {
-    
+
     public static void isValidOrder(String order){
         if (! ((order == null) || order.toLowerCase().equals("name_asc") || order.toLowerCase().equals("name_desc"))) throw new BadRequestException("Parametro de orden incorrecto");
     }
