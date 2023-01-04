35b2b5996546b84638297b53a8fb6e2a1ac81610
Lucia Balestrazzi <lucia.balestrazzi@mercadolibre.com>
2023-01-04 18:32:05 -0300

    add methods to post service interface

--- 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialmeli/be_java_hisp_w20_g03/service/IPostService.java
+++ 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialmeli/be_java_hisp_w20_g03/service/IPostService.java
@@ -1,10 +1,14 @@
 package com.socialmeli.be_java_hisp_w20_g03.service;
 
 import com.socialmeli.be_java_hisp_w20_g03.dto.PostDTO;
+import com.socialmeli.be_java_hisp_w20_g03.dto.SellerPromoCountDTO;
+import com.socialmeli.be_java_hisp_w20_g03.dto.SellerPromoListDTO;
 
 import java.util.List;
 
 public interface IPostService {
     String addPost(PostDTO postDTO);
     List<PostDTO> getPost(int userId, String order);
+    SellerPromoCountDTO getPromoPostCount(int userId);
+    SellerPromoListDTO getPromoPostList(int userId);
 }
