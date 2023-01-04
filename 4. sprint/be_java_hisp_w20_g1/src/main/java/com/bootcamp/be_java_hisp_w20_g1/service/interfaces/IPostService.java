ef9e098944a1c8bfc6a6a36492c1e81bceeec443
Esteban Demarchi <esteban.demarchi@mercadolibre.com>
2023-01-04 17:07:25 -0300

    Copio proyecto grupal en mi rama individual

---
+++ 4. sprint/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/service/interfaces/IPostService.java
@@ -0,0 +1,12 @@
+package com.bootcamp.be_java_hisp_w20_g1.service.interfaces;
+
+import com.bootcamp.be_java_hisp_w20_g1.dto.request.PostRequestDto;
+import com.bootcamp.be_java_hisp_w20_g1.dto.response.PostListResponseDto;
+import com.bootcamp.be_java_hisp_w20_g1.dto.response.PostResponseDto;
+
+import java.util.List;
+
+public interface IPostService {
+    PostListResponseDto lastTwoWeeksPostsFromFollowers(int id, String order);
+    PostResponseDto add(PostRequestDto postDto);
+}
