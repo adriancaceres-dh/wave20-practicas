d39278b93084fbdb80a43bc2ae267e470ea365fb
manueldiazmeli <manuel.diaz@mercadolibre.com.co>
2023-01-05 11:26:29 -0500

    Annadiendo carpeta desarrollo grupal

---
+++ 4. sprint/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/BeJavaHispW20G7Application.java
@@ -0,0 +1,13 @@
+package com.bootcamp.be_java_hisp_w20_g7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaHispW20G7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeJavaHispW20G7Application.class, args);
+    }
+
+}
---
+++ 4. sprint/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.bootcamp.be_java_hisp_w20_g7.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@ControllerAdvice(annotations = RestController.class)
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(FollowAlreadyExistException.class)
+    public ResponseEntity<?> FollowAlreadyExistException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> userNotFoundException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(UnfollowNotExistException.class)
+    public ResponseEntity<?> UnFollowNotExistException(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(DataIsnotCorrectException.class)
+    public ResponseEntity<?> dataIsNotCorrect(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+    @ExceptionHandler(PostEmptyException.class)
+    public ResponseEntity<?> postEmpty(Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+
+}
