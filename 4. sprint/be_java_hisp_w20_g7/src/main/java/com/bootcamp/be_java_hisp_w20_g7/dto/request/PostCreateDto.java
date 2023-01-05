88d85060c23e0f09e89d21fae94781bfd3341b54
Ronald Esteban Rosero Montana <ronald.rosero@mercadolibre.com.co>
2023-01-05 14:59:40 -0500

    Agregando el proyecto grupal

--- 4. sprint/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/dto/request/PostCreateDto.java
+++ 4. sprint/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/dto/request/PostCreateDto.java
@@ -1,28 +1,24 @@
 package com.bootcamp.be_java_hisp_w20_g7.dto.request;
 
-
 import com.bootcamp.be_java_hisp_w20_g7.dto.ProductDto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-public class ProductPromoDto {
+@Data
+public class PostCreateDto {
+
+
     private int userId;
-    private int postId;
     @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
     private ProductDto product;
     private int category;
     private double price;
-    private boolean hasPromo;
-    private double discount;
 
 }
--- 4. sprint/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/entity/Post.java
+++ 4. sprint/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/entity/Post.java
@@ -1,25 +1,20 @@
-package com.bootcamp.be_java_hisp_w20_g7.dto.request;
+package com.bootcamp.be_java_hisp_w20_g7.entity;
 
-
-import com.bootcamp.be_java_hisp_w20_g7.dto.ProductDto;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
-public class ProductPromoDto {
+@Data
+public class Post {
+
     private int userId;
     private int postId;
-    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
-    private ProductDto product;
+    private Product product;
     private int category;
     private double price;
     private boolean hasPromo;
