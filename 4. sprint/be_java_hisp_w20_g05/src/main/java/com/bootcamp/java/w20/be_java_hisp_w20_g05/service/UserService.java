ba77da4fdd5d6fa87e28ae76a3ebb07d1a35b660
Jose Ivan Drozd <jose.drozd@mercadolibre.com>
2023-01-05 13:45:31 -0300

    Updating group solution to user stories from 1 to 9

---
+++ 4. sprint/be_java_hisp_w20_g05/src/main/java/com/bootcamp/java/w20/be_java_hisp_w20_g05/service/UserService.java
@@ -0,0 +1,113 @@
+package com.bootcamp.java.w20.be_java_hisp_w20_g05.service;
+
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.dto.MessageExceptionDTO;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.dto.response.FollowersBySellerDTO;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.dto.response.FollowersCountDTO;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.dto.response.UserResponseDTO;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.dto.response.followed_users_posts.FollowedListDTO;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.exceptions.IdNotFoundException;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.exceptions.WrongRequestParamException;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.model.User;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.repository.IRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UserService implements IUserService{
+    @Autowired
+    private IRepository<User> userRepository;
+
+    @Override
+    public List<UserResponseDTO> filterBy(String name) {
+        return null;
+    }
+
+    @Override
+    public FollowersCountDTO getFollowersCount (int id){
+        User user= getById(id);
+        return new FollowersCountDTO(user.getId(),user.getFollowers().size(),user.getUserName());
+    }
+
+    public User getById(int id) {
+        return userRepository.getById(id);
+    }
+
+    @Override
+    public ResponseEntity<String> followUser(int userId, int userIdToFollow) {
+
+        try {
+            User user1 = userRepository.getById(userId);
+            User user2 = userRepository.getById(userIdToFollow);
+            user1.followUser(user2.getId());
+            user2.addFollower(user1.getId());
+        } catch (IdNotFoundException exception) {
+            return new ResponseEntity<>("Bad Request", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("todo OK", HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<String> unfollowUser(int userId, int userIdToUnfollow) {
+        try {
+            User user1 = userRepository.getById(userId);
+            User user2 = userRepository.getById(userIdToUnfollow);
+            user1.unfollowUser(user2.getId());
+            user2.removeFollower(user1.getId());
+        } catch (IdNotFoundException exception) {
+            return new ResponseEntity<>("Bad Request", HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>("todo OK", HttpStatus.OK);
+    }
+
+    @Override
+    public FollowedListDTO getFollowedListDto(int userId, String order){
+        validateOrder(order);
+
+        User user = getById(userId);
+        List<UserResponseDTO> followed = new ArrayList<>();
+        for(Integer i: user.getFollowing()) followed.add(new UserResponseDTO(i, getById(i).getUserName()));
+
+        orderList(followed,order);
+
+        return new FollowedListDTO(user.getId(), user.getUserName(), followed);
+    }
+
+    @Override
+    public FollowersBySellerDTO getFollowersBySeller(int userId, String order) {
+        validateOrder(order);
+
+        User seller = userRepository.getById(userId);
+        List<User> followers = new ArrayList<>();
+        seller.getFollowers().stream().forEach(x -> followers.add(userRepository.getById(x)));
+
+        List<UserResponseDTO> followersDto = new ArrayList<>();
+        followers.stream().forEach(f -> followersDto.add(new UserResponseDTO(f.getId(),f.getUserName())));
+
+        orderList(followersDto,order);
+
+        return new FollowersBySellerDTO(seller.getId(), seller.getUserName(), followersDto);
+    }
+
+    public boolean orderList(List<UserResponseDTO> userResponseDTOList, String order){
+        if (order!=null) {
+            userResponseDTOList.sort((x, y) -> (order.equalsIgnoreCase("name_asc")) ?
+                    String.CASE_INSENSITIVE_ORDER.compare(x.getUserName(), y.getUserName()) :
+                    String.CASE_INSENSITIVE_ORDER.compare(y.getUserName(), x.getUserName()));
+            return true;
+        }else return false;
+    }
+
+    public boolean validateOrder(String order){
+        if(order!=null && !order.equalsIgnoreCase("name_asc") && !order.equalsIgnoreCase("name_desc")){
+            throw new WrongRequestParamException(new MessageExceptionDTO("WRONG ORDER PARAMETER"));}
+        else return true;
+
+    }
+
+}
