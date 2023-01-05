c34c0ec649c5d7eec92ac36a5f6d7425502d973c
Ivan <ivan.cano@mercadolibre.com>
2023-01-05 16:18:58 -0300

    Se agrega carpeta de desarrollo grupal.

---
+++ 4. sprint/be_java_hisp_w20_g05/src/main/java/com/bootcamp/java/w20/be_java_hisp_w20_g05/BeJavaHispW20G05Application.java
@@ -0,0 +1,13 @@
+package com.bootcamp.java.w20.be_java_hisp_w20_g05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeJavaHispW20G05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeJavaHispW20G05Application.class, args);
+    }
+
+}
---
+++ 4. sprint/be_java_hisp_w20_g05/src/main/java/com/bootcamp/java/w20/be_java_hisp_w20_g05/repository/PostRepository.java
@@ -0,0 +1,78 @@
+package com.bootcamp.java.w20.be_java_hisp_w20_g05.repository;
+
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.dto.MessageExceptionDTO;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.exceptions.IdNotFoundException;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.model.Post;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Repository
+public class PostRepository implements IPostRepository{
+    private Set<Post> posts = new HashSet<>();
+
+    public PostRepository() {
+        posts = loadDataBase(); }
+
+    @Override
+    public boolean contains(Post post) {
+        return posts.contains(post);
+    }
+    @Override
+    public boolean add(Post post) {
+        if (contains(post)) return false;
+        return posts.add(post);
+    }
+    public Post getById(int userId) {
+        return posts.stream().filter(u -> u.getId()== userId)
+                .findFirst().orElseThrow(() -> new IdNotFoundException(new MessageExceptionDTO("Elemento no encontrado")));
+    }
+
+    @Override
+    public Set<Post> filterBy(String productName) {
+        return posts.stream().filter(p -> p.getProduct().getName().toLowerCase()
+                        .matches("^.*" + productName.toLowerCase() + ".*$"))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<Post> getAll() {
+        return this.posts;
+    }
+
+    public Set<Post> filterByUserId(int userId) {
+        return posts.stream().filter(p -> p.getUserId()== userId)
+                .collect(Collectors.toSet());
+    }
+
+    private static Set<Post> loadDataBase() {
+        File file = null;
+        try {
+            file = ResourceUtils.getFile("classpath:posts.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        TypeReference<Set<Post>> typeRef = new TypeReference<>() {};
+        Set<Post> posts = null;
+
+        try {
+            posts = objectMapper.readValue(file, typeRef);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return posts;
+    }
+}
