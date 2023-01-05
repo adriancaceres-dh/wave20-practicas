86b4dfa849866fcb6d2ec232e987b173e5c5ea92
Lucia Balestrazzi <lucia.balestrazzi@mercadolibre.com>
2023-01-05 10:25:17 -0300

    Added request mapping annotation at the class level for each controller

--- 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialmeli/be_java_hisp_w20_g03/controller/PostController.java
+++ 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialmeli/be_java_hisp_w20_g03/controller/PostController.java
@@ -1,44 +1,40 @@
 package com.socialmeli.be_java_hisp_w20_g03.controller;
 
-import com.socialmeli.be_java_hisp_w20_g03.dto.PostDTO;
-import com.socialmeli.be_java_hisp_w20_g03.dto.PromoPostDTO;
-import com.socialmeli.be_java_hisp_w20_g03.dto.SellerPromoCountDTO;
-import com.socialmeli.be_java_hisp_w20_g03.dto.SellerPromoListDTO;
+import com.socialmeli.be_java_hisp_w20_g03.dto.*;
 import com.socialmeli.be_java_hisp_w20_g03.service.IPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
+@RequestMapping("/products")
 public class PostController {
 
     @Autowired
     private IPostService iPostService;
 
-    @PostMapping("/products/post")
+    @PostMapping("/post")
     public ResponseEntity<String> addPost(@RequestBody PostDTO postDto) {
         return new ResponseEntity<>(iPostService.addPost(postDto), HttpStatus.OK);
     }
 
-    @PostMapping("/products/promo-post")
+    @PostMapping("/promo-post")
     public ResponseEntity<String> addPromoPost(@RequestBody PromoPostDTO postDto) {
         return new ResponseEntity<>(iPostService.addPost(postDto), HttpStatus.OK);
     }
 
-    @GetMapping("/products/followed/{userId}/list")
-    public ResponseEntity <List<PostDTO>> getPosts(@PathVariable int userId, @RequestParam(required = false) String order) {
+    @GetMapping("/followed/{userId}/list")
+    public ResponseEntity <FavoritePostsListDTO> getPosts(@PathVariable int userId, @RequestParam(required = false) String order) {
         return new ResponseEntity<>(iPostService.getPost(userId, order), HttpStatus.OK);
     }
 
-    @GetMapping("/products/promo-post/count")
+    @GetMapping("/promo-post/count")
     public ResponseEntity<SellerPromoCountDTO> getPromoPostCount(@RequestParam(name = "user_id") int userId) {
         return new ResponseEntity<>(iPostService.getPromoPostCount(userId), HttpStatus.OK);
     }
 
-    @GetMapping("/products/promo-post/list")
+    @GetMapping("/promo-post/list")
     public ResponseEntity<SellerPromoListDTO> getPromoPostList(@RequestParam(name = "user_id") int userId) {
         return new ResponseEntity<>(iPostService.getPromoPostList(userId), HttpStatus.OK);
     }
--- 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialmeli/be_java_hisp_w20_g03/controller/UserController.java
+++ 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialmeli/be_java_hisp_w20_g03/controller/UserController.java
@@ -13,40 +13,41 @@ import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private IUserService iUserService;
 
 
-    @GetMapping("/users")
+    @GetMapping("")
     public ResponseEntity<List<UserExtendedDTO>> list() {
         return new ResponseEntity<>(iUserService.getUsers(), HttpStatus.OK);
     }
 
-    @PostMapping("/users/{userId}/follow/{userIdToFollow}")
+    @PostMapping("/{userId}/follow/{userIdToFollow}")
     public ResponseEntity<String> addFollower(@PathVariable int userId, @PathVariable int userIdToFollow) {
         return new ResponseEntity<>(iUserService.addFollower(userId,userIdToFollow), HttpStatus.OK);
     }
 
-    @GetMapping("/users/{userId}/followers/count")
+    @GetMapping("/{userId}/followers/count")
     public ResponseEntity<UserFollowerCountDTO> getFollowerCount(@PathVariable int userId) {
         return new ResponseEntity<>(iUserService.getFollowerCount(userId), HttpStatus.OK);
     }
 
-    @GetMapping("/users/{userId}/followers/list")
+    @GetMapping("/{userId}/followers/list")
     public ResponseEntity <UserFollowersDTO> getFollowersList(@PathVariable int userId,
                                                               @RequestParam (required = false) String order) {
         return new ResponseEntity<>(iUserService.getFollowersList(userId, order), HttpStatus.OK);
     }
 
-    @GetMapping("/users/{userId}/followed/list")
+    @GetMapping("/{userId}/followed/list")
     public ResponseEntity<UserFollowersDTO> getFollowedList(@PathVariable int userId,
                                                             @RequestParam (required = false) String order) {
         return new ResponseEntity<>(iUserService.getFollowedList(userId, order), HttpStatus.OK);
     }
 
-    @PostMapping("/users/{userId}/unfollow/{userIdToUnfollow}")
+    @PostMapping("/{userId}/unfollow/{userIdToUnfollow}")
     public ResponseEntity<String> unfollow(@PathVariable int userId, @PathVariable int userIdToUnfollow) {
         return new ResponseEntity<>(iUserService.unfollow(userId, userIdToUnfollow), HttpStatus.OK);
     }
