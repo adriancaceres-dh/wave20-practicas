722e5bb1e8f46c1a63c00e1520e66bcd5ac0da42
Lucia Balestrazzi <lucia.balestrazzi@mercadolibre.com>
2023-01-05 15:35:35 -0300

    proyecto grupal

---
+++ 4. sprint/be_java_hisp_w20_g03/be_java_hisp_w20_g03/src/test/java/com/socialMeli/be_java_hisp_w20_g03/BeJavaHispW20G03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialMeli.be_java_hisp_w20_g03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaHispW20G03ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
--- 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialmeli/be_java_hisp_w20_g03/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.socialmeli.be_java_hisp_w20_g03.exception;
-
-public class NotFoundException extends RuntimeException{
-
-    public NotFoundException(String mensaje){
-        super(mensaje);
-    }
-}
