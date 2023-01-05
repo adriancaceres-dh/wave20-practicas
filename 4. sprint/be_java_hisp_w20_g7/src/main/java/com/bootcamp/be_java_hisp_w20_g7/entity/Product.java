fd044aa8931b519bd1a38a23f0928382a268ed4e
JuanSHenao <sebastian.henao@mercadolibre.com.co>
2023-01-05 12:26:01 -0500

    Agregando proyecto grupal

---
+++ 4. sprint/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/entity/Product.java
@@ -0,0 +1,20 @@
+package com.bootcamp.be_java_hisp_w20_g7.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+
+    private int productId;
+    private String productName;
+    private String type;
+    private String brand;
+    private String color;
+    private String notes;
+
+}
