0de22587a272f87e58467561f85d5eb127821780
Sebastian Anttury <sebastian.anttury@mercadolibre.com.co>
2023-01-05 12:01:47 -0500

    Se agrega el proyecto grupal

---
+++ 4. sprint/be_java_hisp_w20_g7/src/test/java/com/bootcamp/be_java_hisp_w20_g7/BeJavaHispW20G7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.be_java_hisp_w20_g7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaHispW20G7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
