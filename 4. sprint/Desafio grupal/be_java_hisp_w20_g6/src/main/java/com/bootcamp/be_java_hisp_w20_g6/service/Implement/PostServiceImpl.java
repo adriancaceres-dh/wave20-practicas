150af910c3afa0308f8905bbd09f66ca4bca1fd3
JuankOrtizR <juancarlos.ortiz@mercadolibre.com.co>
2023-01-05 14:55:55 -0500

    actualizo repo grupal

--- 4. sprint/Desafio grupal/be_java_hisp_w20_g6/src/main/java/com/bootcamp/be_java_hisp_w20_g6/service/Implement/PostServiceImpl.java
+++ 4. sprint/Desafio grupal/be_java_hisp_w20_g6/src/main/java/com/bootcamp/be_java_hisp_w20_g6/service/Implement/PostServiceImpl.java
@@ -1,28 +1,31 @@
 package com.bootcamp.be_java_hisp_w20_g6.service.Implement;
 
 import java.time.LocalDate;
-import java.time.Period;
+
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 import com.bootcamp.be_java_hisp_w20_g6.dto.response.PostListResponseDTO;
 import com.bootcamp.be_java_hisp_w20_g6.dto.response.PostResponseDTO;
+import com.bootcamp.be_java_hisp_w20_g6.exception.InvalidParamException;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bootcamp.be_java_hisp_w20_g6.dto.request.PostRequestDto;
-import com.bootcamp.be_java_hisp_w20_g6.exception.UserExistsException;
-import com.bootcamp.be_java_hisp_w20_g6.exception.UserNotFoundException;
+
 import com.bootcamp.be_java_hisp_w20_g6.model.PostModel;
 import com.bootcamp.be_java_hisp_w20_g6.repository.PostRepository;
 import com.bootcamp.be_java_hisp_w20_g6.service.Interface.IPostService;
 import com.bootcamp.be_java_hisp_w20_g6.service.Interface.IUserService;
 
 import org.modelmapper.config.Configuration;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 @Service
 public class PostServiceImpl implements IPostService {
@@ -46,13 +49,13 @@ public class PostServiceImpl implements IPostService {
 
     @Override
     public boolean save(PostRequestDto postRequestDto) {
-            userService.getUserById(postRequestDto.getUser_id());
-            PostModel postModel = mapper.map(postRequestDto, PostModel.class);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-            postModel.setDate(LocalDate.parse(postRequestDto.getDate(),formatter));
-            postModel.setId(postRepository.idGenerator());
-            postRepository.save(postModel);
-            return true;
+        userService.getUserById(postRequestDto.getUser_id());
+        PostModel postModel = mapper.map(postRequestDto, PostModel.class);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        postModel.setDate(LocalDate.parse(postRequestDto.getDate(),formatter));
+        postModel.setId(postRepository.idGenerator());
+        postRepository.save(postModel);
+        return true;
     }
 
     @Override
@@ -61,7 +64,7 @@ public class PostServiceImpl implements IPostService {
         List<PostResponseDTO> followedPost=new ArrayList<>();
         for(int id : userService.getUserById(user_id).getFollowed()){
             postRepository.getPostList().stream().filter(p->p.getUser_id()==id)
-                    .filter(p-> Period.between(p.getDate(),dateNow).getDays()<=15)
+                    .filter(p-> DAYS.between(p.getDate(),dateNow)<=15)
                     .forEach(p->followedPost.add(
                             new PostResponseDTO(p.getUser_id(),p.getId(),p.getDate()
                                     ,p.getProduct(),p.getCategory(),p.getPrice())
@@ -70,10 +73,12 @@ public class PostServiceImpl implements IPostService {
 
         if(orderBy != null && orderBy.equals("date_asc")) {
             followedPost.sort(Comparator.comparing(PostResponseDTO::getDate));
-        }else{
+        }else if(orderBy == null || orderBy.equals("date_desc")){
             followedPost.sort(Comparator.comparing(PostResponseDTO::getDate).reversed());
+        }else{
+            throw new InvalidParamException("Argumento invalido");
         }
-        
+
         return new PostListResponseDTO(user_id,followedPost );
     }
 
