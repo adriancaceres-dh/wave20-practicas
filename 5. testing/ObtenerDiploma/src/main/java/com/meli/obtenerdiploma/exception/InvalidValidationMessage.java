48376d915fe34460d9f90692fd70034041c4443a
Esteban Demarchi <esteban.demarchi@mercadolibre.com>
2023-01-06 15:08:28 -0300

    Add snake case strategy

--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/exception/InvalidValidationMessage.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/exception/InvalidValidationMessage.java
@@ -1,10 +1,13 @@
 package com.meli.obtenerdiploma.exception;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class InvalidValidationMessage {
     private String field;
     private String rejectedValue;
