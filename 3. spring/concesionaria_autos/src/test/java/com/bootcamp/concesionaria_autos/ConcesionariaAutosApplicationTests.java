311e95605b5cd39b568d0dc978261a40636ccfb7
luis-lopezgomez <luis.lopezgomez@mercadolibre.com.mx>
2023-01-06 16:02:50 -0600

    Ejercicio - Concesionaria de autos

---
+++ 3. spring/concesionaria_autos/src/test/java/com/bootcamp/concesionaria_autos/ConcesionariaAutosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.concesionaria_autos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesionariaAutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
