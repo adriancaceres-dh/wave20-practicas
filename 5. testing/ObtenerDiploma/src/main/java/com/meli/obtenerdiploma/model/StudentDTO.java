538ec4a27799e0281d6306c44fedd7a80e32eae1
franidalgoml <francisco.idalgo@mercadolibre.com>
2023-01-10 10:41:34 -0300

    ejercicio user story

---
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -0,0 +1,21 @@
+package com.meli.obtenerdiploma.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+import java.util.List;
+
+@Getter @Setter
+public class StudentDTO {
+    @NotBlank(message = "El nombre del alumno no puede estar vacío.")
+    @Pattern(regexp = "[A-ZÁ-Ú][A-Za-zá-ú ]*", message = "El nombre del alumno debe comenzar con mayúscula.")
+    @Size(max = 50, message = "La longitud del nombre no puede superar los 50 caracteres.")
+    String studentName;
+    String message;
+    Double averageScore;
+    @NotEmpty(message = "La lista no puede ser vacía.")
+    @Valid
+    List<SubjectDTO> subjects;
+}
