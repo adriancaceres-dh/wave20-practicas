33061b185de9ee60280e406d1c702d0e9ee4177d
luis-lopezgomez <luis.lopezgomez@mercadolibre.com.mx>
2023-01-11 14:37:25 -0600

    Ejercicio - Start Wars - Tests de Integración

--- 5. testing/starwars/src/test/java/com/mercadolibre/starwars/integration/FindControllerIntegrationTest.java
+++ 5. testing/starwars/src/test/java/com/mercadolibre/starwars/integration/FindControllerIntegrationTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
@@ -33,6 +34,7 @@ public class FindControllerIntegrationTest {
         // act
         MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/{query}", query))
                 .andDo(print()).andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Luke Skywalker"))
                 .andReturn();
         // assert
         assertEquals("application/json", mvcResult.getResponse().getContentType());
