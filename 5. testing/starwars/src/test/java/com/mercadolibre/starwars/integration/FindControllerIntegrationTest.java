135d0133627b92ddb37f701bacf2fa2d49b0b94b
luis-lopezgomez <luis.lopezgomez@mercadolibre.com.mx>
2023-01-11 12:16:24 -0600

    Ejercicio - Start Wars - Tests de Integración

---
+++ 5. testing/starwars/src/test/java/com/mercadolibre/starwars/integration/FindControllerIntegrationTest.java
@@ -0,0 +1,70 @@
+package com.mercadolibre.starwars.integration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.mercadolibre.starwars.dto.CharacterDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class FindControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void testFindOutput() throws Exception {
+        // arrange
+        String query = "Luke";
+        // act
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/{query}", query))
+                .andDo(print()).andExpect(status().isOk())
+                .andReturn();
+        // assert
+        assertEquals("application/json", mvcResult.getResponse().getContentType());
+    }
+
+    @Test
+    void testFindNotFoundOutput() throws Exception {
+        // arrange
+        String query = "";
+        // act
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/{query}", query))
+                .andDo(print()).andExpect(status().isNotFound())
+                .andReturn();
+        // assert
+        assertEquals(404, mvcResult.getResponse().getStatus());
+    }
+
+    @Test
+    void testFindResponseContentOutput() throws Exception {
+        // arrange
+        String query = "Luke";
+        List<CharacterDTO> characters = List.of(new CharacterDTO("Luke Skywalker", "blond", "fair", "blue", "19BBY", "male", "Tatooine", "Human", 172, 77));
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+        String expectedJson = writer.writeValueAsString(characters);
+        // act
+        MvcResult mvcResult = this.mockMvc.perform(MockMvcRequestBuilders.get("/{query}", query))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+        // assert
+        assertEquals(expectedJson, mvcResult.getResponse().getContentAsString());
+    }
+}
