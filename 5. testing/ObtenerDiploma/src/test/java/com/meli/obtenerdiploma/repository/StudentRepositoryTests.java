93c39cd7cfa9b7ff0c08a309d3bba6936dc7ae48
ale-espindola <alejandra.espindola@mercadolibre.com>
2023-01-11 14:47:16 -0300

    Validaciones y tests unitarios VIVO - Ejercicio 1 corrección en StudentRepositoryTests

--- 5. testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/repository/StudentRepositoryTests.java
+++ 5. testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/repository/StudentRepositoryTests.java
@@ -1,15 +1,13 @@
 package com.meli.obtenerdiploma.repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.meli.obtenerdiploma.model.StudentDTO;
-import com.meli.obtenerdiploma.repository.IStudentRepository;
-import com.meli.obtenerdiploma.repository.StudentRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.util.HashSet;
 import java.util.Set;
 
 public class StudentRepositoryTests {
@@ -21,31 +19,62 @@ public class StudentRepositoryTests {
     }
 
     @Test
-    @DisplayName("Listar todos los alumnos")
+    @DisplayName("Camino feliz - Debería listar todos los alumnos")
     void findAllStudents() {
         //Arrange
-        String json1 = "{\"id\":1,\"studentName\":\"Juan\",\"message\":null,\"averageScore\":null,\"subjects\":[{\"name\":\"Matemática\",\"score\":9.0},{\"name\":\"Física\",\"score\":7.0},{\"name\":\"Química\",\"score\":6.0}]}";
-        String json2 = "{\"id\":2,\"studentName\":\"Pedro\",\"message\":null,\"averageScore\":null,\"subjects\":[{\"name\":\"Matemática\",\"score\":10.0},{\"name\":\"Física\",\"score\":8.0},{\"name\":\"Química\",\"score\":4.0}]}";
+        String json = "[\n" +
+                "  {\n" +
+                "    \"id\": 1,\n" +
+                "    \"studentName\": \"Juan\",\n" +
+                "    \"subjects\": [\n" +
+                "      {\n" +
+                "        \"name\": \"Matemática\",\n" +
+                "        \"score\": 9\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"name\": \"Física\",\n" +
+                "        \"score\": 7\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"name\": \"Química\",\n" +
+                "        \"score\": 6\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  },\n" +
+                "  {\n" +
+                "    \"id\": 2,\n" +
+                "    \"studentName\": \"Pedro\",\n" +
+                "    \"subjects\": [\n" +
+                "      {\n" +
+                "        \"name\": \"Matemática\",\n" +
+                "        \"score\": 10\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"name\": \"Física\",\n" +
+                "        \"score\": 8\n" +
+                "      },\n" +
+                "      {\n" +
+                "        \"name\": \"Química\",\n" +
+                "        \"score\": 4\n" +
+                "      }\n" +
+                "    ]\n" +
+                "  }\n" +
+                "]";
         ObjectMapper mapper = new ObjectMapper();
-        Set<StudentDTO> students = new HashSet<>();
-        StudentDTO student1, student2;
+        Set<StudentDTO> students;
 
+        //Set<StudentDTO> studentDTOS = mapper.readValue(json.getBytes(), Set<StudentDTO.class>);
         try {
-            student1 = mapper.readValue(json1.getBytes(), StudentDTO.class);
-            student2 = mapper.readValue(json2.getBytes(), StudentDTO.class);
-        } catch (IOException e) {
+            students = mapper.readValue(json, new TypeReference<Set<StudentDTO>>() {});
+        } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        //Set<StudentDTO> studentDTOS = mapper.readValue(json.getBytes(), Set<StudentDTO.class>);
-
-        students.add(student1);
-        students.add(student2);
 
         //Act
         Set<StudentDTO> studentsFound = studentRepository.findAll();
 
-        //Assert: chequeo el tamaño de los Sets
-        Assertions.assertEquals(students.size(), studentsFound.size());
+        //Assert
+        Assertions.assertEquals(students, studentsFound);
 
     }
 
