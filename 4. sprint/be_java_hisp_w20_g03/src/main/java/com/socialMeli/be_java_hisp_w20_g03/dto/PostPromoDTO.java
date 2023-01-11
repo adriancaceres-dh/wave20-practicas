2a400ccba267697dc4c955eca8ce71b9a2842261
IvanGonWM <ivanamarowm@gmail.com>
2023-01-11 00:31:07 -0300

    Auto stash before merge of "ivan_amaro" and "origin/ivan_amaro"

--- 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/PostPromoDTO.java
+++ 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/PostPromoDTO.java
@@ -1,6 +1,7 @@
 package com.socialMeli.be_java_hisp_w20_g03.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.socialMeli.be_java_hisp_w20_g03.model.Product;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,15 +11,13 @@ import java.time.LocalDate;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PostDTO {
-    @JsonProperty("post_id")
-    private int postId;
-    @JsonProperty("user_id")
-    private int userId;
-    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+public class PostPromoDTO {
+    private int user_id;
+    @JsonFormat(pattern = "dd-MM-yyyy")
     private LocalDate date;
-    private ProductDTO product;
+    private Product product;
     private int category;
     private double price;
-
+    private boolean has_promo;
+    private double discount;
 }
--- 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/UserDTO.java
+++ 4. sprint/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/UserDTO.java
@@ -1,9 +1,13 @@
 package com.socialMeli.be_java_hisp_w20_g03.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     @JsonProperty("user_id")
     private int userId;
