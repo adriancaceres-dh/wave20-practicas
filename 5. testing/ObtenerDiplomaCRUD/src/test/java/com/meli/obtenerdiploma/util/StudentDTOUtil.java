fe8fa20cb56408ba68f9629a9462f9a44055e981
franidalgoml <francisco.idalgo@mercadolibre.com>
2023-01-11 12:23:28 -0300

    updates en StudentDTOUtil
    Ahora usa Java faker para generar alumnos con distinto nombre.

--- 5. testing/ObtenerDiplomaCRUD/src/test/java/com/meli/obtenerdiploma/util/StudentDTOUtil.java
+++ 5. testing/ObtenerDiplomaCRUD/src/test/java/com/meli/obtenerdiploma/util/StudentDTOUtil.java
@@ -1,5 +1,6 @@
 package com.meli.obtenerdiploma.util;
 
+import com.github.javafaker.Faker;
 import com.meli.obtenerdiploma.model.StudentDTO;
 import com.meli.obtenerdiploma.model.SubjectDTO;
 
@@ -10,11 +11,12 @@ public class StudentDTOUtil {
     private StudentDTOUtil() {}
 
     public static StudentDTO createStudentWithSubject() {
+        Faker faker = new Faker();
         SubjectDTO subjectDTO = new SubjectDTO("Mátematicas", 3d);
         return new StudentDTO(null,
-                "Jorge",
-                "Un mensaje",
-                8.0,
+                faker.name().fullName(),
+                null,
+                null,
                 new ArrayList<>(List.of(subjectDTO)));
 
     }
