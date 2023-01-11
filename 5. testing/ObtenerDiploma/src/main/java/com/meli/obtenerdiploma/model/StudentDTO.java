bb0be8f79d8c4edd934ddddc51e875f5ed9c03a4
Santiago Valdez <santiago.svaldez@mercadolibre.com>
2023-01-11 08:07:09 -0300

    Se agrega validaciones en subjectDTO y studentDTO para los datos de los modelos

--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/StudentDTO.java
@@ -3,13 +3,23 @@ package com.meli.obtenerdiploma.model;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter @Setter
 public class StudentDTO {
+
+    @Size(max = 20, message = "El nombre del alumno no puede estar vacío")
+    @NotNull(message = "La longitud del nombre no puede superar los 50 caracteres")
     String studentName;
     String message;
     Double averageScore;
+    @Valid
+    @NotEmpty(message = "La lista no puede estar vacía. ")
     List<SubjectDTO> subjects;
 }
--- 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/model/SubjectDTO.java
@@ -3,8 +3,19 @@ package com.meli.obtenerdiploma.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter @Setter
 public class SubjectDTO {
+    @Size(max = 30, message = "La longitud del nombre no puede superar los 30 caracteres.")
+    @NotNull(message = "El nombre de la materia no puede estar vacío.")
     String name;
+
+    @NotNull(message = "La nota no puede estar vacía.")
+    @Min(value = 0, message = "La mínima nota es 0.0.")
+    @Max(value = 10, message = "La maximo nota es 10.0.")
     Double score;
 }
