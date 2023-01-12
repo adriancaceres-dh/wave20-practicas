d7ff00192db5cd1c1da48eff83de51d1ec90d53b
Lautaro Walsh <lautaro.walsh@mercadolibre.com>
2023-01-12 12:37:45 -0300

    Subida de ejercicio Tests de integracion/star wars

---
+++ 5. testing/starwars/src/test/java/com/mercadolibre/starwars/Controller/FindControllerTest.java
@@ -0,0 +1,34 @@
+package com.mercadolibre.starwars.Controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class FindControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testEndpoint() throws Exception {
+        MvcResult mvcResult =
+                this.mockMvc.perform(MockMvcRequestBuilders.get("/darth"))
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("Darth Vader"))
+                        .andExpect(MockMvcResultMatchers.jsonPath("$[1].name").value("Darth Maul"))
+                        .andReturn();
+        Assertions.assertEquals("application/json", mvcResult.getResponse().getContentType());
+    }
+
+}
---
+++ 5. testing/starwars/src/test/java/com/mercadolibre/starwars/repositories/CharacterRepositoryImplTest.java
@@ -0,0 +1,27 @@
+
+
+package com.mercadolibre.starwars.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CharacterRepositoryImplTest {
+
+    CharacterRepositoryImpl repository;
+
+    @Autowired
+    public CharacterRepositoryImplTest(CharacterRepositoryImpl characterRepository) {
+        this.repository = characterRepository;
+    }
+
+    @Test
+    void fwhenQueryIsNullThenThrowException() {
+        String query = null;
+        assertThrows(NullPointerException.class, () -> repository.findAllByNameContains(query));
+    }
+}
