54765aabb286f2eff90756c01b7c457683da30db
EdwardGs1702 <john.garcia@mercadolibre.com.co>
2023-01-12 12:39:13 -0500

    add

---
+++ 5. testing/ObtenerDiploma/src/test/java/com/meli/obtenerdiploma/repository/StudentDAOTest.java
@@ -0,0 +1,71 @@
+package com.meli.obtenerdiploma.repository;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class StudentDAOTest {
+    private final StudentDAO studentDAO;
+
+    public StudentDAOTest() {
+        this.studentDAO = new StudentDAO();
+    }
+
+
+    @Test
+    void testSaveEstudent(){
+        List<SubjectDTO> subjectDTOList = new ArrayList<>();
+        subjectDTOList.add(new SubjectDTO("Maths",4.0) );
+        subjectDTOList.add(new SubjectDTO("History",4.5) );
+        StudentDTO studentDTO = new StudentDTO(1231L,"Camilo","Buen estudiante"
+                , 4.0, subjectDTOList );
+
+        studentDAO.save(studentDTO);
+        assertTrue(studentDAO.exists(studentDTO));
+    }
+
+
+    @Test
+    void testFindStudentById(){
+
+        List<SubjectDTO> subjectDTOList = new ArrayList<>();
+        subjectDTOList.add(new SubjectDTO("Maths",4.0) );
+        subjectDTOList.add(new SubjectDTO("History",4.5) );
+        StudentDTO studentDTO = new StudentDTO(1231L,"Camilo","Buen estudiante"
+                , 4.0, subjectDTOList );
+
+        studentDAO.save(studentDTO);
+        StudentDTO student = studentDAO.findById(studentDTO.getId());
+
+        assertNotNull(student);
+        assertEquals(student.getId(),studentDTO.getId());
+    }
+    @Test
+    void testModifyStudent(){
+
+        List<SubjectDTO> subjectDTOList = new ArrayList<>();
+        subjectDTOList.add(new SubjectDTO("Maths",4.0) );
+        subjectDTOList.add(new SubjectDTO("History",4.5) );
+
+        studentDAO.save(new StudentDTO(1L,"Andres",null,3.4,subjectDTOList));
+
+        assertEquals("Andres",studentDAO.findById(1L).getStudentName());
+    }
+
+    @Test
+    void deleteStudent(){
+        long queryId = 2L;
+
+        boolean result = studentDAO.delete(queryId);
+
+        assertTrue(result);
+    }
+
+
+}
