0370f98dc81a49c6cc3e0290cbe6ebe7c1d1d768
luis-lopezgomez <luis.lopezgomez@mercadolibre.com.mx>
2023-01-12 14:29:26 -0600

    Ejercicio - Números Romanos - Tests unitarios

--- 5. testing/roman-numerals/src/test/java/com/mercadolibre/romannumerals/RomanNumeralsRestControllerTests.java
+++ 5. testing/roman-numerals/src/test/java/com/mercadolibre/romannumerals/RomanNumeralsRestControllerTests.java
@@ -1,4 +1,59 @@
 package com.mercadolibre.romannumerals;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class RomanNumeralsRestControllerTests {
+
+    RomanNumeralsRestController romanNumeralsRestController;
+
+    public RomanNumeralsRestControllerTests() {
+        romanNumeralsRestController = new RomanNumeralsRestController();
+    }
+
+    @Test
+    void toRomanOk() {
+        // arrange
+        Integer[] numbers = new Integer[]{1, 3, 5, 7, 10, 50};
+        String[] expected = new String[]{"I", "III", "V", "VII", "X", "L"};
+        // act & assert
+        for(int i=0; i<numbers.length; i++) {
+            assertRomanExpected(numbers[i], expected[i]);
+        }
+    }
+
+    @Test
+    void toRomanLarge() {
+        // arrange
+        Integer number = 4999;
+        String expected = "MMMMCMXCIX";
+        // act & assert
+        assertRomanExpected(number, expected);
+    }
+
+    @Test
+    void toRomanZero() {
+        // arrange
+        Integer number = 0;
+        String expected = "";
+        // act & assert
+        assertRomanExpected(number, expected);
+    }
+
+    @Test
+    void toRomanNegative() {
+        // arrange
+        Integer number = -1;
+        String expected = "";
+        // act & assert
+        assertRomanExpected(number, expected);
+    }
+
+    private void assertRomanExpected(Integer number, String expected) {
+        // act
+        String actual = romanNumeralsRestController.toRoman(number);
+        // assert
+        assertEquals(expected, actual);
+    }
 }
