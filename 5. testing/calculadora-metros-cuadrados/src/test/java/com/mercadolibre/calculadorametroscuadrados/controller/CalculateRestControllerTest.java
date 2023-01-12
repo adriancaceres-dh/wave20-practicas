8852dbfb388a239bcb270b7008a2274900450c59
alejandrotm20 <edison.torres@mercadolibre.com.co>
2023-01-12 18:55:07 -0500

    Se agregan tests

---
+++ 5. testing/calculadora-metros-cuadrados/src/test/java/com/mercadolibre/calculadorametroscuadrados/controller/CalculateRestControllerTest.java
@@ -0,0 +1,33 @@
+package com.mercadolibre.calculadorametroscuadrados.controller;
+
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseDTO;
+import com.mercadolibre.calculadorametroscuadrados.dto.HouseResponseDTO;
+import com.mercadolibre.calculadorametroscuadrados.service.CalculateService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.mercadolibre.calculadorametroscuadrados.helper.CalculadoraHelper.createHouse;
+import static com.mercadolibre.calculadorametroscuadrados.helper.CalculadoraHelper.createHouseResponse;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CalculateRestControllerTest {
+
+    @Mock
+    CalculateService calculateService;
+
+    @InjectMocks
+    CalculateRestController calculateRestController;
+
+    @Test
+    void calculate() {
+        HouseDTO houseDTO = createHouse();
+
+        calculateRestController.calculate(houseDTO);
+
+        verify(calculateService, atLeastOnce()).calculate(houseDTO);
+    }
+}
