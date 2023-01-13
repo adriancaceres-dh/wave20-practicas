aa8e5d5f1ceab473224191ff54e5a6937bc18738
joaquinpereyrar <joaquin.pereyrarotulo@mercadolibre.com>
2023-01-13 17:19:04 -0300

    Se crean los test para el req T-0001 -> metodo follow

--- 6. sprint II/be_java_hisp_w20_g4/src/test/java/com/bootcamp/be_java_hisp_w20_g4/service/user/ServiceUsrTest.java
+++ 6. sprint II/be_java_hisp_w20_g4/src/test/java/com/bootcamp/be_java_hisp_w20_g4/service/user/ServiceUsrTest.java
@@ -1,13 +1,24 @@
 package com.bootcamp.be_java_hisp_w20_g4.service.user;
 
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.ListedUserDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserFollowedDTO;
+import com.bootcamp.be_java_hisp_w20_g4.exception.BadRequestException;
+import com.bootcamp.be_java_hisp_w20_g4.exception.NotFoundException;
+import com.bootcamp.be_java_hisp_w20_g4.model.Buyer;
+import com.bootcamp.be_java_hisp_w20_g4.model.Seller;
 import com.bootcamp.be_java_hisp_w20_g4.repository.user.IUserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ServiceUsrTest {
@@ -19,11 +30,35 @@ class ServiceUsrTest {
     ServiceUsr mockServiceUser;
 
     @Test
+    @DisplayName("Test user to follow exists ok")
     void followOKTest() {
+        // arrange
+        Buyer userFollow = new Buyer(1, "user which follows");
+        Seller sellerToFollow = new Seller(2, "userTofollow");
+        when(mockUserRepository.findById(userFollow.getUser_id())).thenReturn(userFollow);
+        when(mockUserRepository.findById(sellerToFollow.getUser_id())).thenReturn(sellerToFollow);
+
+        List<ListedUserDTO> followedExpected = List.of(new ListedUserDTO(sellerToFollow.getUser_id(),sellerToFollow.getUser_name()));
+        UserFollowedDTO expected = new UserFollowedDTO(userFollow.getUser_id(),userFollow.getUser_name(), followedExpected);
+
+        // act
+        UserFollowedDTO result = mockServiceUser.follow(userFollow.getUser_id(), sellerToFollow.getUser_id());
+
+        // assert
+        Assertions.assertEquals(expected, result);
     }
 
     @Test
+    @DisplayName("Test user to follow doesn't exist. Throw exception")
     void followInvalidIDTest() {
+        // arrange
+        Buyer userFollow = new Buyer(1, "user which follows");
+        Seller sellerToFollow = new Seller(2, "userTofollow");
+        when(mockUserRepository.findById(userFollow.getUser_id())).thenReturn(userFollow);
+        when(mockUserRepository.findById(sellerToFollow.getUser_id())).thenReturn(null);
+
+        // act && assert
+        Assertions.assertThrows(BadRequestException.class, () ->{mockServiceUser.follow(userFollow.getUser_id(), sellerToFollow.getUser_id());});
     }
 
     @Test
