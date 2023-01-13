a554ecd1604d1cc8f73971906a2a395c0646a177
DiegoAlejandroMalagon <diego.malagon@mercadolibre.com.co>
2023-01-13 10:07:11 -0500

    ultimo cambio en test

--- 5. testing/ObtenerDiploma (Base con CRUD logico)/src/test/java/com/meli/obtenerdiploma/testUnitarios/repository/StudentDAOTest.java
+++ 5. testing/ObtenerDiploma (Base con CRUD logico)/src/test/java/com/meli/obtenerdiploma/testUnitarios/repository/StudentDAOTest.java
@@ -9,12 +9,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.List;
 
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class StudentDAOTest {
 
