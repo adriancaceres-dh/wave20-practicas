bebe7f00eaa534c329c47c3e9c95d844ea5c18d6
IvanGonWM <ivan.amaro@mercadolibre.com>
2023-01-13 12:44:12 -0300

    Agregar validaciones a dtos

--- 6. sprint II/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/request/ProductDTO.java
+++ 6. sprint II/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/request/ProductDTO.java
@@ -5,16 +5,32 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProductDTO {
     @JsonProperty("product_id")
+    @NotNull(message = "La id no puede estar vacía.")
+    @Min(value = 1, message = "El id debe ser mayor a cero")
     private int productId;
     @JsonProperty("product_name")
+    @NotBlank(message = "El campo no puede estar vacío.")
+    @Max(value = 40, message = "La longitud no puede superar los 40 caracteres.")
+    @Pattern(regexp = "[A-Za-z0-9 ]+", message="El campo no puede poseer caracteres especiales.")
     private String productName;
+    @NotBlank(message = "El campo no puede estar vacío.")
+    @Max(value = 15, message = "La longitud no puede superar los 15 caracteres.")
+    @Pattern(regexp = "[A-Za-z0-9\\s]+", message="El campo no puede poseer caracteres especiales.")
     private String type;
+    @Max(value = 25, message = "La longitud no puede superar los 25 caracteres")
     private String brand;
+    @NotBlank(message = "El campo no puede estar vacío.")
+    @Max(value = 15, message = "La longitud no puede superar los 15 caracteres.")
+    @Pattern(regexp = "[A-Za-z0-9]+", message="El campo no puede poseer caracteres especiales.")
     private String color;
+    @Max(value = 80, message = "La longitud no puede superar los 80 caracteres.")
+    @Pattern(regexp = "[A-Za-z0-9]+", message="El campo no puede poseer caracteres especiales.")
     private String notes;
 }
--- 6. sprint II/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/request/UserDTO.java
+++ 6. sprint II/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/request/UserDTO.java
@@ -5,12 +5,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
     @JsonProperty("user_id")
+    @NotNull(message = "El id no puede estar vacio")
+    @Min(value = 1, message = "El id debe ser mayor a 0")
     private int userId;
     @JsonProperty("user_name")
+    @NotBlank(message = "El nombre de usuario no puede estar vacio")
+    @Max(value = 15, message = "El nombre de usuario debe tener menos de 15 caracteres")
     private String userName;
 }
