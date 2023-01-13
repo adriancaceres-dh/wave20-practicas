be75d1daa41099a701076367b70500042c709d7a
joaquinpereyrar <joaquin.pereyrarotulo@mercadolibre.com>
2023-01-13 11:57:30 -0300

    Se copia el proyecto grupal del sprint 1 a la carpeta del sprint 2

---
+++ 6. sprint II/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/service/user/IServiceUsr.java
@@ -0,0 +1,18 @@
+package com.bootcamp.be_java_hisp_w20_g4.service.user;
+
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserCountDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserFollowedDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserFollowersDTO;
+
+public interface IServiceUsr {
+
+    public UserCountDTO followersCount(int userId);
+
+    public UserFollowersDTO followers(int userId, String order);
+
+    public UserFollowedDTO follow(int userId, int userIdToFollow);
+
+    public UserFollowedDTO followed(int userId, String order);
+    public UserFollowedDTO unfollow(int userId, int userIdToUnfollow);
+
+}
