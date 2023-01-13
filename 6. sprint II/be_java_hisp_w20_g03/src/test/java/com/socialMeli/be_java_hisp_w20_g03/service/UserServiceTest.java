d7e0f83223e0f23054e10516dceeb95e16f8bca9
MatiasSegoviaML <matiasfederico.segovia@mercadolibre.com>
2023-01-13 15:43:43 -0300

    US0001-Permite continuar con normalidad.

--- 6. sprint II/be_java_hisp_w20_g03/src/test/java/com/socialMeli/be_java_hisp_w20_g03/service/UserServiceTest.java
+++ 6. sprint II/be_java_hisp_w20_g03/src/test/java/com/socialMeli/be_java_hisp_w20_g03/service/UserServiceTest.java
@@ -1,6 +1,10 @@
 package com.socialMeli.be_java_hisp_w20_g03.service;
 
+import com.socialMeli.be_java_hisp_w20_g03.model.User;
 import com.socialMeli.be_java_hisp_w20_g03.repository.IUserRepository;
+import com.socialMeli.be_java_hisp_w20_g03.utils.UserUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -8,6 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -19,7 +24,20 @@ class UserServiceTest {
     UserService userService;
 
     @Test
+    @DisplayName("US0001-Permite continuar con normalidad.")
     void addFollower() {
+        //Arrange
+        int idRequestUser = 234;
+        int idRequestUser2 = 6631;
+        User user1 = UserUtils.getUserAddFollower(idRequestUser);
+        User user2 = UserUtils.getUserAddFollower(idRequestUser2);
+        String expected = "Comenzaste a seguir al usuario: " + user2.getUserName();
+        when(userRepository.getUserById(idRequestUser)).thenReturn(user1);
+        when(userRepository.getUserById(idRequestUser2)).thenReturn(user2);
+        //Act
+        String result = userService.addFollower(idRequestUser,idRequestUser2);
+        // assert
+        Assertions.assertEquals(expected,result);
     }
 
     @Test
--- 6. sprint II/be_java_hisp_w20_g03/src/test/java/com/socialMeli/be_java_hisp_w20_g03/utils/UserUtils.java
+++ 6. sprint II/be_java_hisp_w20_g03/src/test/java/com/socialMeli/be_java_hisp_w20_g03/utils/UserUtils.java
@@ -1,4 +1,36 @@
 package com.socialMeli.be_java_hisp_w20_g03.utils;
 
+import com.socialMeli.be_java_hisp_w20_g03.dto.request.UserDTO;
+import com.socialMeli.be_java_hisp_w20_g03.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserUtils {
+    private static List<User> userList;
+public static User getUserAddFollower(int usId){
+     userList = new ArrayList<>();
+    User usuario1 = User.builder()
+            .userId(234)
+            .userName("usuario1")
+            .followers(new ArrayList<>())
+            .followed(new ArrayList<>())
+            .build();
+    User usuario2 = User.builder()
+            .userId(6631)
+            .userName("usuario2")
+            .followers(new ArrayList<>())
+            .followed(new ArrayList<>())
+            .build();
+    userList.add(usuario1);
+    userList.add(usuario2);
+   User userResponse = userList.stream()
+           .filter(user -> user.getUserId() == usId)
+           .findAny()
+           .orElse(null);
+
+   return userResponse;
+
+}
+
 }
