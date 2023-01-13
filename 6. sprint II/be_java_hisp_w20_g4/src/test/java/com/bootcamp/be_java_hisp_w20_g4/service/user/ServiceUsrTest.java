bafbd0b2b2aed166b782d1811801bbb137ada4c0
IvanMajewski1 <ivan.majewski@mercadolibre.com>
2023-01-13 16:17:38 -0300

    Desarrollo Test UnfollowOkTest

--- 6. sprint II/be_java_hisp_w20_g4/src/test/java/com/bootcamp/be_java_hisp_w20_g4/service/user/ServiceUsrTest.java
+++ 6. sprint II/be_java_hisp_w20_g4/src/test/java/com/bootcamp/be_java_hisp_w20_g4/service/user/ServiceUsrTest.java
@@ -1,13 +1,23 @@
 package com.bootcamp.be_java_hisp_w20_g4.service.user;
 
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.ListedUserDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserFollowedDTO;
+import com.bootcamp.be_java_hisp_w20_g4.model.Buyer;
+import com.bootcamp.be_java_hisp_w20_g4.model.Seller;
+import com.bootcamp.be_java_hisp_w20_g4.model.User;
 import com.bootcamp.be_java_hisp_w20_g4.repository.user.IUserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ServiceUsrTest {
@@ -56,6 +66,20 @@ class ServiceUsrTest {
 
     @Test
     void unfollowOKTest() {
+        Buyer userWhoUnfollow = new Buyer(1, "nombre1");
+        Seller userUnfollowed = new Seller(2, "nombre2");
+        userWhoUnfollow.addUserToMyFollowedList(userUnfollowed);
+        userUnfollowed.addUserToMyFollowersList(userWhoUnfollow);
+
+        UserFollowedDTO expected = new UserFollowedDTO(1, "nombre1", new ArrayList<>()); //No sigue a nadie
+
+        when(mockUserRepository.findById(1)).thenReturn(userWhoUnfollow);
+        when(mockUserRepository.findById(2)).thenReturn(userUnfollowed);
+
+        UserFollowedDTO result = mockServiceUser.unfollow(1,2);
+
+        Assertions.assertEquals(expected, result);
+
     }
 
     @Test
