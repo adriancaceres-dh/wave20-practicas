8b29187b72bc1d67964ae7db0cc9bbc401640d4c
ttrivelli1 <tammara.trivelli@mercadolibre.com>
2023-01-13 17:01:47 -0300

    Se agrega test para verificar que los nombres de los seguidos los devuleve en orden acendente

--- 6. sprint II/be_java_hisp_w20_g4/src/test/java/com/bootcamp/be_java_hisp_w20_g4/service/user/ServiceUsrTest.java
+++ 6. sprint II/be_java_hisp_w20_g4/src/test/java/com/bootcamp/be_java_hisp_w20_g4/service/user/ServiceUsrTest.java
@@ -1,13 +1,23 @@
 package com.bootcamp.be_java_hisp_w20_g4.service.user;
 
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.ListedUserDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserFollowedDTO;
+import com.bootcamp.be_java_hisp_w20_g4.model.Seller;
+import com.bootcamp.be_java_hisp_w20_g4.model.User;
 import com.bootcamp.be_java_hisp_w20_g4.repository.user.IUserRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ServiceUsrTest {
@@ -29,9 +39,46 @@ class ServiceUsrTest {
     @Test
     void followersCountOKTest() {
     }
+    
 
     @Test
     void followersOrderAscOKTest() {
+        Seller seller1 = new Seller();
+        seller1.setUser_name("rodri");
+        seller1.setUser_id(1);
+
+        Seller seller2 = new Seller();
+        seller2.setUser_name("Tammi");
+        seller2.setUser_id(6);
+
+        Seller seller3 = new Seller();
+        seller3.setUser_name("Daniela");
+        seller3.setUser_id(10);
+
+        Seller seller4 = new Seller();
+        seller4.setUser_name("Julieta");
+        seller4.setUser_id(7);
+
+        HashMap<Integer, User> followed = new HashMap<>();
+        followed.put(6,seller2);
+        followed.put(7,seller3);
+        followed.put(10,seller4);
+
+        seller1.setFollowed(followed);
+
+        List<ListedUserDTO> expectedList = new ArrayList<>();
+        expectedList.add(new ListedUserDTO(10,"Daniela"));
+        expectedList.add(new ListedUserDTO(7,"Julieta"));
+        expectedList.add(new ListedUserDTO(6,"Tammi"));
+
+        UserFollowedDTO expected =  new UserFollowedDTO(1,"rodri",expectedList);
+
+        when(mockUserRepository.findById(1)).thenReturn(seller1);
+
+        UserFollowedDTO result = mockServiceUser.followed(1,"name_asc");
+
+        Assertions.assertEquals(expected,result);
+
     }
 
     @Test
