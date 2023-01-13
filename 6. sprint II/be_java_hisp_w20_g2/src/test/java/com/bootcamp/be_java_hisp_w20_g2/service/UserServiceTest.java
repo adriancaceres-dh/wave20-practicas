c36b69d8641316f57ba7217cf96597eacc85324e
Federico Javier Veron <federico.veron@mercadolibre.com>
2023-01-13 15:38:25 -0300

    Agregada clase UtilsTest para generar objetos de pruebas, primer avance del US-0001

--- 6. sprint II/be_java_hisp_w20_g2/src/test/java/com/bootcamp/be_java_hisp_w20_g2/service/UserServiceTest.java
+++ 6. sprint II/be_java_hisp_w20_g2/src/test/java/com/bootcamp/be_java_hisp_w20_g2/service/UserServiceTest.java
@@ -1,13 +1,59 @@
 package com.bootcamp.be_java_hisp_w20_g2.service;
 
+import com.bootcamp.be_java_hisp_w20_g2.model.User;
+import com.bootcamp.be_java_hisp_w20_g2.repository.UserRepository;
+import com.bootcamp.be_java_hisp_w20_g2.repository.interfaces.IUserRepository;
+import com.bootcamp.be_java_hisp_w20_g2.util.UtilsTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class UserServiceTest {
+
+
+    @Mock
+    private IUserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
 
     @Test
     void findAllFollowers() {
+
+        // test follow an existing user (OK)
+
+        HashMap<Integer, User> users = UtilsTest.generateUsersToTestFollow();
+
+        User user1 = users.get(1);
+        User user2 = users.get(2);
+
+
+        when(userRepository.exists(1)).thenReturn(true);
+        when(userRepository.exists(2)).thenReturn(true);
+
+        when(userRepository.findOne(1)).thenReturn(user1);
+        when(userRepository.findOne(2)).thenReturn(user2);
+
+        boolean res = userService.follow(1,2);
+
+        verify(userRepository,atLeastOnce()).exists(1);
+        verify(userRepository,atLeastOnce()).exists(2);
+        verify(userRepository,atLeastOnce()).findOne(2);
+        verify(userRepository,atLeastOnce()).findOne(2);
+
+        Assertions.assertTrue(res);
     }
 
     @Test
---
+++ 6. sprint II/be_java_hisp_w20_g2/src/test/java/com/bootcamp/be_java_hisp_w20_g2/util/UtilsTest.java
@@ -0,0 +1,33 @@
+package com.bootcamp.be_java_hisp_w20_g2.util;
+
+import com.bootcamp.be_java_hisp_w20_g2.model.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class UtilsTest {
+    public static HashMap<Integer,User> generateUsersToTestFollow(){
+        User user1 = new User(1,"usuario1",new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
+        User user2 = new User(2,"usuario2",new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
+        user1.addFollower(user2);
+
+
+        HashMap<Integer,User> users = new HashMap<>();
+        users.put(1,user1);
+        users.put(2,user2);
+        return users;
+
+    }
+
+    public static HashMap<Integer,User> generateUsersToTestExistingFollow(){
+        User user1 = new User(1,"usuario1",new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
+        User user2 = new User(2,"usuario2",new ArrayList<>(),new ArrayList<>(),new ArrayList<>());
+        user2.addFollower(user1);
+
+        HashMap<Integer,User> users = new HashMap<>();
+        users.put(1,user1);
+        users.put(2,user2);
+        return users;
+
+    }
+}
