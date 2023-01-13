c0aa8a1ee0466cf4e574db5db5690c816cd72dda
Esteban Demarchi <esteban.demarchi@mercadolibre.com>
2023-01-13 14:50:22 -0300

    Se controla MethodArgumentNotValidException para múltiples validaciones

--- 6. sprint II/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/exception/GlobalExceptionHandler.java
+++ 6. sprint II/be_java_hisp_w20_g1/src/main/java/com/bootcamp/be_java_hisp_w20_g1/exception/GlobalExceptionHandler.java
@@ -3,10 +3,14 @@ package com.bootcamp.be_java_hisp_w20_g1.exception;
 import com.bootcamp.be_java_hisp_w20_g1.Parameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
@@ -27,6 +31,25 @@ public class GlobalExceptionHandler {
     public ResponseEntity<?> BadRequestException(Exception e) {
         return ResponseEntity.badRequest()
                 .body(new MessageException(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+
+        MessageException message = new MessageException(
+                "Campos inválidos o faltantes",
+                HttpStatus.BAD_REQUEST.value(),
+                getListOfInvalidFields(ex)
+        );
+        return ResponseEntity.badRequest().body(message);
+    }
+
+    private List<InvalidValidationMessage> getListOfInvalidFields(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(e -> new InvalidValidationMessage(e.getField(), e.getRejectedValue().toString(), e.getDefaultMessage()))
+                .collect(Collectors.toList());
     }
 
 }
