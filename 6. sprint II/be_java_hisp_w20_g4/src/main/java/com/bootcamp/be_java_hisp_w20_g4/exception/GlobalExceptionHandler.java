5a8f954466d2264bd3e84ba4f88b76d2fb648c5d
ttrivelli1 <tammara.trivelli@mercadolibre.com>
2023-01-13 12:23:28 -0300

    Exception para las validaciones

--- 6. sprint II/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/exception/GlobalExceptionHandler.java
+++ 6. sprint II/be_java_hisp_w20_g4/src/main/java/com/bootcamp/be_java_hisp_w20_g4/exception/GlobalExceptionHandler.java
@@ -14,6 +14,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
     }
 
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleValidateExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<String, String>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String message = error.getDefaultMessage();
+
+            errors.put(fieldName, message);
+        });
+
+        return errors;
+    }
+
     @ExceptionHandler(BadRequestException.class)
     public ResponseEntity<?> BadRequestException(Exception e){
         return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
