45f6f3af7a60ef9f79d6686e81c9e20e452bdd43
Lucia Balestrazzi <lucia.balestrazzi@mercadolibre.com>
2023-01-16 17:29:07 -0300

    Se agrega test de integracion add post invalid field

--- 6. sprint II/be_java_hisp_w20_g03_balestrazzi/src/test/java/com/socialMeli/be_java_hisp_w20_g03/integration/PostControllerIntegrationTest.java
+++ 6. sprint II/be_java_hisp_w20_g03_balestrazzi/src/test/java/com/socialMeli/be_java_hisp_w20_g03/integration/PostControllerIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -49,4 +50,40 @@ class PostControllerIntegrationTest {
             .andDo(MockMvcResultHandlers.print())
             .andExpect(expectedStatus);
   }
+
+  @Test
+  void integrationTestAddPostInvalidField() throws Exception {
+
+    String postJson = "{\n" +
+            "    \"user_id\": 123,\n" +
+            "    \"date\": \"29-04-2021\",\n" +
+            "    \"product\": {\n" +
+            "        \"product_id\": 1,\n" +
+            "        \"product_name\": \"Silla Gamer\",\n" +
+            "        \"type\": \"Gamer\",\n" +
+            "        \"brand\": \"Racer\",\n" +
+            "        \"color\": \"Red & Black\",\n" +
+            "        \"notes\": \"Special Edition\"\n" +
+            "    },\n" +
+            "    \"category\": 100,\n" +
+            "    \"price\": 1500.50\n" +
+            "}";
+
+    ResultMatcher expectedStatus = status().isBadRequest();
+    ResultMatcher expectedContentType = MockMvcResultMatchers.content().contentType("text/plain;charset=UTF-8");
+    ResultMatcher expectedMessage = MockMvcResultMatchers.content()
+            .string("El campo no puede poseer caracteres especiales.");
+    MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(
+                    "/products/post")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(postJson);
+
+    // Act & Assert
+    mockMvc.perform(request)
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(expectedStatus)
+            .andExpect(expectedContentType)
+            .andExpect(expectedMessage);
+  }
+
 }
