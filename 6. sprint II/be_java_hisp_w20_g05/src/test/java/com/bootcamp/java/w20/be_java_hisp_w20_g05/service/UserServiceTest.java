5462f4c0abd4d1c81d19de81dfd9d80b461086c1
LuBenavides22 <luciano.benavides@mercadolibre.com>
2023-01-16 12:04:59 -0300

    T-0007 added

--- 6. sprint II/be_java_hisp_w20_g05/src/test/java/com/bootcamp/java/w20/be_java_hisp_w20_g05/service/UserServiceTest.java
+++ 6. sprint II/be_java_hisp_w20_g05/src/test/java/com/bootcamp/java/w20/be_java_hisp_w20_g05/service/UserServiceTest.java
@@ -1,10 +1,22 @@
 package com.bootcamp.java.w20.be_java_hisp_w20_g05.service;
 
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.dto.response.FollowersCountDTO;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.model.User;
 import com.bootcamp.java.w20.be_java_hisp_w20_g05.repository.IUserRepository;
+import com.bootcamp.java.w20.be_java_hisp_w20_g05.util.TestUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+import java.util.Optional;
+import java.util.Set;
+
 
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
@@ -13,4 +25,18 @@ public class UserServiceTest {
 
     @InjectMocks
     UserService userService;
+
+    @Test
+    @DisplayName("T-0007 Cantidad de Segiodores correcta")
+    public void getFollowersCountTest() throws Exception{
+        //arrange
+        Set<User> users = TestUtils.createMockUsers();
+        Optional<User> user1 = users.stream().findFirst();
+        when(userRepository.getById(user1.get().getId())).thenReturn(user1);
+        //act
+        FollowersCountDTO followersCountDTO = userService.getFollowersCount(user1.get().getId());
+        //verify
+        verify(userRepository, atLeastOnce()).getById(user1.get().getId());
+        assertEquals(user1.get().getFollowers().size(), followersCountDTO.getFollowers_count());
+    }
 }
