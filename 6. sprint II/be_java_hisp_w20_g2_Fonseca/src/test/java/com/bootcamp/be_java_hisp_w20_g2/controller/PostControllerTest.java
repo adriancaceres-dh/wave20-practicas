324de7e74b7bd2c6cbd83ab5bf99856c7ba9f760
Emanoide47 <emanuel.fonseca@mercadolibre.com>
2023-01-16 16:32:25 -0300

    Agregado de carpeta para realizar parte individual - test integradores

---
+++ 6. sprint II/be_java_hisp_w20_g2_Fonseca/src/test/java/com/bootcamp/be_java_hisp_w20_g2/controller/PostControllerTest.java
@@ -0,0 +1,16 @@
+package com.bootcamp.be_java_hisp_w20_g2.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PostControllerTest {
+
+    @Test
+    void addNewProduct() {
+    }
+
+    @Test
+    void sendLastPostOfFollowed() {
+    }
+}
