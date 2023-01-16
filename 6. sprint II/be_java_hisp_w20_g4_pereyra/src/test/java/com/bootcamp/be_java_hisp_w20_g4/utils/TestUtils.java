1937a70daf4eadbfed1955f57a595c9d2820b6ec
joaquinpereyrar <joaquin.pereyrarotulo@mercadolibre.com>
2023-01-16 17:44:40 -0300

    Se crea proyecto individual

---
+++ 6. sprint II/be_java_hisp_w20_g4_pereyra/src/test/java/com/bootcamp/be_java_hisp_w20_g4/utils/TestUtils.java
@@ -0,0 +1,10 @@
+package com.bootcamp.be_java_hisp_w20_g4.utils;
+
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.modelmapper.ModelMapper;
+
+public class TestUtils {
+
+    public static ModelMapper mapper = new ModelMapper();
+
+}
