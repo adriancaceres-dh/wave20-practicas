6bf209c0277da45b2605f26dcbced301c36935f1
alejandrotm20 <edison.torres@mercadolibre.com.co>
2023-01-16 15:40:04 -0500

    Se agrega test de integracion para PostController

---
+++ 6. sprint II/Practica Individual/be_java_hisp_w20_g6_torres/src/test/java/com/bootcamp/be_java_hisp_w20_g6/Integration/controller/PostControllerTest.java
@@ -0,0 +1,32 @@
+package com.bootcamp.be_java_hisp_w20_g6.Integration.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("Test de integracion para endopint: /products/followed/{userId}/list")
+    void countFollowers() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/products/followed/{userId}/list", 1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"));
+    }
+
+}
