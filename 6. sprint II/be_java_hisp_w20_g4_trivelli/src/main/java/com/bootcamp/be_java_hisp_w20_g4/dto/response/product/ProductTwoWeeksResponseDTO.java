6aec92d23fff28765d04f4927b5948c42c2ccf12
ttrivelli1 <tammara.trivelli@mercadolibre.com>
2023-01-16 16:34:17 -0300

    Parte hecha en grupo se pasa a mi branch

---
+++ 6. sprint II/be_java_hisp_w20_g4_trivelli/src/main/java/com/bootcamp/be_java_hisp_w20_g4/dto/response/product/ProductTwoWeeksResponseDTO.java
@@ -0,0 +1,15 @@
+package com.bootcamp.be_java_hisp_w20_g4.dto.response.product;
+
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.publication.ListedPostDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ProductTwoWeeksResponseDTO {
+    private Integer user_id;
+    private List<ListedPostDTO> posts;
+}
---
+++ 6. sprint II/be_java_hisp_w20_g4_trivelli/src/main/java/com/bootcamp/be_java_hisp_w20_g4/repository/product/ProductRepository.java
@@ -0,0 +1,51 @@
+package com.bootcamp.be_java_hisp_w20_g4.repository.product;
+
+import com.bootcamp.be_java_hisp_w20_g4.model.Product;
+import com.bootcamp.be_java_hisp_w20_g4.repository.product.IProductRepository;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
+
+@Repository
+public class ProductRepository implements IProductRepository {
+
+    private List<Product> products = loadProducts();
+
+    public ProductRepository(){}
+
+    private List<Product> loadProducts() {
+        File file = null;
+        try {
+            file = ResourceUtils.getFile("classpath:products.json");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<Product>> typeRef = new TypeReference<>() {};
+        List<Product> products = null;
+        try {
+            products = objectMapper.readValue(file, typeRef);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+
+    /**
+     * Este método se encarga de verificar que un producto exista
+     * @param product - Es el producto que hay que verificar si existe
+     * @return boolean - Devuelve true si el producto existe
+     */
+    public boolean productExist(Product product){
+        Product productRepository = products.stream().filter(p -> p.getProduct_id() == product.getProduct_id()).findFirst().orElse(null);
+        if(productRepository == null) return false;
+        return productRepository.equals(product);
+    }
+
+}
