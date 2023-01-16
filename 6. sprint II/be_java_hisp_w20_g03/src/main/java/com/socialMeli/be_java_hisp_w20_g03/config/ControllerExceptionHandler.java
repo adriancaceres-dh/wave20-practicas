1c69eb6edf903b9018c33c9ba0cb6ca80873a11b
Lucia Balestrazzi <lucia.balestrazzi@mercadolibre.com>
2023-01-16 09:08:52 -0300

    se agrega metodo para manejar excepciones de validacion

--- 6. sprint II/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/config/ControllerExceptionHandler.java
+++ 6. sprint II/be_java_hisp_w20_g03/src/main/java/com/socialMeli/be_java_hisp_w20_g03/config/ControllerExceptionHandler.java
@@ -4,20 +4,27 @@ import com.socialMeli.be_java_hisp_w20_g03.exception.BadRequestException;
 import com.socialMeli.be_java_hisp_w20_g03.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> userNotFound(NotFoundException e) {
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler(NotFoundException.class)
+  public ResponseEntity<String> userNotFound(NotFoundException e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+  }
 
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<?> badRequestException(Exception e) {
-        return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
-    }
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<String> badRequestException(Exception e) {
+    return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<String> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+    String message = e.getBindingResult().getFieldError().getDefaultMessage();
+    return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+  }
 
 }
