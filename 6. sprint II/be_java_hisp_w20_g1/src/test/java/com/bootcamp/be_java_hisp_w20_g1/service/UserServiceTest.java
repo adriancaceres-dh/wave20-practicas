61f26b8082c0ad1c93c1a95c43e3d9dcd20466e6
brumen18 <agustin.iglesias@mercadolibre.com>
2023-01-16 11:58:07 -0300

    Se modificaron los nombres de los t3 y t4 para que se ajusten a la convención de nombres.

--- 6. sprint II/be_java_hisp_w20_g1/src/test/java/com/bootcamp/be_java_hisp_w20_g1/service/UserServiceTest.java
+++ 6. sprint II/be_java_hisp_w20_g1/src/test/java/com/bootcamp/be_java_hisp_w20_g1/service/UserServiceTest.java
@@ -86,7 +86,7 @@ class UserServiceTest {
 
     @Test
     @DisplayName("T4 se obtiene lista de los seguidores de un usuario ordenada ascendentemente.")
-    public void shouldGetSellersFollowersDtoOrderedByAsc() {
+    public void whenGivingAnUserIdAndAscendingOrderByQueryParam_getSellerFollowers_ShouldGetAnAscendentListOfFollowers() {
 
         // Arrange
         User user = TestUtil.getUserWithFollowers(true);
@@ -99,14 +99,15 @@ class UserServiceTest {
 
         UserFollowersResponseDto expectedResult = TestUtil.getUserFollowersResponseDto(user, user1, user2);
 
-        //Act y Assert
+        // Act
         UserFollowersResponseDto actual = userService.getSellerFollowersDto(1, Parameter.getString("NameAsc"));
+        // Assert 
         assertEquals(expectedResult, actual);
     }
 
     @Test
     @DisplayName("T4 se obtiene lista de los seguidores de un usuario ordenada descendente.")
-    public void shouldGetSellersFollowersDtoOrderedByDesc() {
+    public void whenGivingAnUserIdAndDescendingOrderByQueryParam_getSellerFollowers_ShouldGetADescendentListOfFollowers() {
 
         // Arrange
         User user = TestUtil.getUserWithFollowers(true);
@@ -119,14 +120,15 @@ class UserServiceTest {
 
         UserFollowersResponseDto expectedResult = TestUtil.getUserFollowersResponseDto(user, user2, user1);
 
-        //Act y Assert
+        // Act
         UserFollowersResponseDto actual = userService.getSellerFollowersDto(1, Parameter.getString("NameDesc"));
+        // Assert
         assertEquals(expectedResult, actual);
     }
 
     @Test
     @DisplayName("T4 se obtiene lista de los que un usuario sigue ordenada ascendente.")
-    public void shouldGetSellersFollowedDtoOrderedByAsc() {
+    public void whenGivingAnUserIdAndAscendingOrderByQueryParam_getSellerFolloweds_ShouldGetAnAscendentListOfFolloweds() {
 
         // Arrange
         User user = TestUtil.getUserWithFollowed(true);
@@ -139,14 +141,15 @@ class UserServiceTest {
 
         UserFollowedResponseDto expectedResult = TestUtil.getUserFollowedResponseDto(user, user1, user2);
 
-        //Act y Assert
+        // Act
         UserFollowedResponseDto actual = userService.getFollowedDto(1, Parameter.getString("NameAsc"));
+        // Assert
         assertEquals(expectedResult, actual);
     }
 
     @Test
     @DisplayName("T4 se obtiene lista de los que un usuario sigue ordenada descendente.")
-    public void shouldGetSellersFollowedDtoOrderedByDesc() {
+    public void whenGivingAnUserIdAndDescendentOrderByQueryParam_getSellerFolloweds_ShouldGetADescendentListOfFolloweds() {
 
         // Arrange
         User user = TestUtil.getUserWithFollowed(true);
@@ -159,39 +162,39 @@ class UserServiceTest {
 
         UserFollowedResponseDto expectedResult = TestUtil.getUserFollowedResponseDto(user, user2, user1);
 
-        //Act y Assert
+        // Act
         UserFollowedResponseDto actual = userService.getFollowedDto(1, Parameter.getString("NameDesc"));
+        // Assert
         assertEquals(expectedResult, actual);
     }
 
-
     @Test
     @DisplayName("T3 Se lanza excepción si el orden es inválido en getFollowed.")
-    public void shouldThrowExceptionIfOrderDoesNotExistWhenCallingGetFollowed() {
+    public void whenAnInvalidContentOfQueryParamIsReceived_getFolloweds_ShouldThrowException() {
         // Arrange
         User user = TestUtil.getUserWithFollowed(true);
 
         when(userRepository.getUserById(1)).thenReturn(user);
 
-        //Act y Assert
+        // Act y Assert
         assertThrows(InvalidQueryParamValueException.class, () -> userService.getFollowedDto(1, Parameter.getString("InvalidOrder")));
     }
 
     @Test
     @DisplayName("T3 Se lanza excepción si el orden es inválido en getSellerFollowers.")
-    public void shouldThrowExceptionIfOrderDoesNotExistWhenCallingGetSellerFollowers() {
+    public void whenAnInvalidContentOfQueryParamIsReceived_getFollowers_ShouldThrowException() {
         // Arrange
         User user = TestUtil.getUserWithFollowed(true);
 
         when(userRepository.getUserById(1)).thenReturn(user);
 
-        //Act y Assert
+        // Act y Assert
         assertThrows(InvalidQueryParamValueException.class, () -> userService.getSellerFollowersDto(1, Parameter.getString("InvalidOrder")));
     }
 
     @Test
     @DisplayName("T3 se obtiene lista de los que un usuario sigue ordenada ascendentemente (por defecto).")
-    public void shouldGetSellersFollowedDtoOrdered() {
+    public void whenNoQueryParamIsReceived_getFollowed_ShouldOrderTheListAscendently() {
 
         // Arrange
         User user = TestUtil.getUserWithFollowed(true);
@@ -202,17 +205,17 @@ class UserServiceTest {
         when(userRepository.getUserById(2)).thenReturn(user1);
         when(userRepository.getUserById(3)).thenReturn(user2);
 
-
         UserFollowedResponseDto expectedResult = TestUtil.getUserFollowedResponseDto(user, user1, user2);
 
-        //Act y Assert
+        // Act
         UserFollowedResponseDto actual = userService.getFollowedDto(1, null);
+        // Assert
         assertEquals(expectedResult, actual);
     }
 
     @Test
     @DisplayName("T3 se obtiene lista de seguidores de un usuario ordenada ascendentemente (por defecto).")
-    public void shouldGetSellersFollowersDtoOrdered() {
+    public void whenNoQueryParamIsReceived_getFollowers_ShouldOrderTheListAscendently() {
 
         // Arrange
         User user = TestUtil.getUserWithFollowers(true);
@@ -225,8 +228,9 @@ class UserServiceTest {
 
         UserFollowersResponseDto expectedResult = TestUtil.getUserFollowersResponseDto(user, user1, user2);
 
-        //Act y Assert
+        // Act
         UserFollowersResponseDto actual = userService.getSellerFollowersDto(1, null);
+        // Assert
         assertEquals(expectedResult, actual);
     }
 
@@ -234,16 +238,16 @@ class UserServiceTest {
     @DisplayName("T7 se obtiene cantidad de seguidores que tiene un vendedor")
     void shouldReturnRealAmountOfFollowers() {
 
-        //Arrange
+        // Arrange
         int id = 1;
         int expected = 2;
 
         when(userRepository.getUserById(id)).thenReturn(TestUtil.getUserWithFollowers(true));
 
-        //Act
+        // Act
         int actual = userService.getFollowersCountDto(id).getFollowersCount();
 
-        //Assert
+        // Assert
         assertEquals(expected, actual);
     }
 
@@ -251,16 +255,16 @@ class UserServiceTest {
     @DisplayName("T7 se lanza excepcion si el usuario a consultar no existe")
     void whenGetAnInvalidUserId_ThenThrowNotFoundException() {
 
-        //Arrange
+        // Arrange
         int id = 1;
         String expectedMessage = Parameter.getString("EX_NotExistentUser");
 
         when(userRepository.getUserById(id)).thenReturn(null);
 
-        //Act
+        // Act
         NotFoundException actual = assertThrows(NotFoundException.class, () -> userService.getFollowersCountDto(id).getFollowersCount());
 
-        //Assert
+        // Assert
         assertEquals(actual.getMessage(), expectedMessage);
     }
 
@@ -268,16 +272,16 @@ class UserServiceTest {
     @DisplayName("T7 se lanza excepcion si el usuario a consultar no es vendedor")
     void ifIsNotASeller_ThenThrowBadRequestException() {
 
-        //Arrange
+        // Arrange
         int id = 1;
         String expectedMessage = Parameter.getString("EX_NotASeller");
 
         when(userRepository.getUserById(id)).thenReturn(TestUtil.getUserWithFollowers(false));
 
-        //Act
+        // Act
         BadRequestException actual = assertThrows(BadRequestException.class, () -> userService.getFollowersCountDto(id).getFollowersCount());
 
-        //Assert
+        // Assert
         assertEquals(actual.getMessage(), expectedMessage);
     }
 }
