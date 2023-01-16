d79cf3e2bbc9df7bfc5517a54d99159eea7b0e7b
brumen18 <agustin.iglesias@mercadolibre.com>
2023-01-16 17:03:48 -0300

    Añadido método de test para campos inválidos (por ejemplo, el id del usuario es inválido, el precio está mal, etc).

--- 6. sprint II/be_java_hisp_w20_g1_Iglesias/be_java_hisp_w20_g1/src/test/java/com/bootcamp/be_java_hisp_w20_g1/integrationTest.java
+++ 6. sprint II/be_java_hisp_w20_g1_Iglesias/be_java_hisp_w20_g1/src/test/java/com/bootcamp/be_java_hisp_w20_g1/integrationTest.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -59,7 +60,33 @@ public class integrationTest {
 
         MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/products/post").contentType(MediaType.APPLICATION_JSON).content(payloadJson)).andDo(print()).andExpect(status().isCreated())
                 .andReturn();
-        System.out.println(payloadJson);
+
     }
 
+    @Test
+    void shouldReturnAnInvalidParam() throws Exception {
+        // Vamos a ponerle un precio indebido al producto y vamos a testear que dé error.
+        // Esto se puede hacer para todos los campos, para asegurar que todas las validaciones estén OK.
+
+        PostRequestDto payloadDto = new PostRequestDto(); // creamos nuestro objeto dto y empezamos a preparar nuestro payload para enviárselo al método.
+        LocalDate fecha = LocalDate.now();
+        payloadDto.setDate(fecha);
+        payloadDto.setUserId(0);
+        ProductRequestDto product = new ProductRequestDto();
+        product.setProductId(20);
+        product.setProductName("Galletas");
+        product.setType("Alimentos");
+        product.setBrand("Oreo");
+        product.setColor("Marrones");
+        product.setNotes("Deliciosas galletas");
+        payloadDto.setProduct(product);
+        payloadDto.setCategory(4);
+        payloadDto.setPrice(0.0);
+        ObjectWriter writer = new ObjectMapper().registerModule(new JavaTimeModule()).configure(SerializationFeature.WRAP_ROOT_VALUE, false).writer();
+        String payloadJson = writer.writeValueAsString(payloadDto); // convertimos nuestro objeto dto a String.
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/products/post").contentType(MediaType.APPLICATION_JSON).content(payloadJson)).andDo(print())
+                .andExpect(status().isBadRequest()).andReturn();
+
+    }
 }
