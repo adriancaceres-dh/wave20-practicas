e0fc26f6479cdb6c9e19ddf45251ab45a56bdd5c
flavio-ambroggio-meli <flavio.ambroggio@mercadolibre.com>
2023-01-17 09:02:01 -0300

    agregue proyecto grupal e individual

---
+++ 6. sprint II/Grupal/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/repository/interfaces/IUserRepository.java
@@ -0,0 +1,6 @@
+package com.bootcamp.be_java_hisp_w20_g2.repository.interfaces;
+
+import com.bootcamp.be_java_hisp_w20_g2.model.User;
+
+public interface IUserRepository extends GenericRepository<User, Integer> {
+}
---
+++ 6. sprint II/Grupal/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/utils/sort/PostStreamSorter.java
@@ -0,0 +1,27 @@
+package com.bootcamp.be_java_hisp_w20_g2.utils.sort;
+
+
+import com.bootcamp.be_java_hisp_w20_g2.model.Post;
+
+import java.util.Comparator;
+import java.util.HashMap;
+
+public class PostStreamSorter {
+    private static final HashMap<String, Comparator<Post>> sortCriterias = new HashMap<>();
+    private static final Comparator<Post> defaultCriteria = Comparator.comparing(Post::getDate).reversed();
+
+    static {
+        sortCriterias.put("date_asc", Comparator.comparing(Post::getDate));
+        sortCriterias.put("date_desc", defaultCriteria);
+        sortCriterias.put("price_asc", Comparator.comparing(Post::getPrice));
+        sortCriterias.put("price_desc", Comparator.comparing(Post::getPrice).reversed());
+    }
+
+    public static Comparator<Post> getSortCriteria(String criteriaString) {
+        return sortCriterias.getOrDefault(criteriaString, defaultCriteria);
+    }
+
+    public static boolean isValid(String sortCriteria) {
+        return sortCriterias.containsKey(sortCriteria);
+    }
+}
