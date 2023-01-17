5b16c37874b962cbcecd7363528ca4b3880d7c76
Daniela Villamizar Mendoza <daniela.villamizar@mercadolibre.com.co>
2023-01-17 10:59:09 -0500

    Se agrega el test de integración del endpoint de dar de alta publicacion

--- 6. sprint II/Desafio Individual/be_java_hisp_w20_g6_villamizar/src/test/java/com/bootcamp/be_java_hisp_w20_g6_villamizar/integration/PostControllerTest.java
+++ 6. sprint II/Desafio Individual/be_java_hisp_w20_g6_villamizar/src/test/java/com/bootcamp/be_java_hisp_w20_g6_villamizar/integration/PostControllerTest.java
@@ -19,6 +19,31 @@ class PostControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Test
+    @DisplayName("US0002 - TI0004, Verificar registro de publicación ")
+    void savePostValid() throws Exception {
+        this.mockMvc.perform(
+                        MockMvcRequestBuilders.post("/products/post")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{" +
+                                        "\"user_id\": 3," +
+                                        "\"date\": \"24-12-2022\"," +
+                                        "\"product\": {" +
+                                        "\"product_id\": 1," +
+                                        "\"product_name\": \"Silla normal\"," +
+                                        "\"type\": \"Gamer\"," +
+                                        "\"brand\": \"Racer\"," +
+                                        "\"color\": \"Red Black\"," +
+                                        "\"notes\": \"Special Edition\"" +
+                                        "}," +
+                                        "\"category\": 100," +
+                                        "\"price\": 1500.50" +
+                                        "}"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andExpect(jsonPath("$").value(true));
+    }
 
 }
 
