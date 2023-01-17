2c0d5e63ce723921bc27e8c0ea951afeda8c0d4f
IvanMajewski1 <ivan.majewski@mercadolibre.com>
2023-01-17 12:33:33 -0300

    Subo clase IntegrationTestPublicationController

---
+++ 6. sprint II/be_java_hisp_w20_g4_majewski/src/test/java/com/bootcamp/be_java_hisp_w20_g4/integration/IntegrationTestPublicationController.java
@@ -0,0 +1,40 @@
+package com.bootcamp.be_java_hisp_w20_g4.integration;
+
+import com.bootcamp.be_java_hisp_w20_g4.dto.request.PostDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.request.ProductRequestDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.product.ProductResponseDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.publication.PublicationDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IntegrationTestPublicationController {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+   
+}
