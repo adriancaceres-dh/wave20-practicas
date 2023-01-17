a141bcab07efa86a0b1844d9b8db6d762c3fbe42
Gabi-S <gabriela.stocco@mercadolibre.com>
2023-01-17 16:30:32 -0300

    Se agregan descripciones en el controlador de usuarios

--- 6. sprint II/be_java_hisp_w20_g4_stocco/src/test/java/com/bootcamp/be_java_hisp_w20_g4/controller/UserControllerIntegrationTest.java
+++ 6. sprint II/be_java_hisp_w20_g4_stocco/src/test/java/com/bootcamp/be_java_hisp_w20_g4/controller/UserControllerIntegrationTest.java
@@ -1,16 +1,11 @@
 package com.bootcamp.be_java_hisp_w20_g4.controller;
 
 import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.ListedUserDTO;
-import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserDTO;
 import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserFollowedDTO;
-import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserFollowersDTO;
-import com.bootcamp.be_java_hisp_w20_g4.model.Buyer;
-import com.bootcamp.be_java_hisp_w20_g4.model.Seller;
-import com.bootcamp.be_java_hisp_w20_g4.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -20,10 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
 import java.util.Arrays;
-import java.util.List;
-
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -35,8 +27,8 @@ class UserControllerIntegrationTest {
 @Autowired
 private MockMvc mockMvc;
     @Test
+    @DisplayName("Se prueba que un usuario pueda seguir a un vendedor")
     void follow() throws  Exception{
-
         this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}", 1,2))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().contentType("application/json"))
@@ -45,6 +37,7 @@ private MockMvc mockMvc;
                 .andReturn();
     }
     @Test
+    @DisplayName("Se lanza excepción cuando un usuario inválido intenta seguir a un seller inválido")
     void followException() throws Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}", 10,20))
                 .andDo(print()).andExpect(status().isBadRequest())
@@ -55,6 +48,7 @@ private MockMvc mockMvc;
     }
 
     @Test
+    @DisplayName("Se prueba contabilizar los seguidores de un usuario")
     void followersCount() throws  Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list",1))
                 .andDo(print()).andExpect(status().isOk())
@@ -63,6 +57,7 @@ private MockMvc mockMvc;
                 .andReturn();
     }
     @Test
+    @DisplayName("Se lanza excepción cuando se intenta contabilizar los seguidores de un usuario con id no válido")
     void followersCountException() throws  Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list",99))
                 .andDo(print()).andExpect(status().isBadRequest())
@@ -70,6 +65,7 @@ private MockMvc mockMvc;
                 .andReturn();
     }
     @Test
+    @DisplayName("Se lanza excepción cuando se pasa un id vacío e intentar contabilizar sus seguidores")
     void followersCountNullException() throws  Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list",""))
                 .andDo(print()).andExpect(status().isNotFound())
@@ -78,6 +74,7 @@ private MockMvc mockMvc;
     }
 
     @Test
+    @DisplayName("Se prueba listar los seguidores de un usuario")
     void followers() throws  Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list", 1))
                 .andDo(print()).andExpect(status().isOk())
@@ -86,6 +83,7 @@ private MockMvc mockMvc;
                 .andReturn();
     }
     @Test
+    @DisplayName("Se lanza excepción cuando se intenta listar los seguidores de un usuario no válido")
     void followersException() throws  Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list", 99))
                 .andDo(print()).andExpect(status().isBadRequest())
@@ -94,6 +92,7 @@ private MockMvc mockMvc;
     }
 
     @Test
+    @DisplayName("Se prueba listar los vendedores seguidos por un usuario")
     void followed() throws Exception{
       MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list",2))
                 .andDo(print()).andExpect(status().isOk())
@@ -104,6 +103,7 @@ private MockMvc mockMvc;
     }
 
     @Test
+    @DisplayName("Se prueba dejas de seguir a un usuario")
     void unfollow() throws  Exception{
         ListedUserDTO listedUser = new ListedUserDTO(2,"ivan");
         UserFollowedDTO expected = new UserFollowedDTO(1,"rodri", Arrays.asList(listedUser));
@@ -122,6 +122,7 @@ private MockMvc mockMvc;
                 .andReturn();
     }
   @Test
+  @DisplayName("Se lanza excepción cuando se intenta dejar de seguir a un usuario que no forma parte de la lista")
     void unfollowException() throws Exception{
       this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/unfollow/{userIdToUnfollow}",2,1))
               .andDo(print()).andExpect(status().isBadRequest())
