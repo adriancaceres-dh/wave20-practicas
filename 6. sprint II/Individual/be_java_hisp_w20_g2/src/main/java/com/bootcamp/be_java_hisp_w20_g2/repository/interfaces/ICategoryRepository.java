f4fc162b12ad11b53bcca832c87c6f1e53eee98c
flavio-ambroggio-meli <flavio.ambroggio@mercadolibre.com>
2023-01-17 10:50:26 -0300

    borre proyecto individual incompleto

--- 6. sprint II/Individual/be_java_hisp_w20_g2/src/main/java/com/bootcamp/be_java_hisp_w20_g2/repository/interfaces/ICategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.be_java_hisp_w20_g2.repository.interfaces;
-
-import com.bootcamp.be_java_hisp_w20_g2.model.Category;
-
-import java.util.Optional;
-
-public interface ICategoryRepository extends GenericRepository<Category, Integer> {
-    Optional<Category> findByCode(int code);
-}
