c2fea5550be4237e6f22f4a0c44c24a0c98b8ad8
lpodio <lorenzo.podio@mercadolibre.com>
2023-01-17 13:46:19 -0300

    Sprint 2 parte individual: implementacion de test integradores sobre UserController, terminado y funcionando

--- 6. sprint II/be_java_hisp_w20_g2_podio/src/test/java/com/bootcamp/be_java_hisp_w20_g2_podio/controller/UserControllerTest.java
+++ 6. sprint II/be_java_hisp_w20_g2_podio/src/test/java/com/bootcamp/be_java_hisp_w20_g2_podio/controller/UserControllerTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -32,6 +33,20 @@ class UserControllerTest {
     }
 
     @Test
+    public void testFollowAlreadyFollowing() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}", 1,2))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(content().string("Ya esta siguiendo a ese usuario"));
+    }
+
+    @Test
+    public void testFollowCannotFollowYoursekf() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}", 1,1))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(content().string("No puedes seguirte a ti mismo"));
+    }
+
+    @Test
     public void testCountFollowersOk() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/count",2))
                 .andDo(print()).andExpect(status().isOk())
@@ -40,6 +55,64 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.followers_count").value(2));
     }
 
+    @Test
+    public void testCountFollowersUserNotFound() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/count",999))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(content().string("El usuario no existe"));
+    }
 
+    @Test
+    public void testGetFollowersByIdOk() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list",2))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.followers", hasSize(2)))
+                .andExpect(jsonPath("$.followers[0].user_name").value("Diego"));
+    }
+
+    @Test
+    public void testGetFollowersByIdUserNotFound() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list",999))
+                .andDo(print()).andExpect(status().isNotFound())
+                .andExpect(content().string("User not found"));
+    }
+
+    @Test
+    public void testGetFollowedByIdOk() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list",1))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.followed[0].user_name").value("Flavio"));
+    }
+
+    @Test
+    public void testGetFollowedByIdUserNotFound() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list",999))
+                .andDo(print()).andExpect(status().isNotFound())
+                .andExpect(content().string("User not found"));
+    }
+
+    @Test
+    public void testUnfollowOk() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/unfollow/{userIdToFollow}", 1,3))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string("Operación exitosa"));
+
+    }
+
+    @Test
+    public void testUnfollowUserNotFound() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/unfollow/{userIdToFollow}", 0,999))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(content().string("User not found"));
+
+    }
+
+    @Test
+    public void testUnfollowCannotUnfollowYourself() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/unfollow/{userIdToFollow}", 1,1))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(content().string("You can't unfollow yourself"));
+
+    }
 
 }
