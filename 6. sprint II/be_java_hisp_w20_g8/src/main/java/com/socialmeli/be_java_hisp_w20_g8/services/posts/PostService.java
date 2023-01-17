e011251d23152653d7544c391c22597904e3ec1c
luis-lopezgomez <luis.lopezgomez@mercadolibre.com.mx>
2023-01-17 11:39:57 -0600

    Added Javadoc to post and product services - Added the author field to the following methods: createPost, createProduct, getProductById

--- 6. sprint II/be_java_hisp_w20_g8/src/main/java/com/socialmeli/be_java_hisp_w20_g8/services/posts/PostService.java
+++ 6. sprint II/be_java_hisp_w20_g8/src/main/java/com/socialmeli/be_java_hisp_w20_g8/services/posts/PostService.java
@@ -53,6 +53,7 @@ public class PostService implements IPostService {
      * Creates a post in the repository from a PostRequestDTO base. It also calls for the creation of the product if it doesn't exist yet.
      * @param postRequestDTO the post to be created
      * @return a ResponseDTO with the result of the operation
+     * @Author: Luis Francisco López Gómez
      */
     @Override
     public ResponseDTO createPost(PostRequestDTO postRequestDTO) {
