c0638307c30c6d8a39e21944b241e080add6c00c
ale-espindola <alejandra.espindola@mercadolibre.com>
2023-01-17 15:37:02 -0300

    Fix en mensajes de validación en ProductDTO

--- 6. sprint II/be_java_hisp_w20_g2_espindola/src/main/java/com/bootcamp/be_java_hisp_w20_g2_espindola/dto/ProductDTO.java
+++ 6. sprint II/be_java_hisp_w20_g2_espindola/src/main/java/com/bootcamp/be_java_hisp_w20_g2_espindola/dto/ProductDTO.java
@@ -15,25 +15,25 @@ import javax.validation.constraints.*;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ProductDTO {
     @NotNull(message = "El id_post no puede estar vacío.") // Se paso a Integer para que pueda agarrar el @ e identificarlo como null.
-    @Positive(message = "El id_post debe ser mayor a cero.")
+    @Positive(message = "El id_product debe ser mayor a cero.")
     private Integer productId;
 
     @NotBlank(message = "El campo no puede estar vacío.")
-    @Size(max = 40,message = "La longitud no puede superar los 40 caracteres.")
+    @Size(max = 40,message = "La longitud del campo name no puede superar los 40 caracteres.")
     @Pattern(regexp = RegexConstants.NO_SPECIAL_CHARACTERS,message = "El campo no puede poseer caracteres especiales.")
     private String productName;
 
     @NotBlank(message = "El campo no puede estar vacio")
-    @Size(max = 15,message = "La longitud no puede superar los 15 caracteres.")
+    @Size(max = 15,message = "La longitud del campo type no puede superar los 15 caracteres.")
     @Pattern(regexp = RegexConstants.NO_SPECIAL_CHARACTERS,message = "El campo no puede poseer caracteres especiales.") // Especificar en el Readme que caracteres se evaluan.
     private String type;
     private String brand;
 
     @NotBlank(message = "El campo no puede estar vacio")
-    @Size(max = 15,message = "La longitud no puede superar los 15 caracteres.")
+    @Size(max = 15,message = "La longitud del campo color no puede superar los 15 caracteres.")
     @Pattern(regexp = RegexConstants.NO_SPECIAL_CHARACTERS,message = "El campo no puede poseer caracteres especiales.")
     private String color;
-    @Size(max = 80,message = "La longitud no puede superar los 80 caracteres.")
+    @Size(max = 80,message = "La longitud del campo notes no puede superar los 80 caracteres.")
     @Pattern(regexp = RegexConstants.NO_SPECIAL_CHARACTERS,message = "El campo no puede poseer caracteres especiales.")
     private String notes;
 }
