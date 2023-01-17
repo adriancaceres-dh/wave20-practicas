5547f9cfa52d28dda19c837f28002b835996fdb4
itornati <ignacio.tornati@mercadolibre.com>
2023-01-17 11:00:01 -0300

    Se agrega proyecto grupal y se cambia estructura de carpetas

---
+++ 6. sprint II/proyecto_individual/be_java_hisp_w20_g03_tornati/src/main/java/com/socialMeli/be_java_hisp_w20_g03/dto/response/UserFollowersDTO.java
@@ -0,0 +1,20 @@
+package com.socialMeli.be_java_hisp_w20_g03.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.socialMeli.be_java_hisp_w20_g03.dto.request.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserFollowersDTO {
+    @JsonProperty("user_id")
+    private int userId;
+    @JsonProperty("user_name")
+    private String userName;
+    private List<UserDTO> followers;
+}
