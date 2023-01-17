23e6b931edba10656653a3ecbd0305bb0b3b1f09
DiegoAlejandroMalagon <diego.malagon@mercadolibre.com.co>
2023-01-17 15:59:29 -0500

    Delete 6. sprint II/be_java_hisp_w20_g8 directory

--- 6. sprint II/be_java_hisp_w20_g8/src/main/java/com/socialmeli/be_java_hisp_w20_g8/services/products/IProductService.java
+++
@@ -1,9 +0,0 @@
-package com.socialmeli.be_java_hisp_w20_g8.services.products;
-
-import com.socialmeli.be_java_hisp_w20_g8.dto.ProductDTO;
-
-public interface IProductService {
-    boolean createProduct(ProductDTO productDTO);
-
-    ProductDTO getProductById(int productId);
-}
