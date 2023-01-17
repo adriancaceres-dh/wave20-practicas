ce4f3d9bfa93ef8399ae308d695cbe1e9433afea
joaquinpereyrar <joaquin.pereyrarotulo@mercadolibre.com>
2023-01-17 16:24:08 -0300

    Se crea el test de integración para las publicaciones que sigue un usuario

--- 6. sprint II/be_java_hisp_w20_g4_pereyra/src/test/java/com/bootcamp/be_java_hisp_w20_g4_pereyra/integration/IntegrationTestPublicationService.java
+++ 6. sprint II/be_java_hisp_w20_g4_pereyra/src/test/java/com/bootcamp/be_java_hisp_w20_g4_pereyra/integration/IntegrationTestPublicationService.java
@@ -3,9 +3,13 @@ package com.bootcamp.be_java_hisp_w20_g4_pereyra.integration;
 import com.bootcamp.be_java_hisp_w20_g4_pereyra.dto.request.PostDTO;
 import com.bootcamp.be_java_hisp_w20_g4_pereyra.dto.request.ProductRequestDTO;
 import com.bootcamp.be_java_hisp_w20_g4_pereyra.dto.response.product.ProductResponseDTO;
+import com.bootcamp.be_java_hisp_w20_g4_pereyra.dto.response.product.ProductTwoWeeksResponseDTO;
+import com.bootcamp.be_java_hisp_w20_g4_pereyra.dto.response.publication.ListedPostDTO;
 import com.bootcamp.be_java_hisp_w20_g4_pereyra.dto.response.publication.PublicationDTO;
 
+import com.bootcamp.be_java_hisp_w20_g4_pereyra.model.Category;
 import com.bootcamp.be_java_hisp_w20_g4_pereyra.utils.TestUtils;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
@@ -17,11 +21,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -29,17 +35,24 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class IntegrationTestPublicationService {
     @Autowired
     MockMvc mockMvc;
 
     PostDTO postDTO;
     ProductRequestDTO productRequestDTO;
+    ProductResponseDTO productResponseDTO;
+    Category category1;
+    LocalDate dateForPost;
 
     @BeforeEach
     void setUp() {
+        dateForPost = LocalDate.now().minusDays(5);
         productRequestDTO = new ProductRequestDTO(1, "Silla Gamer", "Gamer", "Racer", "Red Black", "Special Edition");
-        postDTO = new PostDTO(3, LocalDate.of(2023, 1,1), productRequestDTO, 10, 1500.50);
+        productResponseDTO = new ProductResponseDTO(1, "Silla Gamer", "Gamer", "Racer", "Red Black", "Special Edition");
+        postDTO = new PostDTO(3, dateForPost, productRequestDTO, 10, 1500.50);
+        category1 = new Category(10, "football");
     }
 
     @Test
@@ -48,7 +61,7 @@ public class IntegrationTestPublicationService {
         String postDtoRequestJson = TestUtils.writer.writeValueAsString(postDTO);
 
         ProductResponseDTO productResponseDTO = new ProductResponseDTO(1, "Silla Gamer", "Gamer", "Racer", "Red Black", "Special Edition");
-        PublicationDTO publicationDtoExpected = new PublicationDTO(LocalDate.of(2023, 01, 01), productResponseDTO, 10, 1500.5);
+        PublicationDTO publicationDtoExpected = new PublicationDTO(dateForPost, productResponseDTO, 10, 1500.5);
         String expectedPublicationDto = TestUtils.writer.writeValueAsString(publicationDtoExpected);
 
         ResultMatcher expectedStatus = status().isOk();
@@ -64,5 +77,35 @@ public class IntegrationTestPublicationService {
                 .andExpect(expectedContentType);
     }
 
+    @Test
+    @DisplayName("Test de integración - obtener la publicaciones de un usuario de las ultimas 2 semanas")
+    public void getLast2WeeksPublicationTest() throws Exception {
+
+        ListedPostDTO expectedListedPostDTO = new ListedPostDTO(3,5, postDTO.getDate(), productResponseDTO, category1, postDTO.getPrice());
+        ProductTwoWeeksResponseDTO expectedProductTwoWeeksResponseDTO = new ProductTwoWeeksResponseDTO(1, List.of(expectedListedPostDTO));
+        String expectedResponse = TestUtils.writer.writeValueAsString(expectedProductTwoWeeksResponseDTO);
+
+        ResultMatcher expectedStatus = status().isOk();
+        ResultMatcher expectedJson = content().json(expectedResponse);
+        ResultMatcher expectedContentType = content().contentType(MediaType.APPLICATION_JSON);
+
+        // el usuario 1 sigue al usuario 3
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}",1,3)
+                .contentType(MediaType.APPLICATION_JSON));
+        // se crea una publicacion para el usuario 3
+        String postDtoRequestJson = TestUtils.writer.writeValueAsString(postDTO);
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/products/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(postDtoRequestJson));
+
+        // se consultan las publicaciones de los usuarios que sigue el usuario 1
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/products/followed/1/list")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(expectedStatus)
+                .andExpect(expectedJson)
+                .andExpect(expectedContentType);
+
+    }
 
 }
