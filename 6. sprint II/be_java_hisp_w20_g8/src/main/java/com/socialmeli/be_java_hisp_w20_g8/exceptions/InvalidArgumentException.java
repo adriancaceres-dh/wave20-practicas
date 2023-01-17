0000c0aa5100bea83f89edd6ff8abfb8de1ffc53
EdwardGs1702 <john.garcia@mercadolibre.com.co>
2023-01-17 15:39:44 -0500

    add project individual and grupal

--- 6. sprint II/be_java_hisp_w20_g8/src/main/java/com/socialmeli/be_java_hisp_w20_g8/exceptions/InvalidArgumentException.java
+++
@@ -1,8 +0,0 @@
-package com.socialmeli.be_java_hisp_w20_g8.exceptions;
-
-public class InvalidArgumentException extends RuntimeException {
-
-    public InvalidArgumentException(String message) {
-        super(message);
-    }
-}
