fd0469f1394c925d69dec6cd195cb07d683386ce
brumen18 <agustin.iglesias@mercadolibre.com>
2023-01-17 09:43:53 -0300

    Se modificó el proyecto añadiéndole un setup.
    Todavía falta acomodar bastante, la idea es que tenga un beforeAll en donde se inicialice una lista de posts, y luego que los siguientes métodos utilicen eso como capa de persistencia, pero aún toca acomodar el método de shouldInsertANewPost.

--- 6. sprint II/be_java_hisp_w20_g1_Iglesias/be_java_hisp_w20_g1/src/test/java/com/bootcamp/be_java_hisp_w20_g1/integrationTest.java
+++ 6. sprint II/be_java_hisp_w20_g1_Iglesias/be_java_hisp_w20_g1/src/test/java/com/bootcamp/be_java_hisp_w20_g1/integrationTest.java
@@ -2,12 +2,15 @@ package com.bootcamp.be_java_hisp_w20_g1;
 
 import com.bootcamp.be_java_hisp_w20_g1.dto.request.PostRequestDto;
 import com.bootcamp.be_java_hisp_w20_g1.dto.request.ProductRequestDto;
+import com.bootcamp.be_java_hisp_w20_g1.model.Post;
+import com.bootcamp.be_java_hisp_w20_g1.model.User;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +33,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -37,6 +42,60 @@ import java.time.LocalDateTime;
 public class integrationTest {
     @Autowired
     private MockMvc mockMvc;
+
+    private List<PostRequestDto> posts;
+
+    @BeforeAll
+    public void setup() {
+        // en este método vamos a definir nuestro setup, es decir, los datos de test que vamos a utilizar en gran parte de nuestro proyecto.
+        PostRequestDto post = new PostRequestDto(); // creamos nuestro objeto dto y empezamos a preparar nuestro payload para enviárselo al método.
+        LocalDate date = LocalDate.now();
+        post.setDate(date);
+        post.setUserId(2);
+        ProductRequestDto product = new ProductRequestDto();
+        product.setProductId(20);
+        product.setProductName("Galletas");
+        product.setType("Alimentos");
+        product.setBrand("Oreo");
+        product.setColor("Marrones");
+        product.setNotes("Deliciosas galletas");
+        post.setProduct(product);
+        post.setCategory(4);
+        post.setPrice(50.0);
+        posts = new ArrayList<>();
+        posts.add(post);
+        post = new PostRequestDto();
+        LocalDate baddate = LocalDate.of(2022, 10, 29);
+        post.setDate(baddate);
+        post.setUserId(2);
+        product = new ProductRequestDto();
+        product.setProductId(21);
+        product.setProductName("Fideos Chinos");
+        product.setType("Alimentos");
+        product.setBrand("Wahuuu");
+        product.setColor("Marrones");
+        product.setNotes("Fideos feos");
+        post.setProduct(product);
+        post.setCategory(4);
+        post.setPrice(5000.0);
+        posts.add(post);
+
+        post = new PostRequestDto();
+        LocalDate yesterday = date.minusDays(1);
+        post.setDate(yesterday);
+        post.setUserId(2);
+        product = new ProductRequestDto();
+        product.setProductId(22);
+        product.setProductName("Televisores");
+        product.setType("Electordomesticos");
+        product.setBrand("Wahuuu");
+        product.setColor("Negros");
+        product.setNotes("Una tele trucha");
+        post.setProduct(product);
+        post.setCategory(5);
+        post.setPrice(5000000.10);
+        posts.add(post);
+    }
 
     @Test
     void shouldInsertANewPost() throws Exception {
