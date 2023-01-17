d524288993753d8db6cb2798a2e0c3258b0898ea
Sebastian Anttury <sebastian.anttury@mercadolibre.com.co>
2023-01-17 12:51:19 -0500

    Se copia el proyecto para empezar la parte individual

---
+++ 6. sprint II/be_java_hisp_w20_g7_anttury/src/main/java/com/bootcamp/be_java_hisp_w20_g7/entity/Post.java
@@ -0,0 +1,28 @@
+package com.bootcamp.be_java_hisp_w20_g7.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class Post {
+    private int userId;
+    private int postId;
+    @JsonFormat(pattern = "dd-MM-yyyy", shape = JsonFormat.Shape.STRING)
+    private LocalDate date;
+    private Product product;
+    private int category;
+    private double price;
+    private boolean hasPromo;
+    private double discount;
+
+}
---
+++ 6. sprint II/be_java_hisp_w20_g7_anttury/src/main/java/com/bootcamp/be_java_hisp_w20_g7/repository/PostRepository.java
@@ -0,0 +1,67 @@
+package com.bootcamp.be_java_hisp_w20_g7.repository;
+
+import com.bootcamp.be_java_hisp_w20_g7.entity.Post;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.Getter;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Repository
+public class PostRepository implements IPostRepository {
+
+    List<Post> posts;
+
+    public PostRepository() {
+        posts = loadData();
+        /*posts = new ArrayList<>();
+        LocalDate date = LocalDate.parse("2022-12-30");
+        Product product = new Product(3, "Silla Gamer", "Gamer", "Racer", "Black", "Special Edition");
+        Product product2 = new Product(3, "Silla Gamer", "Gamer", "Racer", "Pink", "Special Edition");
+        Post post = new Post(3, 1, date, product, 100, 2, false, 22.0);
+        Post post2 = new Post(3, 2, LocalDate.parse("2022-11-30"), product2, 100, 2, false, 22.0);
+        Post post3 = new Post(3, 3, LocalDate.parse("2023-01-01"), product2, 100, 2, false, 22.0);
+        posts.add(post);
+        posts.add(post2);
+        posts.add(post3);*/
+    }
+
+    @Override
+    public List<Post> findAll() {
+        return posts;
+    }
+
+    @Override
+    public boolean save(Post post) {
+        return posts.add(post);
+
+    }
+
+    public List<Post> loadData() {
+        List<Post> loadedData = new ArrayList<>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        try {
+            File file = ResourceUtils.getFile("./src/main/resources/posts.json");
+            loadedData = objectMapper.readValue(file, new TypeReference<List<Post>>() {
+            });
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Failed while initializing DB, check your resources files");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Failed while initializing DB, check your JSON formatting.");
+        }
+
+        return loadedData;
+    }
+}
