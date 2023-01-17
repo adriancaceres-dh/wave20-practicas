3b99d95539fa2ce89dc55d173f9fb75a5e11d66d
Sebastian Anttury <sebastian.anttury@mercadolibre.com.co>
2023-01-17 15:53:15 -0500

    Se agregan InTest para verificar los seguidores y seguidos

--- 6. sprint II/be_java_hisp_w20_g7_anttury/src/test/java/com/bootcamp/be_java_hisp_w20_g7/controller/IntegrationTestUserController.java
+++ 6. sprint II/be_java_hisp_w20_g7_anttury/src/test/java/com/bootcamp/be_java_hisp_w20_g7/controller/IntegrationTestUserController.java
@@ -1,14 +1,26 @@
 package com.bootcamp.be_java_hisp_w20_g7.controller;
 
 import com.bootcamp.be_java_hisp_w20_g7.dto.UserDto;
+import com.bootcamp.be_java_hisp_w20_g7.dto.response.UserFollowedDto;
+import com.bootcamp.be_java_hisp_w20_g7.dto.response.UserFollowersDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -21,7 +33,7 @@ class IntegrationTestUserController {
     @Test
     void testValidUserCountFollowers() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/count", 3))
-                .andDo(MockMvcResultHandlers.print())
+                .andDo(print())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$.user_id").value(3))
                 .andExpect(jsonPath("$.user_name").value("Soraya"))
@@ -31,11 +43,50 @@ class IntegrationTestUserController {
     @Test
     void testInvalidUserIdToCountFollowers() throws Exception {
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/count", 9))
-                .andDo(MockMvcResultHandlers.print())
+                .andDo(print())
                 .andExpect(status().isBadRequest())
                 .andExpect(content().contentType("application/json"))
                 .andExpect(jsonPath("$.name").value("UserNotFoundException"))
                 .andExpect(jsonPath("$.message").value("User Not Found"));
     }
 
+    @Test
+    void testValidUserFollowed() throws Exception {
+        UserFollowedDto userFollowedDto = new UserFollowedDto(5, "Nathalia",
+                new ArrayList<>(List.of(new UserDto(4, "Ronald"), new UserDto(2, "Manuel"))));
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+
+        String expected = writer.writeValueAsString(userFollowedDto);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list", 5)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(expected, mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    void testValidUserFollowers() throws Exception {
+        UserFollowersDto userFollowersDto = new UserFollowersDto(2, "Manuel",
+                new ArrayList<>(List.of(new UserDto(5, "Nathalia"))));
+
+        ObjectWriter writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+
+        String expected = writer.writeValueAsString(userFollowersDto);
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list", 2)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(expected, mvcResult.getResponse().getContentAsString());
+    }
 }
