62b0e8d947b845833c9f7163b57d634ef513a417
joaquinpereyrar <joaquin.pereyrarotulo@mercadolibre.com>
2023-01-17 15:26:22 -0300

    Se crea test de integración para obtener la lsita de usuario que sigue a un determinado usuario

--- 6. sprint II/be_java_hisp_w20_g4_pereyra/src/test/java/com/bootcamp/be_java_hisp_w20_g4_pereyra/integration/IntegrationTestUserService.java
+++ 6. sprint II/be_java_hisp_w20_g4_pereyra/src/test/java/com/bootcamp/be_java_hisp_w20_g4_pereyra/integration/IntegrationTestUserService.java
@@ -3,6 +3,7 @@ package com.bootcamp.be_java_hisp_w20_g4_pereyra.integration;
 
 import com.bootcamp.be_java_hisp_w20_g4_pereyra.dto.response.user.ListedUserDTO;
 import com.bootcamp.be_java_hisp_w20_g4_pereyra.dto.response.user.UserFollowedDTO;
+import com.bootcamp.be_java_hisp_w20_g4_pereyra.dto.response.user.UserFollowersDTO;
 import com.bootcamp.be_java_hisp_w20_g4_pereyra.utils.TestUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.DisplayName;
@@ -11,10 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -23,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class IntegrationTestUserService {
     @Autowired
     MockMvc mockMvc;
@@ -50,28 +54,28 @@ public class IntegrationTestUserService {
     }
 
     @Test
+    @DisplayName("Test de integración - Lista de seguidores de un usuario")
     public void followersUserTest() throws Exception {
         int idUser = 1;
         int idUserToFollow = 2;
 
-        ListedUserDTO expectedListedUserDTO = new ListedUserDTO(2, "ivan");
-        UserFollowedDTO expectedResponseDto = new UserFollowedDTO(1, "rodri", List.of(expectedListedUserDTO));
+        ListedUserDTO expectedListedUserDTO = new ListedUserDTO(1, "rodri");
+        UserFollowersDTO expectedResponseDto = new UserFollowersDTO(2, "ivan", List.of(expectedListedUserDTO));
         String expectedResponse = TestUtils.writer.writeValueAsString(expectedResponseDto);
+        System.out.println(expectedResponse);
 
         this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}",idUser,idUserToFollow)
                 .contentType(MediaType.APPLICATION_JSON));
-
         ResultMatcher expectedStatus = status().isOk();
         ResultMatcher expectedJson = content().json(expectedResponse);
         ResultMatcher expectedContentType = content().contentType(MediaType.APPLICATION_JSON);
 
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/followers/list",idUser)
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list",idUserToFollow)
                         .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
+                .andDo(print()).andDo(print())
                 .andExpect(expectedStatus)
                 .andExpect(expectedJson)
                 .andExpect(expectedContentType);
     }
-
 
 }
