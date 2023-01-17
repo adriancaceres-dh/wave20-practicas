ea49b516c6b70c63bb7f21bf365acf883260e6db
FrannBerthet <francisco.berthet@mercadolibre.com>
2023-01-17 00:54:03 -0300

    se crea paquete test de integracion PostController

---
+++ 6. sprint II/be_java_hisp_w20_g1_Berthet/src/test/java/com/bootcamp/be_java_hisp_w20_g1/integration/PostControllerIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.bootcamp.be_java_hisp_w20_g1.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class PostControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+
+
+}
