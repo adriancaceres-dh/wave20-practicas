0617574b04c76fad5517475edd3a9873da61b7f8
FlorSicre <flor.sicre@mercadolibre.com>
2023-01-17 15:18:16 -0300

    Se agregó el displayName a cada test. Nuevo test de integración con la excepción de la US-0002

--- 6. sprint II/individual_sicre/be_java_hisp_w20_g03sicre/src/test/java/com/socialMeli/be_java_hisp_w20_g03/integration/UserServiceTest.java
+++ 6. sprint II/individual_sicre/be_java_hisp_w20_g03sicre/src/test/java/com/socialMeli/be_java_hisp_w20_g03/integration/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.socialMeli.be_java_hisp_w20_g03.integration;
 
 import jdk.jfr.ContentType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,12 +22,22 @@ public class UserServiceTest {
     private MockMvc mockMvc;
 
     @Test
-    void getFollowedCount()throws Exception{
+    @DisplayName("T-Integracion sobre US-002")
+    void getFollowedCountOK()throws Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/count",234))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
                 .andExpect(MockMvcResultMatchers.jsonPath("followers_count").value(3))
                 .andExpect(MockMvcResultMatchers.jsonPath("user_id").value(234))
                 .andExpect(MockMvcResultMatchers.jsonPath("user_name").value("usuario1"));
+    }
+    @Test
+    @DisplayName("T-Integracion sobre US-002. Devuelve una excepción ")
+    void getFollowedCountUserNotFound()throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/count",000))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(MockMvcResultMatchers.content().string("El usuario ingresado no existe."));
+
     }
 }
