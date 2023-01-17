fe13d1a18c001171109bfc92cb7ee9a25f19aee2
FrannBerthet <francisco.berthet@mercadolibre.com>
2023-01-17 16:24:12 -0300

    Test de integracion UserController

---
+++ 6. sprint II/be_java_hisp_w20_g1_Berthet/src/test/java/com/bootcamp/be_java_hisp_w20_g1/integration/UserControllerIntegrationTest.java
@@ -0,0 +1,81 @@
+package com.bootcamp.be_java_hisp_w20_g1.integration;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.io.UnsupportedEncodingException;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Disabled
+public class UserControllerIntegrationTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void givenValidId_getUserFollowers_ShouldReturnUserFollowersList() throws Exception {
+        Integer validId = 2;
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list", validId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.user_name").value("zmohamedDohamed"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.followers").isNotEmpty())
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void givenInvalidId_getUserFollowers_ShouldThrowException() throws Exception {
+        Integer inexistentId = 99;
+        String expectedErrorMessage = "El usuario no existe";
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list", inexistentId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(expectedErrorMessage))
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void givenValidId_getUserFollowed_ShouldReturnUserFollowedList() throws Exception {
+        Integer validId = 2;
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list", validId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.user_name").value("zmohamedDohamed"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.followed").exists())
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+    @Test
+    public void givenInvalidId_getUserFollowed_ShouldThrowException() throws Exception {
+        Integer inexistentId = 99;
+        String expectedErrorMessage = "El usuario no existe";
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list", inexistentId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotFound())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value(expectedErrorMessage))
+                .andReturn();
+
+        System.out.println(mvcResult.getResponse().getContentAsString());
+    }
+
+
+}
