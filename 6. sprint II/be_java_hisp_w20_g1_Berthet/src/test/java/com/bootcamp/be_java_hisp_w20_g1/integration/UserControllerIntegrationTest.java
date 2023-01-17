c9f07bb79c584a997c83821be32a38126ccc8994
FrannBerthet <francisco.berthet@mercadolibre.com>
2023-01-17 16:51:04 -0300

    fix se quita annotation @disabled

--- 6. sprint II/be_java_hisp_w20_g1_Berthet/src/test/java/com/bootcamp/be_java_hisp_w20_g1/integration/UserControllerIntegrationTest.java
+++ 6. sprint II/be_java_hisp_w20_g1_Berthet/src/test/java/com/bootcamp/be_java_hisp_w20_g1/integration/UserControllerIntegrationTest.java
@@ -17,7 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Disabled
 public class UserControllerIntegrationTest {
 
     @Autowired
