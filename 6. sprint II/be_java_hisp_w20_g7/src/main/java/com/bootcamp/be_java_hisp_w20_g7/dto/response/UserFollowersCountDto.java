5f0a56a3b62dff35cf56186fe9445fdda6bc2312
Manuel Stiven Gonzalez Mahecha <119961005+steveng45@users.noreply.github.com>
2023-01-17 18:24:09 -0500

    se añade test de integracion verificando que el usuario exista, pendiente terminar el readme

---
+++ 6. sprint II/be_java_hisp_w20_g7/src/main/java/com/bootcamp/be_java_hisp_w20_g7/dto/response/UserFollowersCountDto.java
@@ -0,0 +1,18 @@
+package com.bootcamp.be_java_hisp_w20_g7.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserFollowersCountDto {
+
+    private int userId;
+
+    private String userName;
+
+    private int followersCount;
+
+}
