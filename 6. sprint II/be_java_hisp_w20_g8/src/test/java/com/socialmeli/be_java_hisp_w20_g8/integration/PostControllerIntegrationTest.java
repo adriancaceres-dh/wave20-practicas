f9e68a8c90bbc3fc551faf1f371237346faebed2
luis-lopezgomez <luis.lopezgomez@mercadolibre.com.mx>
2023-01-17 11:27:14 -0600

    Modified the post controller integration tests - Added an optional annotation

--- 6. sprint II/be_java_hisp_w20_g8/src/test/java/com/socialmeli/be_java_hisp_w20_g8/integration/PostControllerIntegrationTest.java
+++ 6. sprint II/be_java_hisp_w20_g8/src/test/java/com/socialmeli/be_java_hisp_w20_g8/integration/PostControllerIntegrationTest.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,6 +28,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class PostControllerIntegrationTest {
 
     @Autowired
