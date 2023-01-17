7d91619b679d870675c81272fb582fa8fee17090
karenhoffman <karen.hoffman@mercadolibre.com>
2023-01-17 09:51:27 -0300

    Se agrega clase para test de integracion sobre controlador de usuario

---
+++ 6. sprint II/be_java_hisp_w20_g1_hoffman/src/test/java/com/bootcamp/be_java_hisp_w20_g1_hoffman/integracion/UserControllerTest.java
@@ -0,0 +1,11 @@
+package com.bootcamp.be_java_hisp_w20_g1_hoffman.integracion;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    
+}
