572d0f2d79af5e977d9b7b735a246c752aa5d98b
easprea <emiliano.asprea@mercadolibre.com>
2023-01-17 11:16:35 -0300

    agregando integration test de funcionalidad follow()

---
+++ 6. sprint II/be_java_hisp_w20_g4/src/test/java/com/bootcamp/be_java_hisp_w20_g4/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,62 @@
+package com.bootcamp.be_java_hisp_w20_g4.controller;
+
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.ListedUserDTO;
+import com.bootcamp.be_java_hisp_w20_g4.dto.response.user.UserFollowedDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import java.util.Arrays;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerIntegrationTest {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    MockMvc mockMvc;
+
+    private static ObjectWriter writer;
+
+    @BeforeAll
+    public static void setUp() {
+        writer = new ObjectMapper()
+                .configure(SerializationFeature.WRAP_ROOT_VALUE, false)
+                .writer();
+
+    }
+
+
+    @Test
+    void followTest() throws Exception {
+        ListedUserDTO listedUser = new ListedUserDTO(2,"ivan");
+        UserFollowedDTO expected = new UserFollowedDTO(1,"rodri", Arrays.asList(listedUser));
+
+        ObjectWriter mapper = new ObjectMapper().configure(SerializationFeature.WRAP_ROOT_VALUE,false).writer();
+
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/users/1/follow/2")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(content().contentType("application/json"))
+                .andReturn();
+
+        assertEquals(mapper.writeValueAsString(expected),mvcResult.getResponse().getContentAsString());
+    }
+
+}
