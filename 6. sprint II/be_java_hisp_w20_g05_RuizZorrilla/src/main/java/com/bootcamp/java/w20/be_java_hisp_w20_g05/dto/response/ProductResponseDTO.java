0df6d169e924bb0148fcca98881dfb0f7490a84e
andrearuiz-ml <andrea.ruiz@mercadolibre.com>
2023-01-17 12:30:13 -0300

    add individual project

---
+++ 6. sprint II/be_java_hisp_w20_g05_RuizZorrilla/src/main/java/com/bootcamp/java/w20/be_java_hisp_w20_g05/dto/response/ProductResponseDTO.java
@@ -0,0 +1,21 @@
+package com.bootcamp.java.w20.be_java_hisp_w20_g05.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductResponseDTO {
+    private int id;
+    private String name;
+    private String type;
+    private String brand;
+    private String color;
+    private String notes;
+
+}
