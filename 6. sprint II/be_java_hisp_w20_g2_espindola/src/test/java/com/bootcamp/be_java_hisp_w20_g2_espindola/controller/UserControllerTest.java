05743a9cb99974185d6af7363595d9a12efe31fc
ale-espindola <alejandra.espindola@mercadolibre.com>
2023-01-17 13:36:07 -0300

    Desarrollo individual - UserControllerTest con todos los test de integración incluyendo los casos borde y excepciones, con un coverage de 81% de líneas

--- 6. sprint II/be_java_hisp_w20_g2_espindola/src/test/java/com/bootcamp/be_java_hisp_w20_g2_espindola/controller/UserControllerTest.java
+++ 6. sprint II/be_java_hisp_w20_g2_espindola/src/test/java/com/bootcamp/be_java_hisp_w20_g2_espindola/controller/UserControllerTest.java
@@ -1,5 +1,7 @@
 package com.bootcamp.be_java_hisp_w20_g2_espindola.controller;
 
+import com.bootcamp.be_java_hisp_w20_g2_espindola.exception.BadRequestException;
+import com.bootcamp.be_java_hisp_w20_g2_espindola.exception.UserNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +25,7 @@ class UserControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("Happy path - It should be able to follow a new user")
+    @DisplayName("Follow User - It should be able to follow a new user")
     void testFollowUserOk() throws Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}", 1, 3))
                 .andDo(print()).andExpect(status().isOk())
@@ -31,7 +33,34 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("Happy path - It should return the number of followers for a given user Id")
+    @DisplayName("Follow User - It should throw an exception when try to follow an already following user")
+    void testFollowUserAlreadyFollowing() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}", 1, 2))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof BadRequestException))
+                .andExpect(content().string("Ya esta siguiendo a ese usuario"));
+    }
+
+    @Test
+    @DisplayName("Follow User - It should throw an exception when try to follow yourself")
+    void testFollowUserFollowYourself() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}", 1, 1))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof BadRequestException))
+                .andExpect(content().string("No puedes seguirte a ti mismo"));
+    }
+
+    @Test
+    @DisplayName("Follow User - It should throw an exception when try to follow with an invalid user id")
+    void testFollowUserNotValid() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/follow/{userIdToFollow}", 1, 555))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof BadRequestException))
+                .andExpect(content().string("Alguno de los usuarios no existe"));
+    }
+
+    @Test
+    @DisplayName("Get Followers Count - It should return the number of followers for a given user Id")
     void testCountFollowersOk() throws Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/count", 2))
                 .andDo(print()).andExpect(status().isOk())
@@ -41,7 +70,16 @@ class UserControllerTest {
     }
 
     @Test
-    @DisplayName("Happy path - It should return the list of followers for a given user Id, unordered")
+    @DisplayName("Get Followers Count - It should throw an exception when given an invalid user Id")
+    void testCountFollowersBadRequest() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/count", 666))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof BadRequestException))
+                .andExpect(content().string("El usuario no existe"));
+    }
+
+    @Test
+    @DisplayName("Find All Followers - It should return the list of followers for a given user Id, unordered")
     void testGetFollowersByIdOk() throws Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list", 2))
                 .andDo(print()).andExpect(status().isOk())
@@ -49,10 +87,46 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.followers[0].user_name").value("Diego"));
     }
 
-    //hacer las bad request y los de ordenamiento
+    @Test
+    @DisplayName("Find All Followers - It should throw an exception when given an invalid user Id")
+    void testGetFollowersByBadRequest() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list", 555))
+                .andDo(print()).andExpect(status().isNotFound())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof UserNotFoundException))
+                .andExpect(content().string("User not found"));
+    }
 
     @Test
-    @DisplayName("Happy path - It should return the list of followed for a given user Id, unordered")
+    @DisplayName("Find All Followers - It should return the list of followers for a given user Id, ascendant order")
+    void testGetFollowersByIdOrderAsc() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list", 2).param("order","name_asc"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.followers", hasSize(2)))
+                .andExpect(jsonPath("$.followers[1].user_name").value("Diego"))
+                .andExpect(jsonPath("$.followers[0].user_name").value("Ale"));
+    }
+
+    @Test
+    @DisplayName("Find All Followers - It should return the list of followers for a given user Id, descendant order")
+    void testGetFollowersByIdOrderDesc() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list", 2).param("order","name_desc"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.followers", hasSize(2)))
+                .andExpect(jsonPath("$.followers[0].user_name").value("Diego"))
+                .andExpect(jsonPath("$.followers[1].user_name").value("Ale"));
+    }
+
+    @Test
+    @DisplayName("Find All Followers - It should throw an exception when given an invalid order criteria")
+    void testGetFollowersByIdOrderNotValid() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followers/list", 2).param("order","anything"))
+                .andDo(print()).andExpect(status().isBadRequest())
+                //.andExpect(result -> assertTrue(result.getResolvedException() instanceof BadRequestException))
+                .andExpect(content().string("Invalid User sort criteria"));
+    }
+
+    @Test
+    @DisplayName("Find All Followed - It should return the list of followed for a given user Id, unordered")
     void testGetFollowedByIdOk() throws Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list", 1))
                 .andDo(print()).andExpect(status().isOk())
@@ -62,11 +136,62 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.followed[0].user_name").value("Flavio"));
     }
 
+    /*
+    * Para probar el ordenamiento no contamos con más usuarios generados en los respositorios,
+    * el desarrollo de los tests sería, en este caso, el mismo para el visto con los test de Find All followers
+    */
+
     @Test
-    @DisplayName("Happy path - It should unfollow a user from a given user Id to another given user Id to unfollow")
+    @DisplayName("Find All Followed - It should throw an exception when given an invalid user Id")
+    void testGetFollowedByBadRequest() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list", 555))
+                .andDo(print()).andExpect(status().isNotFound())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof UserNotFoundException))
+                .andExpect(content().string("User not found"));
+    }
+
+    @Test
+    @DisplayName("Find All Followed - It should throw an exception when given an invalid order criteria")
+    void testGetFollowedByIdOrderNotValid() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/users/{userId}/followed/list", 2).param("order","anything"))
+                .andDo(print()).andExpect(status().isBadRequest())
+                //.andExpect(result -> assertTrue(result.getResolvedException() instanceof BadRequestException))
+                .andExpect(content().string("Invalid User sort criteria"));
+    }
+
+    @Test
+    @DisplayName("Unfollow User - It should unfollow a user from a given user Id to another given user Id to unfollow")
     void testUnfollowUserOk() throws Exception{
         this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/unfollow/{userIdToUnfollow}", 1, 2))
                 .andDo(print()).andExpect(status().isOk())
                 .andExpect(content().string("Operación exitosa"));
     }
+
+    @Test
+    @DisplayName("Unfollow User - It should throw an exception when given an invalid user Id")
+    void testUnfollowUserBadRequest() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/unfollow/{userIdToUnfollow}", 777, 2))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof BadRequestException))
+                .andExpect(content().string("User not found"));
+    }
+
+    @Test
+    @DisplayName("Unfollow User - It should throw an exception when given an invalid user Id to unfollow")
+    void testUnfollowUserBadRequestII() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/unfollow/{userIdToUnfollow}", 1, 777))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof BadRequestException))
+                .andExpect(content().string("User to unfollow not found"));
+    }
+
+    @Test
+    @DisplayName("Unfollow User - It should throw an exception when users are no longer following each other")
+    void testUnfollowUserBadRequestIII() throws Exception{
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/users/{userId}/unfollow/{userIdToUnfollow}", 1, 3))
+                .andDo(print()).andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof BadRequestException))
+                .andExpect(content().string("Users no longer follow each other"));
+    }
+
 }
