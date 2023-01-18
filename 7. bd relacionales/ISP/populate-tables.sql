ed04c04ecd2528646a9a9155902c16726003d02d
ramosjpruiz <juanpablo.ramos@mercadolibre.com>
2023-01-18 18:04:34 -0300

    Adds Internet Service Provider exercise

---
+++ 7. bd relacionales/ISP/populate-tables.sql
@@ -0,0 +1,20 @@
+INSERT INTO plan
+VALUES
+(1, 10, 1000, 0),
+(2, 50, 3000, 0),
+(3, 100, 10000, 0.25),
+(4, 200, 15000, 0),
+(5, 300, 20000, 0.1);
+
+INSERT INTO cliente
+VALUES
+(40534986, "Pepito", "Pepitez", "1990-05-22", "Catamarca", "Catamarca", 1),
+(40774986, "Laura", "Lauriana", "1955-04-11", "Tucuman", "San Miguel de Tucuman", 3),
+(40535566, "Samuel", "Juarez", "2000-09-19", "Salta", "Salta", 4),
+(34533386, "Lautaro", "Salomon", "1988-11-17", "Tierra del Fuego", "Ushuaia", 3),
+(33694686, "Briana", "Perez", "2001-06-07", "Buenos Aires", "CABA", 5),
+(32535644, "Simone", "Gonzalez", "1975-05-15", "La Rioja", "Catamarca", 1),
+(29595436, "Mercedes", "Benz", "1998-02-22", "Buenos Aires", "Catamarca", 2),
+(35533586, "Carlos", "Samuel", "1990-07-21", "Chubut", "Rawson", 5),
+(44533799, "Ruben", "", "1995-06-29", "Buenos Aires", "Lobos", 3),
+(40537846, "Mateo", "Martes", "1960-10-02", "San Luis", "Catamarca", 2);
