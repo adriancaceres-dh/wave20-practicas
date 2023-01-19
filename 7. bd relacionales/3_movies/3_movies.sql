418fa124a088443443cdeddaec9dfe59b9492061
Jose Ivan Drozd <jose.drozd@mercadolibre.com>
2023-01-19 09:59:35 -0300

    Ejercicios parte 2 Vivo movies

---
+++ 7. bd relacionales/3_movies/3_movies.sql
@@ -0,0 +1,18 @@
+SELECT * FROM movies;
+SELECT first_name, last_name, rating FROM actors;
+SELECT title AS titulo FROM series AS series;
+SELECT first_name, last_name FROM actors WHERE rating>7.5;
+SELECT title, rating, awards FROM movies WHERE rating>7.5 AND awards>2;
+SELECT title, rating FROM movies ORDER BY rating ASC;
+SELECT title FROM movies LIMIT 3;
+SELECT title FROM movies ORDER BY rating DESC LIMIT 5;
+SELECT first_name, last_name FROM actors LIMIT 10;
+SELECT title, rating FROM movies WHERE title LIKE "%Toy Story%";
+SELECT first_name, last_name FROM actors WHERE first_name LIKE "Sam%";
+SELECT title FROM movies WHERE release_date BETWEEN "2004-01-01" AND "2008-01-01";
+
+SELECT title FROM movies 
+WHERE rating > 3 
+AND awards>1 
+AND release_date BETWEEN "1988-01-01" AND "2009-01-01"
+ORDER BY rating DESC;
