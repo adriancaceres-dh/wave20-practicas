8fdaf706678cb8fed2f2a38f504b161a94ed61a8
LuBenavides22 <luciano.benavides@mercadolibre.com>
2023-01-19 08:01:02 -0300

    Movies_db query 1 done

---
+++ 7. bd relacionales/movies_db.sql
@@ -0,0 +1,13 @@
+SELECT * FROM movies;
+SELECT first_name, last_name, rating FROM actors;
+SELECT title AS titulo FROM series;
+SELECT first_name, last_name FROM actors WHERE rating > 7.5;
+SELECT title, rating, awards FROM movies WHERE rating > 7.5 AND awards > 2;
+SELECT title, rating from movies ORDER BY rating ASC;
+SELECT title FROM movies LIMIT 3;
+SELECT title, rating FROM movies ORDER BY rating DESC LIMIT 5;
+SELECT CONCAT(first_name, " " , last_name) AS Nombre_Completo FROM actors LIMIT 10;
+SELECT title, rating FROM movies WHERE title LIKE '%Toy Story%';
+SELECT CONCAT(first_name, " ", last_name) AS Full_Name FROM actors WHERE first_name LIKE 'Sam%';
+SELECT title AS titulo, YEAR(release_date) AS anio FROM movies WHERE YEAR(release_date) BETWEEN 2004 AND 2008;
+SELECT title FROM movies WHERE rating > 3 AND awards > 1 AND YEAR(release_date) BETWEEN 1988 AND 2009;
