fef993c3234c1421b453aa60db97860d0158c843
ramosjpruiz <juanpablo.ramos@mercadolibre.com>
2023-01-19 10:02:18 -0300

    Adds 10 queries.

--- 7. bd relacionales/ISP/populate-tables.sql
+++ 7. bd relacionales/ISP/populate-tables.sql
@@ -2,9 +2,9 @@ INSERT INTO plan
 VALUES
 (1, 10, 1000, 0),
 (2, 50, 3000, 0),
-(3, 100, 10000, 0.25),
+(3, 100, 10000, 25),
 (4, 200, 15000, 0),
-(5, 300, 20000, 0.1);
+(5, 300, 20000, 10);
 
 INSERT INTO cliente
 VALUES
@@ -18,3 +18,56 @@ VALUES
 (35533586, "Carlos", "Samuel", "1990-07-21", "Chubut", "Rawson", 5),
 (44533799, "Ruben", "", "1995-06-29", "Buenos Aires", "Lobos", 3),
 (40537846, "Mateo", "Martes", "1960-10-02", "San Luis", "Catamarca", 2);
+
+
+-- 1. Obtener todos los planes disponibles.
+
+SELECT velocidad_mb, precio from plan
+ORDER BY velocidad_mb ASC;
+
+-- 2. Obtener los planes a los que se suscriben los clientes de Buenos Aires.
+
+SELECT dni, nombre, plan_id_plan FROM cliente
+WHERE provincia LIKE "Buenos Aires"; 
+
+-- 3. Obtener los clientes que nacieron despues de los 2000.
+
+SELECT dni, nombre FROM cliente
+WHERE YEAR(fecha_nacimiento) >= 2000;
+
+-- 4. Obtener los clientes que cumplen años en Octubre.
+
+SELECT dni, nombre, plan_id_plan FROM cliente
+WHERE MONTH(fecha_nacimiento) = 10;
+
+-- 5. Aplicar descuento de 10% a todos los planes > 100mb
+
+UPDATE plan
+SET descuento = 10
+WHERE velocidad_mb >= 100;
+
+-- 6. Obtener los planes con descuento.
+
+SELECT velocidad_mb, precio, descuento FROM plan
+WHERE descuento > 0
+ORDER BY descuento DESC;
+
+-- 7. Listar los primeros 5 clientes, ordenados por plan contratado.
+
+SELECT dni, concat(nombre, " ", apellido), plan as nombre_completo FROM cliente
+ORDER BY plan_id_plan ASC
+LIMIT 5;
+
+-- 8. Obtener los clientes cuyo documento comienza con 4.
+
+SELECT dni, nombre FROM cliente
+WHERE dni LIKE "4%";
+
+-- 9. Obtener los clientes del interior.
+
+SELECT dni, nombre, plan_id_plan, provincia FROM cliente
+WHERE provincia NOT LIKE "Buenos Aires"; 
+
+-- 10. Eliminar el plan basico.
+DELETE FROM plan
+WHERE id_plan = 1;
