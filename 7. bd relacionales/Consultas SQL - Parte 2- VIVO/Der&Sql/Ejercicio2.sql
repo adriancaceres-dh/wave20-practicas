646a0071c85f550f3125ddd549056b04ac8928ef
Santiago Valdez <santiago.svaldez@mercadolibre.com>
2023-01-19 17:28:42 -0300

    Se agregan los ejericios de Consultas sql, DER Y SQL

---
+++ 7. bd relacionales/Consultas SQL - Parte 2- VIVO/Der&Sql/Ejercicio2.sql
@@ -0,0 +1,15 @@
+
+
+
+-- EJERCICIO 2.
+
+-- a. ¿Cuál es la primary key para la tabla de clientes? Justificar respuesta
+-- Ra. la pk de la tabla clientes es el dni ya que con el puedes identificar los demas atributos asociados de esa tabla y ese cliente.
+
+-- b. ¿Cuál es la primary key para la tabla de planes de internet? Justificar respuesta.
+--Rb. la pk de la tabla de planes es el id de plan.
+
+--Rc. Cómo serían las relaciones entre tablas? ¿En qué tabla debería haber foreign key? ¿A qué campo de qué tabla hace referencia dicha foreign key? Justificar respuesta.
+-- c. Debe haber una fk entre el "cliente" y la tabla "cliente_tiene_plan" sobre el dni, debe haber otra fk entre la table "cliente_tiene_plan" y "plan" sobre el idPlan.
+
+
