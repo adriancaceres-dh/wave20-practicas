c6b4dde948f5dc778ac3366a7bc80771a38a84b6
Daniel Angel <daniel.rojas@mercadolibre.com.co>
2023-01-20 01:19:18 -0500

    Solución de consultas. Ejercicios Practicos P2_VIVO

---
+++ 7. bd relacionales/Consultas1_P2_VIVO.sql
@@ -0,0 +1,13 @@
+SELECT * FROM movies;
+SELECT first_name, last_name, rating FROM actors;
+SELECT title AS Titulo FROM series;
+SELECT first_name, last_name FROM actors WHERE rating > 7.5;
+SELECT title AS Titulo, rating AS Clasificacion, awards AS Premios FROM movies WHERE rating > 7.5 AND awards > 2;
+SELECT title, rating FROM movies ORDER BY rating ASC;
+SELECT title FROM movies LIMIT 3;
+SELECT rating, title as TITULO FROM movies ORDER BY rating DESC LIMIT 5;
+SELECT * FROM actors LIMIT 10;
+SELECT title AS Titulo, rating FROM movies WHERE title  LIKE '%Toy Story%';
+SELECT CONCAT(first_name, ' ', last_name) "Full Name", rating FROM actors WHERE first_name LIKE 'sam';
+SELECT title FROM movies WHERE release_date BETWEEN '2004-01-01' AND '2008-12-31';
+SELECT title AS Titulo FROM movies WHERE rating > 3 AND awards > 1 AND release_date BETWEEN '1988-01-01' AND '2009-12-31' ORDER BY rating DESC;
