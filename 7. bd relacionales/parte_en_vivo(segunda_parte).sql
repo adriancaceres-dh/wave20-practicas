232d6ebb8a6168c692294aa5fa2e93bd1e0a3ba5
ttrivelli1 <tammara.trivelli@mercadolibre.com>
2023-01-20 09:33:13 -0300

    ejercicio en vivo(segunda parte) consultas que faltaban por subir

--- 7. bd relacionales/parte_en_vivo(segunda_parte).sql
+++ 7. bd relacionales/parte_en_vivo(segunda_parte).sql
@@ -8,13 +8,11 @@ SELECT *
 FROM autor;
 
 
-
 2) Listar nombre y edad de los estudiantes
 Consulta:
 
 SELECT nombre, edad
 FROM estudiante;
-
 
 
 3) ¿Qué estudiantes pertenecen a la carrera informática?
@@ -25,15 +23,12 @@ FROM estudiante
 WHERE carrera LIKE “Informatica”;
 
 
-
-
 4) ¿Qué autores son de nacionalidad francesa o italiana?
 Consulta:
 
 SELECT nombre
 FROM autor
 WHERE nacionalidad LIKE “francesa” OR nacionalidad LIKE “italiana” ;
-
 
 
 5) ¿Qué libros no son del área de internet?
@@ -52,7 +47,6 @@ FROM libro
 WHERE editorial IN (“salamandra”);
 
 
-
 7) Listar los datos de los estudiantes cuya edad es mayor al promedio.
 Consulta:
 
@@ -69,7 +63,6 @@ FROM estudiante
 WHERE apellido LIKE “G%”
 
 
-
 9) Listar los autores del libro “El Universo: Guía de viaje”. (Se debe listar solamente los nombres).
 Consulta:
 
@@ -83,26 +76,53 @@ HAVING lib.titulo LIKE “El Universo: Guía de viaje”
 Consulta: 
 
 
+SELECT lib.titulo, lib.editorial, lib.area
+FROM libro lib INNER JOIN prestamo pre ON (lib.idlibro = pre.idlibro)
+JOIN estudiante est ON (pre.idlector = est.idlector)
+GROUP BY est.nombre 
+HAVING est.nombre LIKE "Filippo" AND est.apellido LIKE "Galli"
+
 
 11) Listar el nombre del estudiante de menor edad.
 Consulta: 
+ Pueden haber 2 formas de hacer:
+ 
+Primera)
+ 
+SELECT est.nombre, est.apellido, est.direccion, est.carrera, est.edad
+FROM estudiante est
+WHERE est.edad = (SELECT MIN(edad) FROM estudiante);
+
+Segunda)
+
+SELECT est.nombre, est.apellido, est.direccion, est.carrera, est.edad
+FROM estudiante est
+ORDER BY est.edad ASC LIMIT 1;
 
 
 12) Listar nombres de los estudiantes a los que se prestaron libros de Base de Datos.
 Consulta:
 
+SELECT est.nombre
+FROM estudiante est INNER JOIN prestamo prest ON est.idlector = prest.idlector
+JOIN libro lib ON prest.idlibro = lib.idlibro
+ORDER BY lib.titulo
+HAVING lib.titulo LIKE "Base de Datos";
 
 
 13)	istar los libros que pertenecen a la autora J.K. Rowling.
 Consulta:
 
+SELECT lib.titulo, lib.editorial, lib.area
+FROM libro lib INNER JOIN libroautor libaut ON lib.idlibro = libautor.idautor
+JOIN autor aut ON libaut.idautor = aut.idautor
+GROUP BY aut.nombre
+HAVING aut.nombre LIKE "J.K. Rowling"
 
 
 14) Listar títulos de los libros que debían devolverse el 16/07/2021.
 Consulta:
 
-
-
-
-
-
+SELECT lib.titulo
+FROM libro lib INNER JOIN prestamo prest ON lib.idlibro = prest.idlibro
+WHERE prest.fechadevolucion = '16/07/2021'
