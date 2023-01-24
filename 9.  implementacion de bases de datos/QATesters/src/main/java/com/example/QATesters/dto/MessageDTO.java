0ad4997d62199f0e6ccb873457ca1d86821ca217
DiegoAlejandroMalagon <diego.malagon@mercadolibre.com.co>
2023-01-24 17:21:50 -0500

    API de Test cases con JPA-Hibernate

---
+++ 9.  implementacion de bases de datos/QATesters/src/main/java/com/example/QATesters/dto/MessageDTO.java
@@ -0,0 +1,17 @@
+package com.example.QATesters.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MessageDTO {
+
+    private String message;
+
+    private String action;
+}
