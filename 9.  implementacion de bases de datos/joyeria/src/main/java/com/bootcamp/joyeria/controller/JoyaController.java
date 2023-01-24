879b2e5e8891ce3ef7944e26fb31594f56a6777b
ramosjpruiz <juanpablo.ramos@mercadolibre.com>
2023-01-24 12:49:04 -0300

    Fix controller endpoints

--- 9.  implementacion de bases de datos/joyeria/src/main/java/com/bootcamp/joyeria/controller/JoyaController.java
+++ 9.  implementacion de bases de datos/joyeria/src/main/java/com/bootcamp/joyeria/controller/JoyaController.java
@@ -5,6 +5,7 @@ import com.bootcamp.joyeria.service.IJoyaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class JoyaController {
     private IJoyaService joyaService;
 
     @PostMapping("/new")
-    public ResponseEntity<String> crearJoya(JoyaDto joyaDto) {
+    public ResponseEntity<String> crearJoya(@RequestBody JoyaDto joyaDto) {
         return ResponseEntity.ok("Se creó la joya satisfactoriamente con el id: " + joyaService.saveJoya(joyaDto));
     }
 }
