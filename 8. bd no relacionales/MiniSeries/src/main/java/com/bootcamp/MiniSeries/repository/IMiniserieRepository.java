e45528f6d3619b19042cddd2bc95abf6cf62ac17
ale-espindola <alejandra.espindola@mercadolibre.com>
2023-01-25 17:45:04 -0300

    Moviendo los proyectos a la carpeta 9. Implementación

--- 8. bd no relacionales/MiniSeries/src/main/java/com/bootcamp/MiniSeries/repository/IMiniserieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bootcamp.MiniSeries.repository;
-
-import com.bootcamp.MiniSeries.model.MiniSerie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IMiniserieRepository extends JpaRepository<MiniSerie, Long> {
-}
