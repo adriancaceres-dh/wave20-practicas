9127e6f262da802af7a6427462dfad0fd0d53cd6
ttrivelli1 <tammara.trivelli@mercadolibre.com>
2023-01-26 11:04:37 -0300

    Proyecto para empezar practica de clave compuesta

---
+++ 9.  implementacion de bases de datos/MapeoDeUnaClaveCompuesta/src/main/java/com/example/MapeoDeUnaClaveCompuesta/MapeoDeUnaClaveCompuestaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MapeoDeUnaClaveCompuesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapeoDeUnaClaveCompuestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapeoDeUnaClaveCompuestaApplication.class, args);
+	}
+
+}
---
+++ 9.  implementacion de bases de datos/MapeoDeUnaClaveCompuesta/src/test/java/com/example/MapeoDeUnaClaveCompuesta/MapeoDeUnaClaveCompuestaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MapeoDeUnaClaveCompuesta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapeoDeUnaClaveCompuestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
