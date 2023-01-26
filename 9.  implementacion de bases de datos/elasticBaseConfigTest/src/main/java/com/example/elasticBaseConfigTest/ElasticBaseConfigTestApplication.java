75882d352f3afc8b6cbe4f9d6895008ab110fe03
Esteban Demarchi <esteban.demarchi@mercadolibre.com>
2023-01-26 14:14:33 -0300

    Add elastic search dummy base proyect

---
+++ 9.  implementacion de bases de datos/elasticBaseConfigTest/src/main/java/com/example/elasticBaseConfigTest/ElasticBaseConfigTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticBaseConfigTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticBaseConfigTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticBaseConfigTestApplication.class, args);
+	}
+
+}
---
+++ 9.  implementacion de bases de datos/elasticBaseConfigTest/src/test/java/com/example/elasticBaseConfigTest/ElasticBaseConfigTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticBaseConfigTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticBaseConfigTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
