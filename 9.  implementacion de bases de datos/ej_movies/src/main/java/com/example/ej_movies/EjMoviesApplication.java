dfbfc7a88b9af4082f99d0d0bbb1928ff9d68515
Lucia Balestrazzi <lucia.balestrazzi@mercadolibre.com>
2023-01-27 16:47:38 -0300

    ej movies

---
+++ 9.  implementacion de bases de datos/ej_movies/src/main/java/com/example/ej_movies/EjMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.example.ej_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjMoviesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EjMoviesApplication.class, args);
+  }
+
+}
