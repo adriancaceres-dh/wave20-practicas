5df52d4ed985683cd0cfe3d44e90344696066552
manueldiazmeli <manuel.diaz@mercadolibre.com.co>
2023-01-31 15:49:30 -0500

    Fury III Practice Time - Item Price in USD

---
+++ 10. fury/2_fury_III_practiceTime/src/main/java/com/mercadolibre/bootcamp_demo_java_app/dtos/CurrencyConvertDTO.java
@@ -0,0 +1,27 @@
+package com.mercadolibre.bootcamp_demo_java_app.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CurrencyConvertDTO {
+
+    @JsonProperty("currency_base")
+    private String currencyBase;
+
+    @JsonProperty("currency_quote")
+    private String currencyQuote;
+
+    @JsonProperty("ratio")
+    private Double ratio;
+
+    @JsonProperty("rate")
+    private Double rate;
+
+    @JsonProperty("inv_rate")
+    private Double invRate;
+}
